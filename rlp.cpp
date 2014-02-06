dd0957359b5d26a9e82e50fee36d800d61687ee6
Gav Wood <i@gavwood.com>
2014-02-06 18:53:46 +0000

    AlephZero renamed AlethZero and moved to cmake project. Other minor changes.

--- rlp.cpp
+++ rlp.cpp
@@ -3,7 +3,7 @@
 
 	cpp-ethereum is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 2 of the License, or
+	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
 	Foobar is distributed in the hope that it will be useful,
