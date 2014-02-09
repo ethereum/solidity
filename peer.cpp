d8c693a338594ae83f94e81e95d499340c36775b
Gav Wood <i@gavwood.com>
2014-02-09 11:37:13 +0000

    usleep moved to std impl.

--- peer.cpp
+++ peer.cpp
@@ -20,6 +20,8 @@
  * Peer Network test functions.
  */
 
+#include <chrono>
+#include <thread>
 #include <BlockChain.h>
 #include <PeerNetwork.h>
 using namespace std;
@@ -53,7 +55,7 @@ int peerTest(int argc, char** argv)
 
 	for (int i = 0; ; ++i)
 	{
-		usleep(100000);
+		this_thread::sleep_for(chrono::milliseconds(100000));
 		pn.process(ch);
 		if (!(i % 10))
 			pn.pingAll();
