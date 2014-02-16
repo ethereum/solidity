0d91f190329e21fa29f47f4b639b06b278bd60eb
Muharem Hrnjadovic <mh@cloud1.it>
2014-02-16 11:20:55 +0100

    Corrected license headers (Foobar -> cpp-ethereum)

--- dagger.cpp
+++ dagger.cpp
@@ -6,7 +6,7 @@
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 
-	Foobar is distributed in the hope that it will be useful,
+	cpp-ethereum is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 	GNU General Public License for more details.
