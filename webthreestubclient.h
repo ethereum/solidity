543699df955fe73c912281d98bd316d413282dbd
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-11-06 15:51:08 +0100

    added eth_lll

--- webthreestubclient.h
+++ webthreestubclient.h
@@ -254,6 +254,19 @@ p.append(param3);
 
         }
 
+        std::string eth_lll(const std::string& param1) throw (jsonrpc::JsonRpcException)
+        {
+            Json::Value p;
+            p.append(param1);
+
+            Json::Value result = this->client->CallMethod("eth_lll",p);
+    if (result.isString())
+        return result.asString();
+     else 
+         throw jsonrpc::JsonRpcException(jsonrpc::Errors::ERROR_CLIENT_INVALID_RESPONSE, result.toStyledString());
+
+        }
+
         bool eth_mining() throw (jsonrpc::JsonRpcException)
         {
             Json::Value p;
