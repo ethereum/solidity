5c67fd341b99e53d8b80a2db13007ee1a73cabf9
Marek Kotewicz <marek.kotewicz@gmail.com>
2014-11-13 12:25:49 +0100

    removed unnecessary methods from jsonrpc and added contract call tests

--- webthreestubclient.h
+++ webthreestubclient.h
@@ -191,33 +191,6 @@ p.append(param3);
 
         }
 
-        std::string eth_contractCall(const std::string& param1, const std::string& param2) throw (jsonrpc::JsonRpcException)
-        {
-            Json::Value p;
-            p.append(param1);
-p.append(param2);
-
-            Json::Value result = this->client->CallMethod("eth_contractCall",p);
-    if (result.isString())
-        return result.asString();
-     else 
-         throw jsonrpc::JsonRpcException(jsonrpc::Errors::ERROR_CLIENT_INVALID_RESPONSE, result.toStyledString());
-
-        }
-
-        std::string eth_contractCreate(const std::string& param1) throw (jsonrpc::JsonRpcException)
-        {
-            Json::Value p;
-            p.append(param1);
-
-            Json::Value result = this->client->CallMethod("eth_contractCreate",p);
-    if (result.isString())
-        return result.asString();
-     else 
-         throw jsonrpc::JsonRpcException(jsonrpc::Errors::ERROR_CLIENT_INVALID_RESPONSE, result.toStyledString());
-
-        }
-
         double eth_countAt(const std::string& param1) throw (jsonrpc::JsonRpcException)
         {
             Json::Value p;
