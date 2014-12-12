3a9837f7f25af435f77762be1d24690132ffe8d6
CJentzsch <jentzsch.software@gmail.com>
2014-12-12 21:09:23 +0100

    include original trie test

--- trie.cpp
+++ trie.cpp
@@ -50,7 +50,7 @@ static unsigned fac(unsigned _i)
 
 BOOST_AUTO_TEST_SUITE(TrieTests)
 
-BOOST_AUTO_TEST_CASE(trie_tests)
+BOOST_AUTO_TEST_CASE(trie_test_anyorder)
 {
 	string testPath = test::getTestPath();
 
@@ -90,6 +90,50 @@ BOOST_AUTO_TEST_CASE(trie_tests)
 			BOOST_REQUIRE(!o["root"].is_null());
 			BOOST_CHECK_EQUAL(o["root"].get_str(), "0x" + toHex(t.root().asArray()));
 		}
+	}
+}
+
+BOOST_AUTO_TEST_CASE(trie_tests_ordered)
+{
+	string testPath = test::getTestPath();
+
+	testPath += "/TrieTests";
+
+	cnote << "Testing Trie...";
+	js::mValue v;
+	string s = asString(contents(testPath + "/trietest.json"));
+	BOOST_REQUIRE_MESSAGE(s.length() > 0, "Contents of 'trietest.json' is empty. Have you cloned the 'tests' repo branch develop?");
+	js::read_string(s, v);
+	for (auto& i: v.get_obj())
+	{
+		cnote << i.first;
+		js::mObject& o = i.second.get_obj();
+		vector<pair<string, string>> ss;
+		for (auto& i: o["in"].get_array())
+		{
+			vector<string> values;
+			for (auto& s: i.get_array())
+				values.push_back(s.get_str());
+
+			assert(values.size() == 2);
+			ss.push_back(make_pair(values[0], values[1]));
+			if (!ss.back().first.find("0x"))
+				ss.back().first = asString(fromHex(ss.back().first.substr(2)));
+			if (!ss.back().second.find("0x"))
+				ss.back().second = asString(fromHex(ss.back().second.substr(2)));
+		}
+
+		MemoryDB m;
+		GenericTrieDB<MemoryDB> t(&m);
+		t.init();
+		BOOST_REQUIRE(t.check(true));
+		for (auto const& k: ss)
+		{
+			t.insert(k.first, k.second);
+			BOOST_REQUIRE(t.check(true));
+		}
+		BOOST_REQUIRE(!o["root"].is_null());
+		BOOST_CHECK_EQUAL(o["root"].get_str(), "0x" + toHex(t.root().asArray()));
 	}
 }
 
