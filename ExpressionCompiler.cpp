c3d36d1fe197156427cd8adb4edbf955bba0531a
liana <liana@ethdev.com>
2015-01-16 16:26:57 +0100

    - corrected delete in case we have more than one locals - added a test

--- ExpressionCompiler.cpp
+++ ExpressionCompiler.cpp
@@ -810,9 +810,9 @@ void ExpressionCompiler::LValue::setToZero(Expression const& _expression) const
 		if (stackDiff > 16)
 			BOOST_THROW_EXCEPTION(CompilerError() << errinfo_sourceLocation(_expression.getLocation())
 												  << errinfo_comment("Stack too deep."));
-		solAssert(stackDiff <= m_size, "");
+		solAssert(stackDiff >= m_size - 1, "");
 		for (unsigned i = 0; i < m_size; ++i)
-			*m_context << u256(0) << eth::swapInstruction(stackDiff + (m_size - i) )
+			*m_context << u256(0) << eth::swapInstruction(stackDiff + 1 - i)
 						<< eth::Instruction::POP;
 		break;
 	}
