fb55f4f24dc1fee6e35e828ba0454c328d14f7ca
Lefteris Karapetsas <lefteris@refu.co>
2015-02-05 22:38:07 +0100

    Minor Style fixes

--- SolidityScanner.cpp
+++ SolidityScanner.cpp
@@ -258,10 +258,10 @@ BOOST_AUTO_TEST_CASE(comments_mixed_in_sequence)
 BOOST_AUTO_TEST_CASE(ether_subdenominations)
 {
 	Scanner scanner(CharStream("wei szabo finney ether"));
-	BOOST_CHECK_EQUAL(scanner.getCurrentToken(), Token::ETH_SUB_WEI);
-	BOOST_CHECK_EQUAL(scanner.next(), Token::ETH_SUB_SZABO);
-	BOOST_CHECK_EQUAL(scanner.next(), Token::ETH_SUB_FINNEY);
-	BOOST_CHECK_EQUAL(scanner.next(), Token::ETH_SUB_ETHER);
+	BOOST_CHECK_EQUAL(scanner.getCurrentToken(), Token::SubWei);
+	BOOST_CHECK_EQUAL(scanner.next(), Token::SubSzabo);
+	BOOST_CHECK_EQUAL(scanner.next(), Token::SubFinney);
+	BOOST_CHECK_EQUAL(scanner.next(), Token::SubEther);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
