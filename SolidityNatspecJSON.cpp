5ab36afaed2372278904b838ba10e3c6d87d1c75
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-02-10 12:15:44 +0100

    fixed jsoncpp find_path

--- SolidityNatspecJSON.cpp
+++ SolidityNatspecJSON.cpp
@@ -21,7 +21,7 @@
  */
 
 #include <boost/test/unit_test.hpp>
-#include <jsoncpp/json/json.h>
+#include <json/json.h>
 #include <libsolidity/CompilerStack.h>
 #include <libsolidity/Exceptions.h>
 #include <libdevcore/Exceptions.h>
