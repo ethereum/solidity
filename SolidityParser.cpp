bb30e9184b37d736e588be51a20fdf9758c3ef57
Lefteris Karapetsas <lefteris@refu.co>
2015-02-11 16:37:46 +0100

    Introducing EnumType and some Parser tests

--- SolidityParser.cpp
+++ SolidityParser.cpp
@@ -717,6 +717,20 @@ BOOST_AUTO_TEST_CASE(enum_declaration)
 	BOOST_CHECK_NO_THROW(parseTextExplainError(text));
 }
 
+BOOST_AUTO_TEST_CASE(empty_enum_declaration)
+{
+	char const* text = R"(
+		contract c {
+			enum foo { };
+			function c ()
+			{
+				a = 5;
+			}
+			uint256 a;
+		})";
+	BOOST_CHECK_NO_THROW(parseTextExplainError(text));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 }
