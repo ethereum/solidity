223a102aff41ad44bf82b348643fd100ba5fa6a8
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-03-10 11:40:30 +0100

    eth_call on block with number

--- webthreestubclient.h
+++ webthreestubclient.h
@@ -198,10 +198,11 @@ class WebThreeStubClient : public jsonrpc::Client
             else
                 throw jsonrpc::JsonRpcException(jsonrpc::Errors::ERROR_CLIENT_INVALID_RESPONSE, result.toStyledString());
         }
-        std::string eth_call(const Json::Value& param1) throw (jsonrpc::JsonRpcException)
+        std::string eth_call(const Json::Value& param1, const std::string& param2) throw (jsonrpc::JsonRpcException)
         {
             Json::Value p;
             p.append(param1);
+            p.append(param2);
             Json::Value result = this->CallMethod("eth_call",p);
             if (result.isString())
                 return result.asString();
