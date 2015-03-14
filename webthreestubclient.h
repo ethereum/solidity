204f53c6ad8fd09884bc3023075f4534ce9bcd81
Gav Wood <i@gavwood.com>
2015-03-14 19:00:41 +0100

    getData -> getCode.

--- webthreestubclient.h
+++ webthreestubclient.h
@@ -197,12 +197,12 @@ class WebThreeStubClient : public jsonrpc::Client
             else
                 throw jsonrpc::JsonRpcException(jsonrpc::Errors::ERROR_CLIENT_INVALID_RESPONSE, result.toStyledString());
         }
-        std::string eth_getData(const std::string& param1, const std::string& param2) throw (jsonrpc::JsonRpcException)
+        std::string eth_getCode(const std::string& param1, const std::string& param2) throw (jsonrpc::JsonRpcException)
         {
             Json::Value p;
             p.append(param1);
             p.append(param2);
-            Json::Value result = this->CallMethod("eth_getData",p);
+            Json::Value result = this->CallMethod("eth_getCode",p);
             if (result.isString())
                 return result.asString();
             else
