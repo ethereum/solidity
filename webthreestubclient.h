1b09e5debba1e04627474ba8ec3b2b7d5bcd1fd6
Gav Wood <i@gavwood.com>
2015-03-14 19:59:07 +0100

    Renaming getData -> getCode, remove getStorage, fix a bunch of JSONRPC methods with more expansive error handling.

--- webthreestubclient.h
+++ webthreestubclient.h
@@ -123,17 +123,6 @@ class WebThreeStubClient : public jsonrpc::Client
             else
                 throw jsonrpc::JsonRpcException(jsonrpc::Errors::ERROR_CLIENT_INVALID_RESPONSE, result.toStyledString());
         }
-        Json::Value eth_getStorage(const std::string& param1, const std::string& param2) throw (jsonrpc::JsonRpcException)
-        {
-            Json::Value p;
-            p.append(param1);
-            p.append(param2);
-            Json::Value result = this->CallMethod("eth_getStorage",p);
-            if (result.isObject())
-                return result;
-            else
-                throw jsonrpc::JsonRpcException(jsonrpc::Errors::ERROR_CLIENT_INVALID_RESPONSE, result.toStyledString());
-        }
         std::string eth_getStorageAt(const std::string& param1, const std::string& param2, const std::string& param3) throw (jsonrpc::JsonRpcException)
         {
             Json::Value p;
