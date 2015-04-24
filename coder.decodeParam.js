1a137629e31ac63d44fcdd72ddf454b1ce692354
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-04-24 11:02:20 +0200

    Squashed 'libjsqrc/ethereumjs/' changes from f8a43ed..c74c854
    c74c854 version 0.3.2
    9fa9cc7 fixed fixed size bytes encoding and decoding
    
    git-subtree-dir: libjsqrc/ethereumjs
    git-subtree-split: c74c854b56cb76bfc941d7aef8a135b7c1cfc4b3

--- coder.decodeParam.js
+++ coder.decodeParam.js
@@ -3,9 +3,12 @@ var assert = chai.assert;
 var coder = require('../lib/solidity/coder');
 
 var tests = [
-    { type: 'int', value: '0000000000000000000000000000000000000000000000000000000000000001', expected: 1},
-    { type: 'int', value: '0000000000000000000000000000000000000000000000000000000000000010', expected: 16},
-    { type: 'int', value: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', expected: -1}
+    { type: 'int',      value: '0000000000000000000000000000000000000000000000000000000000000001', expected: 1},
+    { type: 'int',      value: '0000000000000000000000000000000000000000000000000000000000000010', expected: 16},
+    { type: 'int',      value: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', expected: -1},
+    { type: 'bytes32',  value: '6761766f66796f726b0000000000000000000000000000000000000000000000', expected: 'gavofyork'},
+    { type: 'bytes',    value: '0000000000000000000000000000000000000000000000000000000000000009' + 
+                               '6761766f66796f726b0000000000000000000000000000000000000000000000', expected: 'gavofyork'}
 ];
 
 describe('lib/solidity/coder', function () {
--- coder.encodeParam.js
+++ coder.encodeParam.js
@@ -3,9 +3,12 @@ var assert = chai.assert;
 var coder = require('../lib/solidity/coder');
 
 var tests = [
-    { type: 'int', value: 1,    expected: '0000000000000000000000000000000000000000000000000000000000000001'},
-    { type: 'int', value: 16,   expected: '0000000000000000000000000000000000000000000000000000000000000010'},
-    { type: 'int', value: -1,   expected: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'}
+    { type: 'int', value: 1,                expected: '0000000000000000000000000000000000000000000000000000000000000001'},
+    { type: 'int', value: 16,               expected: '0000000000000000000000000000000000000000000000000000000000000010'},
+    { type: 'int', value: -1,               expected: 'ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff'},
+    { type: 'bytes32', value: 'gavofyork',  expected: '6761766f66796f726b0000000000000000000000000000000000000000000000'},
+    { type: 'bytes', value: 'gavofyork',    expected: '0000000000000000000000000000000000000000000000000000000000000009' + 
+                                                      '6761766f66796f726b0000000000000000000000000000000000000000000000'}
 ];
 
 describe('lib/solidity/coder', function () {
