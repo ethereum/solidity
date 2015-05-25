b85f66060f054ecfb160314cc46e9b47bea403e3
chriseth <c@ethdev.com>
2015-05-25 11:49:38 +0200

    Sort keywords and add some reserved keywords.

--- Token.h
+++ Token.h
@@ -142,34 +142,34 @@ namespace solidity
 	K(Delete, "delete", 0)                                             \
 	\
 	/* Keywords */                                                     \
+	K(Anonymous, "anonymous", 0)                                       \
 	K(Break, "break", 0)                                               \
 	K(Const, "constant", 0)                                            \
-	K(Anonymous, "anonymous", 0)                                       \
 	K(Continue, "continue", 0)                                         \
 	K(Contract, "contract", 0)                                         \
 	K(Default, "default", 0)                                           \
 	K(Do, "do", 0)                                                     \
 	K(Else, "else", 0)                                                 \
+	K(Enum, "enum", 0)                                                 \
 	K(Event, "event", 0)                                               \
 	K(External, "external", 0)                                         \
-	K(Is, "is", 0)                                                     \
-	K(Indexed, "indexed", 0)                                           \
 	K(For, "for", 0)                                                   \
 	K(Function, "function", 0)                                         \
 	K(If, "if", 0)                                                     \
+	K(Indexed, "indexed", 0)                                           \
+	K(Internal, "internal", 0)                                         \
 	K(Import, "import", 0)                                             \
+	K(Is, "is", 0)                                                     \
 	K(Mapping, "mapping", 0)                                           \
 	K(Modifier, "modifier", 0)                                         \
 	K(New, "new", 0)                                                   \
 	K(Public, "public", 0)                                             \
 	K(Private, "private", 0)                                           \
-	K(Internal, "internal", 0)                                         \
 	K(Return, "return", 0)                                             \
 	K(Returns, "returns", 0)                                           \
 	K(Struct, "struct", 0)                                             \
 	K(Var, "var", 0)                                                   \
 	K(While, "while", 0)                                               \
-	K(Enum, "enum", 0)                                                 \
 	\
 	/* Ether subdenominations */                                       \
 	K(SubWei, "wei", 0)                                                \
@@ -304,15 +304,21 @@ namespace solidity
 	T(Identifier, NULL, 0)                                             \
 	\
 	/* Keywords reserved for future. use*/                             \
-	T(String, "string", 0)                                             \
+	K(As, "as", 0)                                                     \
 	K(Case, "case", 0)                                                 \
+	K(Catch, "catch", 0)                                               \
+	K(Final, "final", 0)                                               \
+	K(Let, "let", 0)                                                   \
+	K(Match, "match", 0)                                               \
+	K(Of, "of", 0)                                                     \
+	K(Relocatable, "relocatable", 0)                                   \
+	T(String, "string", 0)                                             \
 	K(Switch, "switch", 0)                                             \
 	K(Throw, "throw", 0)                                               \
 	K(Try, "try", 0)                                                   \
-	K(Catch, "catch", 0)                                               \
-	K(Using, "using", 0)                                               \
 	K(Type, "type", 0)                                                 \
 	K(TypeOf, "typeof", 0)                                             \
+	K(Using, "using", 0)                                               \
 	/* Illegal token - not able to scan. */                            \
 	T(Illegal, "ILLEGAL", 0)                                           \
 	\
