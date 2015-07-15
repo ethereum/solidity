d21689502523b38c3625d6f3139b66baaf1c05cc
Liana Husikyan <liana@ethdev.com>
2015-07-15 14:45:35 +0200

    corrected comments

--- InterfaceHandler.h
+++ InterfaceHandler.h
@@ -72,19 +72,17 @@ public:
 	);
 	/// Get the ABI Interface of the contract
 	/// @param _contractDef The contract definition
-	/// @return             A unique pointer contained string with the json
-	///                     representation of the contract's ABI Interface
+	/// @return             A string with the json representation of the contract's ABI Interface
 	std::string getABIInterface(ContractDefinition const& _contractDef);
 	std::string getABISolidityInterface(ContractDefinition const& _contractDef);
 	/// Get the User documentation of the contract
 	/// @param _contractDef The contract definition
-	/// @return             A unique pointer contained string with the json
-	///                     representation of the contract's user documentation
+	/// @return             A string with the json representation of the contract's user documentation
 	std::string userDocumentation(ContractDefinition const& _contractDef);
 	/// Genereates the Developer's documentation of the contract
 	/// @param _contractDef The contract definition
-	/// @return             A unique pointer contained string with the json
-	///                     representation of the contract's developer documentation
+	/// @return             A string with the json representation
+	///						of the contract's developer documentation
 	std::string devDocumentation(ContractDefinition const& _contractDef);
 
 private:
