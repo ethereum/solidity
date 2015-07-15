13effae9d3247ff29f91310b18367fdb5920736d
chriseth <c@ethdev.com>
2015-07-16 01:06:19 +0200

    Allow structs containing mappings in memory.

--- libsolidity/SolidityEndToEndTest.cpp
+++ libsolidity/SolidityEndToEndTest.cpp
@@ -5034,6 +5034,29 @@ BOOST_AUTO_TEST_CASE(literal_strings)
 }
 
 
+BOOST_AUTO_TEST_CASE(memory_structs_with_mappings)
+{
+	char const* sourceCode = R"(
+		contract Test {
+			struct S { uint8 a; mapping(uint => uint) b; uint8 c; }
+			S s;
+			function f() returns (uint) {
+				S memory x;
+				if (x.a != 0 || x.c != 0) return 1;
+				x.a = 4; x.c = 5;
+				s = x;
+				if (s.a != 4 || s.c != 5) return 2;
+				x = S(2, 3);
+				if (x.a != 2 || x.c != 3) return 3;
+				x = s;
+				if (s.a != 4 || s.c != 5) return 4;
+			}
+		}
+	)";
+	compileAndRun(sourceCode, 0, "Test");
+	BOOST_CHECK(callContractFunction("f()") == encodeArgs(u256(0)));
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 }
