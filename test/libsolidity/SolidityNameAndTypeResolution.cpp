79c5d032fee1d9e96db9540dbafd312626d7711a
chriseth <c@ethdev.com>
2015-12-18 13:56:37 +0100

    Fix bug preventing overloads of different array types.

--- test/libsolidity/SolidityNameAndTypeResolution.cpp
+++ test/libsolidity/SolidityNameAndTypeResolution.cpp
@@ -2743,6 +2743,17 @@ BOOST_AUTO_TEST_CASE(invalid_args_creating_memory_array)
 	BOOST_CHECK(expectError(text) == Error::Type::TypeError);
 }
 
+BOOST_AUTO_TEST_CASE(function_overload_array_type)
+{
+	char const* text = R"(
+			contract M {
+				function f(uint[] values);
+				function f(int[] values);
+			}
+	)";
+	BOOST_CHECK(success(text));
+}
+
 /*BOOST_AUTO_TEST_CASE(inline_array_declaration_and_passing)
 {
 	char const* text = R"(
