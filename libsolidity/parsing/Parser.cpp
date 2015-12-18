e4f713b55bf0af4135b38170c4b8b3509c7ead1a
Lu Guanqun <guanqun.lu@gmail.com>
2015-12-18 23:48:34 +0800

    correct true/false literal's source
    Before:
            Literal, token: true value: true
               Type: bool
               Source: "true;"
    
    After:
            Literal, token: true value: true
               Type: bool
               Source: "true"
    
    Extra token is removed.

--- libsolidity/parsing/Parser.cpp
+++ libsolidity/parsing/Parser.cpp
@@ -1004,6 +1004,7 @@ ASTPointer<Expression> Parser::parsePrimaryExpression()
 	{
 	case Token::TrueLiteral:
 	case Token::FalseLiteral:
+		nodeFactory.markEndPosition();
 		expression = nodeFactory.createNode<Literal>(token, getLiteralAndAdvance());
 		break;
 	case Token::Number:
