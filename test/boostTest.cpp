a3c07adfe15f1af85b46783562152ffdc2558547
Bob Summerwill <bob@summerwill.net>
2016-03-21 15:42:18 -0700

    Fix syntax error.

--- test/boostTest.cpp
+++ test/boostTest.cpp
@@ -28,7 +28,7 @@
 #if defined(_MSC_VER)
 #pragma warning(push)
 #pragma warning(disable:4535) // calling _set_se_translator requires /EHa
-endif
+#endif
 #include <boost/test/included/unit_test.hpp>
 #if defined(_MSC_VER)
 #pragma warning(pop)
