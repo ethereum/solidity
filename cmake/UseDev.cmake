202c3f6ed704b2777cf3f00fff8b06696740e704
Bob Summerwill <bob@summerwill.net>
2016-04-28 12:49:39 -0700

    Chop out more stuff.   Looks like we're not doing any database stuff in solidity.

--- cmake/UseDev.cmake
+++ cmake/UseDev.cmake
@@ -2,13 +2,10 @@ function(eth_apply TARGET REQUIRED SUBMODULE)
 
 	# Base is where all dependencies for devcore are
 	if (${SUBMODULE} STREQUAL "base")
-		# if it's ethereum source dir, alwasy build BuildInfo.h before
-		eth_use(${TARGET} ${REQUIRED} Dev::buildinfo Jsoncpp)
-		if (NOT EMSCRIPTEN)
-			eth_use(${TARGET} ${REQUIRED} DB::auto)
-		endif()
-		target_include_directories(${TARGET} SYSTEM PUBLIC ${Boost_INCLUDE_DIRS})
+		# if it's ethereum source dir, always build BuildInfo.h before
+		eth_use(${TARGET} ${REQUIRED} Dev::buildinfo)
 
+		target_include_directories(${TARGET} SYSTEM PUBLIC ${Boost_INCLUDE_DIRS})
 		target_link_libraries(${TARGET} ${Boost_THREAD_LIBRARIES})
 		target_link_libraries(${TARGET} ${Boost_RANDOM_LIBRARIES})
 		target_link_libraries(${TARGET} ${Boost_FILESYSTEM_LIBRARIES})
@@ -17,10 +14,6 @@ function(eth_apply TARGET REQUIRED SUBMODULE)
 		if (DEFINED MSVC)
 			target_link_libraries(${TARGET} ${Boost_CHRONO_LIBRARIES})
 			target_link_libraries(${TARGET} ${Boost_DATE_TIME_LIBRARIES})
-		endif()
-
-		if (NOT DEFINED MSVC)
-			target_link_libraries(${TARGET} pthread)
 		endif()
 	endif()
 
