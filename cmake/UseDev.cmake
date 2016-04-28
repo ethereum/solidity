1ab145eb895fb493fb6d24ae4e34da536f9d61a7
Bob Summerwill <bob@summerwill.net>
2016-04-28 14:08:04 -0700

    Re-added pthread dependency for Linux builds of solidity. It was previously "not MSVC", but it looks like we only need it for Linux, not for OS X. The same fix might even be possible upstream.   I should try that.

--- cmake/UseDev.cmake
+++ cmake/UseDev.cmake
@@ -15,6 +15,11 @@ function(eth_apply TARGET REQUIRED SUBMODULE)
 			target_link_libraries(${TARGET} ${Boost_CHRONO_LIBRARIES})
 			target_link_libraries(${TARGET} ${Boost_DATE_TIME_LIBRARIES})
 		endif()
+
+		if ("${CMAKE_SYSTEM_NAME}" MATCHES "Linux")
+			target_link_libraries(${TARGET} pthread)
+		endif()
+
 	endif()
 
 	if (${SUBMODULE} STREQUAL "devcore")
