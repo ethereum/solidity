f34f9acd06a72e1ad9139fc2dcebfb161f64d98c
Bob Summerwill <bob@summerwill.net>
2016-05-03 16:18:30 -0700

    Chopped out unused base36 code.

--- libdevcore/Base64.h
+++ libdevcore/Base64.h
@@ -38,25 +38,5 @@ namespace dev
 std::string toBase64(bytesConstRef _in);
 bytes fromBase64(std::string const& _in);
 
-template <size_t N> inline std::string toBase36(FixedHash<N> const& _h)
-{
-	static char const* c_alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	typename FixedHash<N>::Arith a = _h;
-	std::string ret;
-	for (; a > 0; a /= 36)
-	{
-		unsigned r = (unsigned)(a - a / 36 * 36); // boost's % is broken
-		ret = c_alphabet[r] + ret;
-	}
-	return ret;
-}
-
-template <size_t N> inline FixedHash<N> fromBase36(std::string const& _h)
-{
-	typename FixedHash<N>::Arith ret = 0;
-	for (char c: _h)
-		ret = ret * 36 + (c < 'A' ? c - '0' : (c - 'A' + 10));
-	return ret;
-}
 
 }
