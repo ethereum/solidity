0b58bb26185fa148d49d8a95525eec1e99c3ea19
Bob Summerwill <bob@summerwill.net>
2016-05-03 17:10:46 -0700

    Only a little bit of CommonIO has strippable. Lots of inlines.

--- libdevcore/CommonIO.h
+++ libdevcore/CommonIO.h
@@ -54,35 +54,6 @@ void writeFile(std::string const& _file, bytesConstRef _data, bool _writeDeleteR
 /// Write the given binary data into the given file, replacing the file if it pre-exists.
 inline void writeFile(std::string const& _file, bytes const& _data, bool _writeDeleteRename = false) { writeFile(_file, bytesConstRef(&_data), _writeDeleteRename); }
 inline void writeFile(std::string const& _file, std::string const& _data, bool _writeDeleteRename = false) { writeFile(_file, bytesConstRef(_data), _writeDeleteRename); }
-
-
-#if defined(_WIN32)
-template <class T> inline std::string toString(std::chrono::time_point<T> const& _e, std::string _format = "%Y-%m-%d %H:%M:%S")
-#else
-template <class T> inline std::string toString(std::chrono::time_point<T> const& _e, std::string _format = "%F %T")
-#endif
-{
-	unsigned long milliSecondsSinceEpoch = std::chrono::duration_cast<std::chrono::milliseconds>(_e.time_since_epoch()).count();
-	auto const durationSinceEpoch = std::chrono::milliseconds(milliSecondsSinceEpoch);
-	std::chrono::time_point<std::chrono::system_clock> const tpAfterDuration(durationSinceEpoch);
-
-	tm timeValue;
-	auto time = std::chrono::system_clock::to_time_t(tpAfterDuration);
-#if defined(_WIN32)
-	gmtime_s(&timeValue, &time);
-#else
-	gmtime_r(&time, &timeValue);
-#endif
-
-	unsigned const millisRemainder = milliSecondsSinceEpoch % 1000;
-	char buffer[1024];
-	if (strftime(buffer, sizeof(buffer), _format.c_str(), &timeValue))
-		return std::string(buffer) + "." + (millisRemainder < 1 ? "000" : millisRemainder < 10 ? "00" : millisRemainder < 100 ? "0" : "") + std::to_string(millisRemainder) + "Z";
-	return std::string();
-}
-
-// Functions that use streaming stuff.
-
 /// Converts arbitrary value to string representation using std::stringstream.
 template <class _T>
 std::string toString(_T const& _t)
