252a26c992a0b34221e8e882937433c604eaf0a5
Bob Summerwill <bob@summerwill.net>
2016-05-03 17:46:52 -0700

    Chopped out a lot of Common.cpp/Common.h. Most of what is left is pretty bad, IMHO. Having global typedefs is not a good pattern in the first place, and the tensity of the names just makes these a maintenance issue. Would probably be better to push these types up into the code which uses them?

--- libdevcore/Common.h
+++ libdevcore/Common.h
@@ -66,15 +66,8 @@ using byte = uint8_t;
 
 #define DEV_IGNORE_EXCEPTIONS(X) try { X; } catch (...) {}
 
-#define DEV_IF_NO_ELSE(X) if(!(X)){}else
-#define DEV_IF_THROWS(X) try{X;}catch(...)
-
 namespace dev
 {
-
-extern char const* Version;
-
-static const std::string EmptyString;
 
 // Binary data types.
 using bytes = std::vector<byte>;
@@ -135,28 +128,13 @@ using u256s = std::vector<u256>;
 using u160s = std::vector<u160>;
 using u256Set = std::set<u256>;
 using u160Set = std::set<u160>;
-
 // Map types.
 using StringMap = std::map<std::string, std::string>;
-using BytesMap = std::map<bytes, bytes>;
-using u256Map = std::map<u256, u256>;
-using HexMap = std::map<bytes, bytes>;
-
-// Hash types.
-using StringHashMap = std::unordered_map<std::string, std::string>;
-using u256HashMap = std::unordered_map<u256, u256>;
 
 // String types.
 using strings = std::vector<std::string>;
-
 // Fixed-length string types.
 using string32 = std::array<char, 32>;
-static const string32 ZeroString32 = {{ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }};
-
-// Null/Invalid values for convenience.
-static const bytes NullBytes;
-static const std::map<u256, u256> EmptyMapU256U256;
-extern const u256 Invalid256;
 
 /// Interprets @a _u as a two's complement signed number and returns the resulting s256.
 inline s256 u2s(u256 _u)
@@ -177,35 +155,6 @@ inline u256 s2u(s256 _u)
     else
 		return u256(c_end + _u);
 }
-
-/// Converts given int to a string and appends one of a series of units according to its size.
-std::string inUnits(bigint const& _b, strings const& _units);
-
-/// @returns the smallest n >= 0 such that (1 << n) >= _x
-inline unsigned int toLog2(u256 _x)
-{
-	unsigned ret;
-	for (ret = 0; _x >>= 1; ++ret) {}
-	return ret;
-}
-
-template <size_t n> inline u256 exp10()
-{
-	return exp10<n - 1>() * u256(10);
-}
-
-template <> inline u256 exp10<0>()
-{
-	return u256(1);
-}
-
-/// @returns the absolute distance between _a and _b.
-template <class N>
-inline N diff(N const& _a, N const& _b)
-{
-	return std::max(_a, _b) - std::min(_a, _b);
-}
-
 /// RAII utility class whose destructor calls a given function.
 class ScopeGuard
 {
@@ -216,118 +165,4 @@ public:
 private:
 	std::function<void(void)> m_f;
 };
-
-/// Inheritable for classes that have invariants.
-class HasInvariants
-{
-public:
-	/// Reimplement to specify the invariants.
-	virtual bool invariants() const = 0;
-};
-
-/// RAII checker for invariant assertions.
-class InvariantChecker
-{
-public:
-	InvariantChecker(HasInvariants* _this, char const* _fn, char const* _file, int _line): m_this(_this), m_function(_fn), m_file(_file), m_line(_line) { checkInvariants(_this, _fn , _file, _line, true); }
-	~InvariantChecker() { checkInvariants(m_this, m_function, m_file, m_line, false); }
-	/// Check invariants are met, throw if not.
-	static void checkInvariants(HasInvariants const* _this, char const* _fn, char const* _file, int line, bool _pre);
-
-private:
-	HasInvariants const* m_this;
-	char const* m_function;
-	char const* m_file;
-	int m_line;
-};
-
-/// Scope guard for invariant check in a class derived from HasInvariants.
-#if ETH_DEBUG
-#define DEV_INVARIANT_CHECK ::dev::InvariantChecker __dev_invariantCheck(this, BOOST_CURRENT_FUNCTION, __FILE__, __LINE__)
-#define DEV_INVARIANT_CHECK_HERE ::dev::InvariantChecker::checkInvariants(this, BOOST_CURRENT_FUNCTION, __FILE__, __LINE__, true)
-#else
-#define DEV_INVARIANT_CHECK (void)0;
-#define DEV_INVARIANT_CHECK_HERE (void)0;
-#endif
-
-/// Simple scope-based timer helper.
-class TimerHelper
-{
-public:
-	TimerHelper(std::string const& _id, unsigned _msReportWhenGreater = 0): m_t(std::chrono::high_resolution_clock::now()), m_id(_id), m_ms(_msReportWhenGreater) {}
-	~TimerHelper();
-
-private:
-	std::chrono::high_resolution_clock::time_point m_t;
-	std::string m_id;
-	unsigned m_ms;
-};
-
-class Timer
-{
-public:
-	Timer() { restart(); }
-
-	std::chrono::high_resolution_clock::duration duration() const { return std::chrono::high_resolution_clock::now() - m_t; }
-	double elapsed() const { return std::chrono::duration_cast<std::chrono::microseconds>(duration()).count() / 1000000.0; }
-	void restart() { m_t = std::chrono::high_resolution_clock::now(); }
-
-private:
-	std::chrono::high_resolution_clock::time_point m_t;
-};
-
-#define DEV_TIMED(S) for (::std::pair<::dev::TimerHelper, bool> __eth_t(S, true); __eth_t.second; __eth_t.second = false)
-#define DEV_TIMED_SCOPE(S) ::dev::TimerHelper __eth_t(S)
-#if defined(_WIN32)
-#define DEV_TIMED_FUNCTION DEV_TIMED_SCOPE(__FUNCSIG__)
-#else
-#define DEV_TIMED_FUNCTION DEV_TIMED_SCOPE(__PRETTY_FUNCTION__)
-#endif
-
-#define DEV_TIMED_ABOVE(S, MS) for (::std::pair<::dev::TimerHelper, bool> __eth_t(::dev::TimerHelper(S, MS), true); __eth_t.second; __eth_t.second = false)
-#define DEV_TIMED_SCOPE_ABOVE(S, MS) ::dev::TimerHelper __eth_t(S, MS)
-#if defined(_WIN32)
-#define DEV_TIMED_FUNCTION_ABOVE(MS) DEV_TIMED_SCOPE_ABOVE(__FUNCSIG__, MS)
-#else
-#define DEV_TIMED_FUNCTION_ABOVE(MS) DEV_TIMED_SCOPE_ABOVE(__PRETTY_FUNCTION__, MS)
-#endif
-
-#ifdef _MSC_VER
-// TODO.
-#define DEV_UNUSED
-#else
-#define DEV_UNUSED __attribute__((unused))
-#endif
-
-enum class WithExisting: int
-{
-	Trust = 0,
-	Verify,
-	Rescue,
-	Kill
-};
-
-/// Get the current time in seconds since the epoch in UTC
-uint64_t utcTime();
-
-}
-
-namespace std
-{
-
-inline dev::WithExisting max(dev::WithExisting _a, dev::WithExisting _b)
-{
-	return static_cast<dev::WithExisting>(max(static_cast<int>(_a), static_cast<int>(_b)));
-}
-
-template <> struct hash<dev::u256>
-{
-	size_t operator()(dev::u256 const& _a) const
-	{
-		unsigned size = _a.backend().size();
-		auto limbs = _a.backend().limbs();
-		return boost::hash_range(limbs, limbs + size);
-	}
-};
-
 }
