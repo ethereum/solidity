33250eef9e3322a366a71fbd55df12e829d70726
Alex Beregszaszi <alex@rtfs.hu>
2016-06-07 19:36:42 +0100

    Reject negative shifts within constants

--- libsolidity/ast/Types.cpp
+++ libsolidity/ast/Types.cpp
@@ -710,9 +710,11 @@ TypePointer RationalNumberType::binaryOperatorResult(Token::Value _operator, Typ
 			using boost::multiprecision::pow;
 			if (fractional)
 				return TypePointer();
-			else if (abs(other.m_value) > numeric_limits<uint32_t>::max())
+			else if (other.m_value < 0)
 				return TypePointer();
-			uint32_t exponent = abs(other.m_value).numerator().convert_to<uint32_t>();
+			else if (other.m_value > numeric_limits<uint32_t>::max())
+				return TypePointer();
+			uint32_t exponent = other.m_value.numerator().convert_to<uint32_t>();
 			value = m_value.numerator() * pow(bigint(2), exponent);
 			break;
 		}
@@ -721,9 +723,11 @@ TypePointer RationalNumberType::binaryOperatorResult(Token::Value _operator, Typ
 			using boost::multiprecision::pow;
 			if (fractional)
 				return TypePointer();
-			else if (abs(other.m_value) > numeric_limits<uint32_t>::max())
+			else if (other.m_value < 0)
 				return TypePointer();
-			uint32_t exponent = abs(other.m_value).numerator().convert_to<uint32_t>();
+			else if (other.m_value > numeric_limits<uint32_t>::max())
+				return TypePointer();
+			uint32_t exponent = other.m_value.numerator().convert_to<uint32_t>();
 			value = rational(m_value.numerator() / pow(bigint(2), exponent), 1);
 			break;
 		}
