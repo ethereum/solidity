d0b04c4abbfc339346502f31f5acd0575e226421
Bob Summerwill <bob@summerwill.net>
2016-07-01 23:53:25 +0100

    Stripped down to just Debug Trusty and El Capitan. Updated Travis steps to account for the fact that we're not using the umbrella anymore.

--- fetch_umbrella_build_and_test.sh
+++ fetch_umbrella_build_and_test.sh
@@ -7,34 +7,21 @@ then
   cd docs && sphinx-build -nW -b html -d _build/doctrees . _build/html && cd ..
 fi
 
-SUBREPO=solidity
-
-cd ..
-git clone --depth 3 -b develop https://github.com/ethereum/tests.git
-export ETHEREUM_TEST_PATH=$(pwd)/tests/
-git clone --recursive -b develop https://github.com/ethereum/webthree-umbrella.git
-cd webthree-umbrella
-rm -rf $SUBREPO
-mv ../$SUBREPO .
 mkdir build
 cd build
-OPTIONS=""
-if [[ "$OSTYPE" != "darwin"* ]]
-then
-  OPTIONS="-DCMAKE_C_COMPILER=/usr/lib/ccache/$CC -DCMAKE_CXX_COMPILER=/usr/lib/ccache/$CXX"
-fi
-cmake .. -DGUI=0 -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE $OPTIONS
-make lllc solc soljson soltest
-
+cmake .. -DCMAKE_BUILD_TYPE=$TRAVIS_BUILD_TYPE $OPTIONS
+make -j4
 
 # Test runs disabled for macos for now,
 # we need to find a way to install eth.
 if [[ "$OSTYPE" != "darwin"* ]]
 then
+  git clone --depth 3 -b develop https://github.com/ethereum/tests.git
+  export ETHEREUM_TEST_PATH=$(pwd)/tests/
+
   eth --test -d /tmp/test &
   while [ ! -S /tmp/test/geth.ipc ]; do sleep 2; done
   
   ./solidity/test/soltest --ipc /tmp/test/geth.ipc
   pkill eth
 fi
-
