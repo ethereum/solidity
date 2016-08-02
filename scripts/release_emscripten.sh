46be8bcda83b8a51823845acb36cd18f27fff9cc
Bob Summerwill <bob@summerwill.net>
2016-08-01 22:04:56 -0700

    Constraint Emscripten publish step to Ubuntu runs. More detailed comments for public step and for unit-test retry logic. Reordered caching steps alphabetically.

---
+++ scripts/release_emscripten.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+#------------------------------------------------------------------------------
+# Bash script which calls the Emscripten "publish binary" script on Ubuntu
+# and does nothing on macOS.  We should probably merge these two scripts in
+# the near future.
+#
+# The documentation for solidity is hosted at:
+#
+#     https://solidity.readthedocs.org
+#
+# ------------------------------------------------------------------------------
+# This file is part of solidity.
+#
+# solidity is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# solidity is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with solidity.  If not, see <http://www.gnu.org/licenses/>
+#
+# (c) 2016 solidity contributors.
+#------------------------------------------------------------------------------
+
+if [[ "$OSTYPE" != "darwin"* ]]; then
+    ./scripts/travis-emscripten/publish_binary.sh
+fi
