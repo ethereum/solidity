ae638bed55bbbc601604e4475c6d57d50d3c8548
Yoichi Hirai <i@yoichihirai.com>
2016-09-08 15:03:32 +0200

    Extend `toFormalType()` function to deal with address types
    Fixes #1062

--- libsolidity/formal/Why3Translator.cpp
+++ libsolidity/formal/Why3Translator.cpp
@@ -73,6 +73,8 @@ string Why3Translator::toFormalType(Type const& _type) const
 		return "bool";
 	else if (auto type = dynamic_cast<IntegerType const*>(&_type))
 	{
+		if (type->isAddress())
+			return "Address.address";
 		if (!type->isAddress() && !type->isSigned() && type->numBits() == 256)
 			return "uint256";
 	}
@@ -130,6 +132,7 @@ bool Why3Translator::visit(ContractDefinition const& _contract)
 	addLine("use import int.ComputerDivision");
 	addLine("use import mach.int.Unsigned");
 	addLine("use import UInt256");
+	addLine("use Address"); // `import` would cause name clashes with Uint256.
 	addLine("exception Revert");
 	addLine("exception Return");
 
