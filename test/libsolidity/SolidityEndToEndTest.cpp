ab3d1b024db6e208e4b63e2ecb07754af1540d6f
Yoichi Hirai <i@yoichihirai.com>
2016-10-14 17:32:13 +0200

    Add tests around calling functions returning functions returning functions

--- test/libsolidity/SolidityEndToEndTest.cpp
+++ test/libsolidity/SolidityEndToEndTest.cpp
@@ -7721,6 +7721,36 @@ BOOST_AUTO_TEST_CASE(function_type_library_internal)
 }
 
 
+BOOST_AUTO_TEST_CASE(call_function_returning_function)
+{
+	char const* sourceCode = R"(
+		contract test {
+			function f0() returns (uint) {
+				return 2;
+			}
+			function f1() returns (function() returns (uint)) {
+				returns f0;
+			}
+			function f2() returns (function() returns (function () returns (uint))) {
+				returns f1;
+			}
+			function f3() returns (function() returns (function () returns (function () returns (uint))))
+			{
+				returns f2;
+			}
+			function f() returns (uint) {
+				function() returns(function() returns(function() returns(function() returns(uint)))) x;
+				x = f3;
+				return x()()()();
+			}
+		}
+	)";
+
+	compileAndRun(sourceCode, 0, "C");
+	BOOST_CHECK(callContractFunction("f()") == encodeArgs(u256(2)));
+}
+
+
 // TODO: arrays, libraries with external functions
 
 BOOST_AUTO_TEST_CASE(shift_constant_left)
