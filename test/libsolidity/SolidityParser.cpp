b87d44e6702d723cc5b9778b638623d855eecb6a
Yoichi Hirai <i@yoichihirai.com>
2016-10-14 17:32:13 +0200

    Add tests around calling functions returning functions returning functions

--- test/libsolidity/SolidityParser.cpp
+++ test/libsolidity/SolidityParser.cpp
@@ -1311,6 +1311,21 @@ BOOST_AUTO_TEST_CASE(function_type_as_parameter)
 	BOOST_CHECK(successParse(text));
 }
 
+BOOST_AUTO_TEST_CASE(calling_function)
+{
+	char const* text = R"(
+		contract test {
+			function f() {
+				function() returns(function() returns(function() returns(function() returns(uint)))) x;
+				uint y;
+				y = x()()()();
+			}
+		}
+	)";
+	BOOST_CHECK(successParse(text));
+}
+
+
 BOOST_AUTO_TEST_SUITE_END()
 
 }
