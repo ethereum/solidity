08015590f23957153e12f06a2bcba6e9246733bc
Yoichi Hirai <i@yoichihirai.com>
2016-10-18 14:52:27 +0200

    analysis: Allow multiple events of the same name
    Fixes #1215

--- libsolidity/analysis/DeclarationContainer.cpp
+++ libsolidity/analysis/DeclarationContainer.cpp
@@ -58,6 +58,13 @@ Declaration const* DeclarationContainer::conflictingDeclaration(
 			return declaration;
 		}
 	}
+	else if (dynamic_cast<EventDefinition const*>(&_declaration))
+	{
+		// check that all other declarations with the same name are events
+		for (Declaration const* declaration: declarations)
+			if (!dynamic_cast<EventDefinition const*>(declaration))
+				return declaration;
+	}
 	else if (declarations.size() == 1 && declarations.front() == &_declaration)
 		return nullptr;
 	else if (!declarations.empty())
