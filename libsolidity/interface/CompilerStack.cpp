db42294a20ad65710f202b731b9e96a3caa2b2e4
Danil Nemirovsky <danil.nemirovsky@gmail.com>
2016-10-20 23:07:03 +0300

    - fixed a misprint.

--- libsolidity/interface/CompilerStack.cpp
+++ libsolidity/interface/CompilerStack.cpp
@@ -145,7 +145,7 @@ bool CompilerStack::parse()
 		}
 	}
 	if (!Error::containsOnlyWarnings(m_errors))
-		// errors while parsing. sould stop before type checking
+		// errors while parsing. should stop before type checking
 		return false;
 
 	resolveImports();
