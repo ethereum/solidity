5a981b59c3519257027a107898c06693c126c7b5
Alex Beregszaszi <alex@rtfs.hu>
2016-10-26 01:10:55 +0100

    LLL: replace (1 0 sub) with literal value

--- liblll/CodeFragment.cpp
+++ liblll/CodeFragment.cpp
@@ -547,9 +547,7 @@ void CodeFragment::constructOperation(sp::utree const& _t, CompilerState& _s)
 			requireDeposit(0, 1);
 
 			m_asm.append(code[0].m_asm, 1);
-			m_asm.append((u256)1);
-			m_asm.append((u256)0);
-			m_asm.append(Instruction::SUB);
+			m_asm.append(bigint(u256(0) - 1);
 			m_asm.append(Instruction::SUB);
 		}
 		else if (us == "SEQ")
