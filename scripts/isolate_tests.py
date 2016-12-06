72f9a4a73eba906f349ccb3e577899d0f9add963
Federico Bond <federicobond@gmail.com>
2016-12-06 19:21:38 -0300

    Cleanup and fix scripts/isolate_tests.py

--- scripts/isolate_tests.py
+++ scripts/isolate_tests.py
@@ -4,21 +4,41 @@
 # multi-line strings into individual files.
 # This can be used to extract the Solidity test cases
 # into files for e.g. fuzz testing as
-# scripts/isolateTests.py tests/libsolidity/SolidityEndToEndTest.cpp
+# scripts/isolate_tests.py test/libsolidity/*
 
 import sys
-lines = sys.stdin.read().split('\n')
-inside = False
-tests = []
-for l in lines:
-  if inside:
-    if l.strip().endswith(')";'):
-      inside = False
-    else:
-      tests[-1] += l + '\n'
-  else:
-    if l.strip().endswith('R"('):
-      inside = True
-      tests += ['']
-for i in range(len(tests)):
-  open('test%d.sol' % i, 'w').write(tests[i])
+
+
+def extract_cases(path):
+    lines = open(path).read().splitlines()
+
+    inside = False
+    tests = []
+
+    for l in lines:
+      if inside:
+        if l.strip().endswith(')";'):
+          inside = False
+        else:
+          tests[-1] += l + '\n'
+      else:
+        if l.strip().endswith('R"('):
+          inside = True
+          tests += ['']
+
+    return tests
+
+
+def write_cases(tests, start=0):
+    for i, test in enumerate(tests, start=start):
+        open('test%d.sol' % i, 'w').write(test)
+
+
+if __name__ == '__main__':
+    files = sys.argv[1:]
+
+    i = 0
+    for path in files:
+        cases = extract_cases(path)
+        write_cases(cases, start=i)
+        i += len(cases)
