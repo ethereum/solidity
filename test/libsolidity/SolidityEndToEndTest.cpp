932e7887bde0ac84b86c257d321a48647b024e6d
Yoichi Hirai <i@yoichihirai.com>
2016-12-13 16:35:42 +0100

    test: add tests that tries different types on <<=

--- test/libsolidity/SolidityEndToEndTest.cpp
+++ test/libsolidity/SolidityEndToEndTest.cpp
@@ -8540,6 +8540,24 @@ BOOST_AUTO_TEST_CASE(shift_left_assignment)
 	BOOST_CHECK(callContractFunction("f(uint256,uint256)", u256(0x4266), u256(256)) == encodeArgs(u256(0)));
 }
 
+BOOST_AUTO_TEST_CASE(shift_left_assignment_different_type)
+{
+	char const* sourceCode = R"(
+		contract C {
+			function f(uint a, uint8 b) returns (uint) {
+				a <<= b;
+				return a;
+			}
+		}
+	)";
+	compileAndRun(sourceCode, 0, "C");
+	BOOST_CHECK(callContractFunction("f(uint256,uint256)", u256(0x4266), u256(0)) == encodeArgs(u256(0x4266)));
+	BOOST_CHECK(callContractFunction("f(uint256,uint256)", u256(0x4266), u256(8)) == encodeArgs(u256(0x426600)));
+	BOOST_CHECK(callContractFunction("f(uint256,uint256)", u256(0x4266), u256(16)) == encodeArgs(u256(0x42660000)));
+	BOOST_CHECK(callContractFunction("f(uint256,uint256)", u256(0x4266), u256(17)) == encodeArgs(u256(0x84cc0000)));
+	BOOST_CHECK(callContractFunction("f(uint256,uint256)", u256(0x4266), u256(240)) == fromHex("4266000000000000000000000000000000000000000000000000000000000000"));
+}
+
 BOOST_AUTO_TEST_CASE(shift_right)
 {
 	char const* sourceCode = R"(
