946a63c26f0c03c31c6342acc65d099ffdc3691f
chriseth <c@ethdev.com>
2017-01-25 17:27:01 +0100

    Add test for assignment.

--- test/libsolidity/InlineAssembly.cpp
+++ test/libsolidity/InlineAssembly.cpp
@@ -182,10 +182,16 @@ BOOST_AUTO_TEST_CASE(error_tag)
 	BOOST_CHECK(successAssemble("{ invalidJumpLabel }"));
 }
 
-BOOST_AUTO_TEST_CASE(inline_assembly_shadowed_instruction)
+BOOST_AUTO_TEST_CASE(inline_assembly_shadowed_instruction_declaration)
 {
 	// Error message: "Cannot use instruction names for identifier names."
 	BOOST_CHECK(!successAssemble("{ let gas := 1 }"));
+}
+
+BOOST_AUTO_TEST_CASE(inline_assembly_shadowed_instruction_assignment)
+{
+	// Error message: "Cannot use instruction names for identifier names."
+	BOOST_CHECK(!successAssemble("{ 2 =: gas }"));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
