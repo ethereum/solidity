e9ae50dc5994a02119655eff4ed488fb3918d41f
RJ Catalano <rj@monax.io>
2017-02-09 11:36:08 -0600

    clarify branches for docker to push on and clarify where to find cmakelists.txt
    Signed-off-by: RJ Catalano <rj@monax.io>

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
-version=$(grep -oP "PROJECT_VERSION \"?\K[0-9.]+(?=\")"? $(dirname "$0")/CMakeLists.txt)
+version=$(grep -oP "PROJECT_VERSION \"?\K[0-9.]+(?=\")"? $(dirname "$0")/../CMakeLists.txt)
 if [ "$TRAVIS_BRANCH" == "develop" ]; then
 	docker tag ethereum/solc:build ethereum/solc:nightly;
 	docker tag ethereum/solc:build ethereum/solc:nightly-"$version"-"$TRAVIS_COMMIT"
@@ -9,9 +9,8 @@ if [ "$TRAVIS_BRANCH" == "develop" ]; then
 	docker push ethereum/solc:nightly;
 elif [ "$TRAVIS_BRANCH" == "release"]; then
 	docker tag ethereum/solc:build ethereum/solc:stable;
-	docker tag ethereum/solc:build ethereum/solc:"$version";
-	docker tag ethereum/solc:build ethereum/solc:
-	docker push ethereum/solc;
-	docker push ethereum/solc:"$version";
 	docker push ethereum/solc:stable;
+elif [ "$TRAVIS_BRANCH" == v"$version"]; then
+	docker tag ethereum/solc:build ethereum/solc:"$version";
+	docker push ethereum/solc:"$version";
 fi
