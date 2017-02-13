bc3e3fd7090def8f4da81c44b31a86c1fda19414
chriseth <c@ethdev.com>
2017-02-13 12:28:39 +0100

    Fix test expressions.

--- scripts/docker_deploy.sh
+++ scripts/docker_deploy.sh
@@ -4,15 +4,20 @@ set -e
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
 version=$(grep -oP "PROJECT_VERSION \"?\K[0-9.]+(?=\")"? $(dirname "$0")/../CMakeLists.txt)
-if [ "$TRAVIS_BRANCH" == "develop" ]; then
-	docker tag ethereum/solc:build ethereum/solc:nightly;
-	docker tag ethereum/solc:build ethereum/solc:nightly-"$version"-"$TRAVIS_COMMIT"
-	docker push ethereum/solc:nightly-"$version"-"$TRAVIS_COMMIT";
-	docker push ethereum/solc:nightly;
-elif [ "$TRAVIS_BRANCH" == "release"]; then
-	docker tag ethereum/solc:build ethereum/solc:stable;
-	docker push ethereum/solc:stable;
-elif [ "$TRAVIS_TAG" == v"$version"]; then
-	docker tag ethereum/solc:build ethereum/solc:"$version";
-	docker push ethereum/solc:"$version";
+if [ "$TRAVIS_BRANCH" = "develop" ]
+then
+    docker tag ethereum/solc:build ethereum/solc:nightly;
+    docker tag ethereum/solc:build ethereum/solc:nightly-"$version"-"$TRAVIS_COMMIT"
+    docker push ethereum/solc:nightly-"$version"-"$TRAVIS_COMMIT";
+    docker push ethereum/solc:nightly;
+elif [ "$TRAVIS_BRANCH" = "release" ]
+then
+    docker tag ethereum/solc:build ethereum/solc:stable;
+    docker push ethereum/solc:stable;
+elif [ "$TRAVIS_TAG" = v"$version" ]
+then
+    docker tag ethereum/solc:build ethereum/solc:"$version";
+    docker push ethereum/solc:"$version";
+else
+    echo "Not publishing docker image from branch $TRAVIS_BRANCH or tag $TRAVIS_TAG"
 fi
