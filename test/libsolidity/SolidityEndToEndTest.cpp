b28be08b6d1aeb642fd35e32e2b7627e4eb7cd38
Alex Beregszaszi <alex@rtfs.hu>
2017-03-15 13:00:58 +0000

    Add tests for negative base

--- test/libsolidity/SolidityEndToEndTest.cpp
+++ test/libsolidity/SolidityEndToEndTest.cpp
@@ -9189,12 +9189,24 @@ BOOST_AUTO_TEST_CASE(scientific_notation)
 			function h() returns (uint) {
 				return 2.5e1;
 			}
+			function i() returns (int) {
+				return -2e10;
+			}
+			function j() returns (int) {
+				return -200e-2;
+			}
+			function k() returns (int) {
+				return -2.5e1;
+			}
 		}
 	)";
 	compileAndRun(sourceCode, 0, "C");
 	BOOST_CHECK(callContractFunction("f()") == encodeArgs(u256(20000000000)));
 	BOOST_CHECK(callContractFunction("g()") == encodeArgs(u256(2)));
 	BOOST_CHECK(callContractFunction("h()") == encodeArgs(u256(25)));
+	BOOST_CHECK(callContractFunction("i()") == encodeArgs(u256(-20000000000)));
+	BOOST_CHECK(callContractFunction("j()") == encodeArgs(u256(-2)));
+	BOOST_CHECK(callContractFunction("k()") == encodeArgs(u256(-25)));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
