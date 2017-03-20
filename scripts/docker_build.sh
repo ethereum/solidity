73efdabd377f3118a17fe4cbac4f3156bdc3c2c8
chriseth <chris@ethereum.org>
2017-03-20 14:03:11 +0100

    Rename solc binary.

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -5,4 +5,4 @@ set -e
 docker build -t ethereum/solc:build -f scripts/Dockerfile .
 tmp_container=$(docker create ethereum/solc:build sh)
 mkdir -p upload
-docker cp ${tmp_container}:/usr/bin/solc upload/
+docker cp ${tmp_container}:/usr/bin/solc upload/solc-static-linux
