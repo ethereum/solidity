a02cf83d86489f5d9c9439c79e27fcecb5c5ddb4
Alex Beregszaszi <alex@rtfs.hu>
2017-08-15 14:36:05 +0100

    Update std to contain visibility specifiers

--- std/StandardToken.sol
+++ std/StandardToken.sol
@@ -8,24 +8,24 @@ contract StandardToken is Token {
 	mapping (address =>
 		mapping (address => uint256)) m_allowance;
 
-	function StandardToken(address _initialOwner, uint256 _supply) {
+	function StandardToken(address _initialOwner, uint256 _supply) public {
 		supply = _supply;
 		balance[_initialOwner] = _supply;
 	}
 
-	function balanceOf(address _account) constant returns (uint) {
+	function balanceOf(address _account) constant public returns (uint) {
 		return balance[_account];
 	}
 
-	function totalSupply() constant returns (uint) {
+	function totalSupply() constant public returns (uint) {
 		return supply;
 	}
 
-	function transfer(address _to, uint256 _value) returns (bool success) {
+	function transfer(address _to, uint256 _value) public returns (bool success) {
 		return doTransfer(msg.sender, _to, _value);
 	}
 
-	function transferFrom(address _from, address _to, uint256 _value) returns (bool) {
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
 		if (m_allowance[_from][msg.sender] >= _value) {
 			if (doTransfer(_from, _to, _value)) {
 				m_allowance[_from][msg.sender] -= _value;
@@ -47,13 +47,13 @@ contract StandardToken is Token {
 		}
 	}
 
-	function approve(address _spender, uint256 _value) returns (bool success) {
+	function approve(address _spender, uint256 _value) public returns (bool success) {
 		m_allowance[msg.sender][_spender] = _value;
 		Approval(msg.sender, _spender, _value);
 		return true;
 	}
 
-	function allowance(address _owner, address _spender) constant returns (uint256) {
+	function allowance(address _owner, address _spender) constant public returns (uint256) {
 		return m_allowance[_owner][_spender];
 	}
 }
--- std/mortal.sol
+++ std/mortal.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.4.0;
 import "./owned.sol";
 
 contract mortal is owned {
-    function kill() {
+    function kill() public {
         if (msg.sender == owner)
             selfdestruct(owner);
     }
