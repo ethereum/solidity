48e0e3b97faaafff4da7687af1d5a25d2d7d1363
Paweł Bylica <chfast@gmail.com>
2017-09-14 23:13:37 +0200

    Circle CI: Emscripten build

--- scripts/travis-emscripten/install_deps.sh
+++ scripts/travis-emscripten/install_deps.sh
@@ -31,10 +31,8 @@ set -ev
 
 echo -en 'travis_fold:start:installing_dependencies\\r'
 test -e boost_1_57_0 -a -e boost_1_57_0/boost || (
-wget 'http://downloads.sourceforge.net/project/boost/boost/'\
-'1.57.0/boost_1_57_0.tar.bz2?r=http%3A%2F%2Fsourceforge.net%2F'\
-'projects%2Fboost%2Ffiles%2Fboost%2F1.57.0%2F&ts=1421887207'\
- -O - | tar xj
+wget 'https://sourceforge.net/projects/boost/files/boost/1.57.0/boost_1_57_0.tar.gz/download'\
+ -O - | tar xz
 cd boost_1_57_0
 ./bootstrap.sh --with-toolset=gcc --with-libraries=thread,system,regex,date_time,chrono,filesystem,program_options,random
 )
