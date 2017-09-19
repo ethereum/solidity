4876842f69e9d59d102f8f26f4bb2f1fc37b4d36
Alex Beregszaszi <alex@rtfs.hu>
2017-09-19 13:39:59 +0100

    Replace fixed-version pragmas in Gnosis

--- test/externalTests.sh
+++ test/externalTests.sh
@@ -54,6 +54,10 @@ function test_truffle
         sed -i -e 's/CappedCrowdsale.new(this.startTime, this.endTime, rate, wallet, 0)/CappedCrowdsale.new(this.startTime, this.endTime, rate, wallet, 0, this.token.address)/' test/crowdsale/CappedCrowdsale.test.js
         sed -i -e 's/RefundableCrowdsale.new(this.startTime, this.endTime, rate, wallet, 0, { from: owner })/RefundableCrowdsale.new(this.startTime, this.endTime, rate, wallet, 0, this.token.address, { from: owner })/' test/crowdsale/RefundableCrowdsale.test.js
       fi
+      if [ "$name" == "Gnosis" ]; then
+        # Replace fixed-version pragmas in Gnosis (part of Consensys best practice)
+        find contracts test -name '*.sol' -type f -print0 | xargs -0 sed -i -e 's/pragma solidity 0/pragma solidity ^0/'
+      fi
       npm run test
     )
     rm -rf "$DIR"
