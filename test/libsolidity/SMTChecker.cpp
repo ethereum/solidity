52cac27aecae8cc2994b5e8c4889fa8f0c693f20
chriseth <chris@ethereum.org>
2017-10-04 21:21:17 +0200

    Test for multi-branches.

--- test/libsolidity/SMTChecker.cpp
+++ test/libsolidity/SMTChecker.cpp
@@ -241,6 +241,24 @@ BOOST_AUTO_TEST_CASE(branches_assert_condition)
 		}
 	)";
 	CHECK_SUCCESS_NO_WARNINGS(text);
+	text = R"(
+		contract C {
+			function f(uint x) public pure {
+				if (x > 10) {
+					assert(x > 9);
+				}
+				else if (x > 2)
+				{
+					assert(x <= 10 && x > 2);
+				}
+				else
+				{
+				   assert(0 <= x && x <= 2);
+				}
+			}
+		}
+	)";
+	CHECK_SUCCESS_NO_WARNINGS(text);
 }
 
 BOOST_AUTO_TEST_CASE(ways_to_clear_variables)
