dcd55c5005468808607ebd92d84124c8949d9d17
Alex Beregszaszi <alex@rtfs.hu>
2017-11-23 12:13:00 +0000

    Add exponential notation to numbers in SolidityLexer

--- docs/utils/SolidityLexer.py
+++ docs/utils/SolidityLexer.py
@@ -73,9 +73,9 @@ class SolidityLexer(RegexLexer):
             (r'(block|msg|tx|now|suicide|selfdestruct|addmod|mulmod|sha3|keccak256|log[0-4]|'
              r'sha256|ecrecover|ripemd160|assert|revert|require)', Name.Builtin),
             (r'[$a-zA-Z_][a-zA-Z0-9_]*', Name.Other),
-            (r'[0-9][0-9]*\.[0-9]+([eE][0-9]+)?[fd]?', Number.Float),
+            (r'[0-9][0-9]*\.[0-9]+([eE][0-9]+)?', Number.Float),
             (r'0x[0-9a-fA-F]+', Number.Hex),
-            (r'[0-9]+', Number.Integer),
+            (r'[0-9]+([eE][0-9]+)?', Number.Integer),
             (r'"(\\\\|\\"|[^"])*"', String.Double),
             (r"'(\\\\|\\'|[^'])*'", String.Single),
         ]
