793537e08926190728e93a666a5fadc6d0765f63
Alex Beregszaszi <alex@rtfs.hu>
2017-12-05 10:45:44 +0000

    Suggest the "if" statement too instead of jumps

--- libsolidity/inlineasm/AsmAnalysis.cpp
+++ libsolidity/inlineasm/AsmAnalysis.cpp
@@ -529,6 +529,6 @@ void AsmAnalyzer::warnOnInstructions(solidity::Instruction _instr, SourceLocatio
 			_location,
 			"Jump instructions and labels are low-level EVM features that can lead to "
 			"incorrect stack access. Because of that they are discouraged. "
-			"Please consider using \"switch\" or \"for\" statements instead."
+			"Please consider using \"switch\", \"if\" or \"for\" statements instead."
 		);
 }
