89a8f4096c6f1cc1025d4026ebfc7b2674330458
chriseth <chris@ethereum.org>
2018-02-20 19:30:30 +0100

    Provide proper input for test call.

--- test/libsolidity/SolidityEndToEndTest.cpp
+++ test/libsolidity/SolidityEndToEndTest.cpp
@@ -3565,8 +3565,8 @@ BOOST_AUTO_TEST_CASE(library_call_protection)
 		}
 	)";
 	compileAndRun(sourceCode, 0, "Lib");
-	ABI_CHECK(callContractFunction("np(Lib.S storage)"), encodeArgs());
-	ABI_CHECK(callContractFunction("v(Lib.S storage)"), encodeArgs(u160(m_sender)));
+	ABI_CHECK(callContractFunction("np(Lib.S storage)", 0), encodeArgs());
+	ABI_CHECK(callContractFunction("v(Lib.S storage)", 0), encodeArgs(u160(m_sender)));
 	ABI_CHECK(callContractFunction("pu()"), encodeArgs(2));
 	compileAndRun(sourceCode, 0, "Test", bytes(), map<string, Address>{{"Lib", m_contractAddress}});
 	ABI_CHECK(callContractFunction("s()"), encodeArgs(0));
