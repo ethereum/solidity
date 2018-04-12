4b6b19187147342a9c2b9774ffb0b58f7179dc85
Raphael Norwitz <raphael.s.norwitz@gmail.com>
2018-04-12 17:30:31 -0400

    Made some asthetic changes for compliance with coding guidelines

--- libsolidity/interface/ftime.cpp
+++ libsolidity/interface/ftime.cpp
@@ -1,9 +1,33 @@
-#include "ftime.h"
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @author Killian <>
+ * @author Jiayang <>
+ * @author Raphael <raphael.s.norwitz@gmail.com>
+ * @date 2017
+ * A timing utility for debugging compiler performance
+ */
 #include <stdexcept>
 #include <iostream>
 #include <iomanip>
 #include <chrono>
 #include <ctime>
+
+#include "ftime.h"
 
 using namespace std;
 
@@ -27,23 +51,31 @@ void TimeNodeStack::push(string name)
 
 void TimeNodeStack::pop()
 {
-	if (stack.size() > 1) {
+	if (stack.size() > 1)
+	{
 		TimeNode t_node = stack[stack.size() - 1];
 		stack.pop_back();
 		t_node.end = std::chrono::high_resolution_clock::now();
 		stack[stack.size() - 1].children.push_back(t_node);
-	} else if (stack.size() == 1) {
+	}
+	else if (stack.size() == 1)
+	{
 		stack[0].end = std::chrono::high_resolution_clock::now();
-                if(print_flag){
+                if(print_flag)
+		{
                         print_recursive(stack[0], string(""));
                         stack.pop_back();
-                } else {
+                }
+		else
+		{
                         print_stack.push_back(stack[0]);
                         stack.pop_back();
                 }
                 //print();
                 //stack.pop_back();
-	} else {
+	}
+	else
+	{
 		throw runtime_error("error");
 	}
 }
@@ -51,15 +83,21 @@ void TimeNodeStack::pop()
 void TimeNodeStack::print_recursive(const TimeNode& x, const string& arrow)
 {
 	cout << setw(70) << left << arrow + x.name << setw(20) << left << 
-                std::chrono::duration_cast<std::chrono::microseconds>(x.begin - start).count() << 
-                setw(20) << left << std::chrono::duration_cast<std::chrono::microseconds>(x.end - x.begin).count() << 
-                '\n';
+                std::chrono::duration_cast<std::chrono::microseconds>(
+		x.begin - start).count() << setw(20) << left << 
+		std::chrono::duration_cast<std::chrono::microseconds>(x.end
+				- x.begin).count() << '\n';
 
-	for (TimeNode child : x.children) {
-		if (arrow.length() == 0) {
+	for (TimeNode child : x.children)
+	{
+		if (arrow.length() == 0)
+		{
 			print_recursive(child, " \\_");
-		} else {
-			print_recursive(child, arrow.substr(0, arrow.length() - 2) + "    " + "\\_");
+		}
+		else
+		{
+			print_recursive(child, arrow.substr(0, arrow.length() - 2) + 
+					"    " + "\\_");
 		}
 	}
 }
--- libsolidity/interface/ftime.h
+++ libsolidity/interface/ftime.h
@@ -1,32 +1,53 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @author Killian <>
+ * @author Jiayang <>
+ * @author Raphael <raphael.s.norwitz@gmail.com>
+ * @date 2017
+ * A timing utility for debugging compiler performance
+ */
 #include <chrono>
 #include <string>
 #include <vector>
-
-using namespace std;
 
 
 class TimeNode
 {
 public:
 	TimeNode();
-	string name;
+	std::string name;
 	std::chrono::high_resolution_clock::time_point begin;
 	std::chrono::high_resolution_clock::time_point end;
-	vector<TimeNode> children;
+	std::vector<TimeNode> children;
 };
 
 class TimeNodeStack
 {
 public:
 	TimeNodeStack();
-	void push(string name);
+	void push(std::string name);
 	void pop();
 	void print();
-	void print_recursive(const TimeNode& x, const string& arrow);
+	void print_recursive(const TimeNode& x, const std::string& arrow);
         bool print_flag = false;
 private:
-	vector<TimeNode> stack;
-        vector<TimeNode> print_stack;
+	std::vector<TimeNode> stack;
+	std::vector<TimeNode> print_stack;
 	std::chrono::high_resolution_clock::time_point start;
 };
 
