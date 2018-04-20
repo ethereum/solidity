65345162b4217c7d3f9035ee8d2f4c3fa2ecb72a
Alex Beregszaszi <alex@rtfs.hu>
2018-04-20 12:00:35 +0100

    Update tight packing test cases

--- test/libsolidity/syntaxTests/tight_packing_literals_fine.sol
+++ test/libsolidity/syntaxTests/tight_packing_literals_fine.sol
@@ -2,10 +2,21 @@ contract C {
     function f() pure public returns (bytes32) {
         return keccak256(uint8(1));
     }
-    function g() pure public returns (bytes) {
-        return abi.encode(1);
+    function g() pure public returns (bytes32) {
+        return sha3(uint8(1));
     }
-    function h() pure public returns (bytes) {
+    function h() pure public returns (bytes32) {
+        return sha256(uint8(1));
+    }
+    function j() pure public returns (bytes32) {
+        return ripemd160(uint8(1));
+    }
+    function k() pure public returns (bytes) {
         return abi.encodePacked(uint8(1));
     }
+    function l() pure public returns (bytes) {
+        return abi.encode(1);
+    }
 }
+// ----
+// Warning: (168-182): "sha3" has been deprecated in favour of "keccak256"
