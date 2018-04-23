1ac0090f31d12a720950991ab4e85710877effd1
Alex Beregszaszi <alex@rtfs.hu>
2018-04-23 14:54:45 +0100

    The "year" denomination is deprecated

--- test/libsolidity/syntaxTests/denominations/denominations.sol
+++ test/libsolidity/syntaxTests/denominations/denominations.sol
@@ -3,3 +3,5 @@ contract C {
 	uint constant b = 1 seconds + 2 minutes + 3 hours + 4 days + 5 weeks + 6 years;
 	uint constant c = 2 szabo / 1 seconds + 3 finney * 3 hours;
 }
+// ----
+// Warning: (142-149): Using "years" as a unit denomination is deprecated.
---
+++ test/libsolidity/syntaxTests/denominations/deprecated_year.sol
@@ -0,0 +1,5 @@
+contract C {
+	uint constant a = 3 years;
+}
+// ----
+// Warning: (32-39): Using "years" as a unit denomination is deprecated.
