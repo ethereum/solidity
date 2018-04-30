e0b178e1c51fad4c405e79e1661a6e4692f17ee8
Alex Beregszaszi <alex@rtfs.hu>
2018-04-30 12:23:30 +0100

    Disable FixedBytesType(0) aka bytes0

--- libsolidity/ast/Types.cpp
+++ libsolidity/ast/Types.cpp
@@ -1283,8 +1283,10 @@ shared_ptr<FixedBytesType> FixedBytesType::smallestTypeForLiteral(string const& 
 
 FixedBytesType::FixedBytesType(int _bytes): m_bytes(_bytes)
 {
-	solAssert(m_bytes >= 0 && m_bytes <= 32,
-			  "Invalid byte number for fixed bytes type: " + dev::toString(m_bytes));
+	solAssert(
+		m_bytes > 0 && m_bytes <= 32,
+		"Invalid byte number for fixed bytes type: " + dev::toString(m_bytes)
+	);
 }
 
 bool FixedBytesType::isImplicitlyConvertibleTo(Type const& _convertTo) const
