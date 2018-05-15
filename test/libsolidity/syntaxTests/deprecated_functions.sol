203475ef020d8bf0c0334e51580dc798185b2359
chriseth <chris@ethereum.org>
2018-05-15 12:53:23 +0200

    Adjust tests.

--- test/libsolidity/syntaxTests/deprecated_functions.sol
+++ test/libsolidity/syntaxTests/deprecated_functions.sol
@@ -1,6 +1,6 @@
 contract test {
 	function f() pure public {
-		bytes32 x = sha3(uint8(1));
+		bytes32 x = sha3();
 		x;
 	}
 	function g() public {
@@ -8,5 +8,5 @@ contract test {
 	}
 }
 // ----
-// Warning: (58-72): "sha3" has been deprecated in favour of "keccak256"
-// Warning: (107-117): "suicide" has been deprecated in favour of "selfdestruct"
+// Warning: (58-64): "sha3" has been deprecated in favour of "keccak256"
+// Warning: (99-109): "suicide" has been deprecated in favour of "selfdestruct"
--- test/libsolidity/syntaxTests/deprecated_functions_050.sol
+++ test/libsolidity/syntaxTests/deprecated_functions_050.sol
@@ -10,4 +10,5 @@ contract test {
 }
 // ----
 // TypeError: (88-102): "sha3" has been deprecated in favour of "keccak256"
+// TypeError: (88-102): This function only accepts a single "bytes" argument. Please use "abi.encodePacked(...)" or a similar function to encode the data. The provided argument of type uint8 is not implicitly convertible to expected type bytes memory.
 // TypeError: (137-147): "suicide" has been deprecated in favour of "selfdestruct"
