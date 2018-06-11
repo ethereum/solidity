80b7d361873f239fef30d10a10c73e724eecff3e
Alex Beregszaszi <alex@rtfs.hu>
2018-06-11 18:10:24 +0100

    Remove non-0.5.0 warning for emit keyword (make it mandatory)

--- test/libsolidity/syntaxTests/types/empty_tuple_event.sol
+++ test/libsolidity/syntaxTests/types/empty_tuple_event.sol
@@ -2,9 +2,9 @@ pragma solidity ^0.4.3;
 contract C {
     event SomeEvent();
     function a() public {
-        (SomeEvent(), 7);
+        (emit SomeEvent(), 7);
     }
 }
 // ----
-// Warning: (95-106): Invoking events without "emit" prefix is deprecated.
+// TypeError: (95-106): Event invocations have to be prefixed by "emit".
 // Warning: (95-106): Tuple component cannot be empty.
