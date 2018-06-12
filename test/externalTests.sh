dae790b118d4c7b084d5cf0b0716af182c09509f
Alex Beregszaszi <alex@rtfs.hu>
2018-06-12 19:11:56 +0100

    Execute external Zeppelin tests before Gnosis

--- test/externalTests.sh
+++ test/externalTests.sh
@@ -57,5 +57,5 @@ function test_truffle
 }
 
 # Using our temporary fork here. Hopefully to be merged into upstream after the 0.5.0 release.
-test_truffle Gnosis https://github.com/axic/pm-contracts.git -b solidity-050
 test_truffle Zeppelin https://github.com/axic/openzeppelin-solidity.git -b solidity-050
+test_truffle Gnosis https://github.com/axic/pm-contracts.git -b solidity-050
