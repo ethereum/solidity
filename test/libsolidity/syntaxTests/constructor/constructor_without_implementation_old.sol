de6cd2425b6ea9cdf2a7b7f49cd88e4f18fa20e3
Erik Kundt <bitshift@posteo.org>
2018-06-29 17:05:23 +0200

    Adjusts syntax tests to new constructor syntax.

--- test/libsolidity/syntaxTests/constructor/constructor_without_implementation_old.sol
+++
@@ -1,6 +0,0 @@
-contract C {
-	function C() public;
-}
-// ----
-// Warning: (14-34): Defining constructors as functions with the same name as the contract is deprecated. Use "constructor(...) { ... }" instead.
-// TypeError: (14-34): Constructor must be implemented if declared.
