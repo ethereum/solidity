0a738b4ac4b4b79654531061f1d11885655e86e8
Daniel Kirchner <daniel@ekpyron.org>
2018-07-03 10:43:56 +0200

    Adjust tests wrt strict view pure checker.

--- test/libsolidity/syntaxTests/viewPureChecker/write_storage_fail.sol
+++ test/libsolidity/syntaxTests/viewPureChecker/write_storage_fail.sol
@@ -3,4 +3,4 @@ contract C {
     function f() view public { x = 2; }
 }
 // ----
-// Warning: (56-57): Function declared as view, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
+// TypeError: (56-57): Function declared as view, but this expression (potentially) modifies the state and thus requires non-payable (the default) or payable.
