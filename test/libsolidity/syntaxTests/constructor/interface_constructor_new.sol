3f53db402f39516f4d89ed39f54a49dc11d89c65
Erik Kundt <bitshift@posteo.org>
2018-07-04 15:15:30 +0200

    Fixes wrong visibility specifier in interface constructor test.

--- test/libsolidity/syntaxTests/constructor/interface_constructor_new.sol
+++ test/libsolidity/syntaxTests/constructor/interface_constructor_new.sol
@@ -1,7 +1,7 @@
 interface I {
-	constructor() public;
+	constructor() external;
 }
 // ----
-// Warning: (15-36): Functions in interfaces should be declared external.
-// TypeError: (15-36): Constructor cannot be defined in interfaces.
-// TypeError: (15-36): Constructor must be implemented if declared.
+// TypeError: (15-38): Constructor must be public or internal.
+// TypeError: (15-38): Constructor cannot be defined in interfaces.
+// TypeError: (15-38): Constructor must be implemented if declared.
