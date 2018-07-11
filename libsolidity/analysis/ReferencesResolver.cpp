a1f54f4e40811e526541f3a54f6a19dd42c8e688
Daniel Kirchner <daniel@ekpyron.org>
2018-07-11 13:23:16 +0200

    Require storage location.

--- libsolidity/analysis/ReferencesResolver.cpp
+++ libsolidity/analysis/ReferencesResolver.cpp
@@ -377,19 +377,10 @@ void ReferencesResolver::endVisit(VariableDeclaration const& _variable)
 					{
 						typeLoc = DataLocation::Storage;
 						if (_variable.isLocalVariable())
-						{
-							if (_variable.sourceUnit().annotation().experimentalFeatures.count(ExperimentalFeature::V050))
-								typeError(
-									_variable.location(),
-									"Data location must be specified as either \"memory\" or \"storage\"."
-								);
-							else
-								m_errorReporter.warning(
-									_variable.location(),
-									"Variable is declared as a storage pointer. "
-									"Use an explicit \"storage\" keyword to silence this warning."
-								);
-						}
+							typeError(
+								_variable.location(),
+								"Data location must be specified as either \"memory\" or \"storage\"."
+							);
 					}
 				}
 				else
