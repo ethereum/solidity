e7dc9d27fcd21479e897a499af415c906bf3f670
Erik Kundt <bitshift@posteo.org>
2018-07-11 13:26:52 +0200

    Cleans up syntax test.

--- test/libsolidity/syntaxTests/inheritance/base_arguments_empty_parentheses.sol
+++ test/libsolidity/syntaxTests/inheritance/base_arguments_empty_parentheses.sol
@@ -1,10 +1,7 @@
 contract Base {
   constructor(uint) public {}
 }
-contract Base2 {
-  constructor(uint, uint) public {}
-}
 contract Derived is Base(2) { }
 contract Derived2 is Base(), Derived() { }
 // ----
-// TypeError: (156-162): Wrong argument count for constructor call: 0 arguments given but expected 1. Remove parentheses if you do not want to provide arguments here.
+// TypeError: (101-107): Wrong argument count for constructor call: 0 arguments given but expected 1. Remove parentheses if you do not want to provide arguments here.
