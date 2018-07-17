b0b35e1e6b762d3849710054fdb2e1909f6780d9
Erik Kundt <bitshift@posteo.org>
2018-07-17 22:46:17 +0200

    Adds warning if function is shadowing a contract.

--- test/libsolidity/syntaxTests/constructor/constructor_old.sol
+++ test/libsolidity/syntaxTests/constructor/constructor_old.sol
@@ -1,3 +1,4 @@
 contract A { function A() public {} }
 // ----
 // SyntaxError: (13-35): Functions are not allowed to have the same name as the contract. If you intend this to be a constructor, use "constructor(...) { ... }" to define it.
+// Warning: (13-35): This declaration shadows an existing declaration.
