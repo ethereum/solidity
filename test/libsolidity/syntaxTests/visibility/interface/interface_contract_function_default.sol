05d0e5c8fa24f122c39f7e50d18f3f4d818665cf
Erik Kundt <bitshift@posteo.org>
2018-07-17 19:41:40 +0200

    Adds comment about reasoning behind syntax test.

--- test/libsolidity/syntaxTests/visibility/interface/interface_contract_function_default.sol
+++ test/libsolidity/syntaxTests/visibility/interface/interface_contract_function_default.sol
@@ -1,3 +1,5 @@
+// State of the syntax checker has to be reset after the interface
+// was visited. The suggested visibility for g() should not be external.
 interface I {
     function f();
 }
@@ -5,6 +7,6 @@ contract C {
     function g();
 }
 // ----
-// SyntaxError: (18-31): No visibility specified. Did you intend to add "external"?
-// SyntaxError: (51-64): No visibility specified. Did you intend to add "public"?
-// TypeError: (18-31): Functions in interfaces must be declared external.
+// SyntaxError: (158-171): No visibility specified. Did you intend to add "external"?
+// SyntaxError: (191-204): No visibility specified. Did you intend to add "public"?
+// TypeError: (158-171): Functions in interfaces must be declared external.
