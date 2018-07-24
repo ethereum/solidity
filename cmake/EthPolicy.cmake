1a64dd1d71d0f75becaccc44ad3cd68a45bef510
Paweł Bylica <chfast@gmail.com>
2018-07-24 17:07:11 +0200

    CMake: Better policy availability checking
    It's better to use if(POLICY ...) and we don't have to check for CMake 3.0 because this is the minimal required version.

--- cmake/EthPolicy.cmake
+++ cmake/EthPolicy.cmake
@@ -4,23 +4,16 @@ macro (eth_policy)
 	# link_directories() treats paths relative to the source dir.
 	cmake_policy(SET CMP0015 NEW)
 
-	if (${CMAKE_VERSION} VERSION_GREATER 3.0)
+	# Avoid warnings in CMake 3.0.2:
+	cmake_policy(SET CMP0042 NEW)
+	cmake_policy(SET CMP0043 NEW)
 
-		# Avoid warnings in CMake 3.0.2:
-		cmake_policy(SET CMP0042 NEW)
-		cmake_policy(SET CMP0043 NEW)
+	# allow VERSION argument in project()
+	cmake_policy(SET CMP0048 NEW)
 
-		# allow VERSION argument in project()
-		cmake_policy(SET CMP0048 NEW)
-
-	endif()
-
-	if (${CMAKE_VERSION} VERSION_GREATER 3.1)
-		
+	if (POLICY CMP0054)
 		# do not interpret if() arguments as variables!
 		cmake_policy(SET CMP0054 NEW)
-
 	endif()
-
 endmacro()
 
