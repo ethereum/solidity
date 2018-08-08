4b20708c497cfbbb8a489339b91e26f05d0bd7a2
Matías Aereal Aeón <aereal@gmail.com>
2018-08-08 11:46:17 -0300

    cmake flags to make solvers optional. Implementation of #4651

--- cmake/FindZ3.cmake
+++ cmake/FindZ3.cmake
@@ -1,7 +1,9 @@
-find_path(Z3_INCLUDE_DIR z3++.h)
-find_library(Z3_LIBRARY NAMES z3 )
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Z3 DEFAULT_MSG Z3_LIBRARY Z3_INCLUDE_DIR)
-
+if (USE_Z3)
+    find_path(Z3_INCLUDE_DIR z3++.h)
+    find_library(Z3_LIBRARY NAMES z3 )
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(Z3 DEFAULT_MSG Z3_LIBRARY Z3_INCLUDE_DIR)
+else()
+    set(Z3_FOUND FALSE)
+endif()
 # TODO: Create IMPORTED library for Z3.
-
