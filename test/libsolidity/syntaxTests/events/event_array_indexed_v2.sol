faed71c6b1235a808a2c87026b345a637238e4a9
Leonardo Alt <leo@ethereum.org>
2018-08-15 16:58:41 +0200

    Review suggestions

--- test/libsolidity/syntaxTests/events/event_array_indexed_v2.sol
+++ test/libsolidity/syntaxTests/events/event_array_indexed_v2.sol
@@ -4,4 +4,4 @@ contract c {
 }
 // ----
 // Warning: (0-33): Experimental features are turned on. Do not use experimental features on live deployments.
-// TypeError: (59-65): Reference types cannot be indexed.
+// TypeError: (59-65): Indexed reference types cannot yet be used with ABIEncoderV2.
--- test/libsolidity/syntaxTests/events/event_struct_indexed_v2.sol
+++ test/libsolidity/syntaxTests/events/event_struct_indexed_v2.sol
@@ -5,4 +5,4 @@ contract c {
 }
 // ----
 // Warning: (0-33): Experimental features are turned on. Do not use experimental features on live deployments.
-// TypeError: (85-86): Reference types cannot be indexed.
+// TypeError: (85-86): Indexed reference types cannot yet be used with ABIEncoderV2.
