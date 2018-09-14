0b4f4469b154865267579d901bdaaf9fe3710b65
Yi Huang <yi@skymizer.com>
2018-09-14 10:38:42 +0000

    Update test expectation for `too_small_negative_numbers`.

--- test/libsolidity/syntaxTests/types/too_small_negative_numbers.sol
+++ test/libsolidity/syntaxTests/types/too_small_negative_numbers.sol
@@ -1,4 +1,5 @@
 contract C {
-  fixed8x80 a = -1e-100;
+  fixed256x76 a = -1e-100;
 }
 // ----
+// TypeError: (31-38): Type rational_const -1 / 1000...(93 digits omitted)...0000 is not implicitly convertible to expected type fixed256x76. Try converting to type fixed8x80 or use an explicit conversion.
