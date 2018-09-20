982d883a8819ec633dc227b47b57f4c24d676b03
liangdzou <liang.d.zou@gmail.com>
2018-09-20 18:01:51 +0800

    enhance test_trailing_whitespace to test_check_style: several format rules for if/for are introduced

---
+++ scripts/check_style.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+REPO_ROOT="$(dirname "$0")"/..
+
+(
+cd $REPO_ROOT
+WHITESPACE=$(git grep -n -I -E "^.*[[:space:]]+$" | grep -v "test/libsolidity/ASTJSON\|test/compilationTests/zeppelin/LICENSE")
+
+if [[ "$WHITESPACE" != "" ]]
+then
+	echo "Error: Trailing whitespace found:" >&2
+	echo "$WHITESPACE" >&2
+	exit 1
+fi
+)
+
+(
+cd $REPO_ROOT
+FORMATERROR=$(
+(
+git grep -nIE "\<(if|for)\(" -- '*.h' '*.cpp'
+git grep -nIE "\<if\>\s*\(.*\)\s*\{\s*$" -- '*.h' '*.cpp'
+) | egrep -v "^[a-zA-Z\./]*:[0-9]*:\s*\/(\/|\*)" | egrep -v "^test/"
+)
+
+if [[ "$FORMATERROR" != "" ]]
+then
+	echo "Error: Format error for if/for:" >&2
+	echo "$FORMATERROR" >&2
+	exit 1
+fi
+)
