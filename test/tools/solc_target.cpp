7502c298a2ab5c096d6c937bc56594bb1f55f36d
Bhargava Shastry <bshastry@sect.tu-berlin.de>
2018-10-23 16:52:13 +0200

    ossfuzz: Add const-opt test; enable all checks probabilistically

--- test/tools/solc_target.cpp
+++ test/tools/solc_target.cpp
@@ -26,6 +26,45 @@ namespace
         return "";
     }
 
+    void testConstantOptimizer(string const& input)
+    {
+	if (!quiet)
+		cout << "Testing constant optimizer" << endl;
+	vector<u256> numbers;
+	stringstream sin(input);
+
+	while (!sin.eof())
+	{
+		h256 data;
+		sin.read(reinterpret_cast<char*>(data.data()), 32);
+		numbers.push_back(u256(data));
+	}
+	if (!quiet)
+		cout << "Got " << numbers.size() << " inputs:" << endl;
+
+	Assembly assembly;
+	for (u256 const& n: numbers)
+	{
+		if (!quiet)
+			cout << n << endl;
+		assembly.append(n);
+	}
+	for (bool isCreation: {false, true})
+	{
+		for (unsigned runs: {1, 2, 3, 20, 40, 100, 200, 400, 1000})
+		{
+			ConstantOptimisationMethod::optimiseConstants(
+				isCreation,
+				runs,
+				EVMVersion{},
+				assembly,
+				const_cast<AssemblyItems&>(assembly.items())
+			);
+		}
+	}
+    }
+
+
     void runCompiler(string input)
     {
         string outputString(compileStandard(input.c_str(), nullptr));
@@ -78,6 +117,12 @@ namespace
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
     string input(reinterpret_cast<const char*>(data), size);
-    testCompiler(input, true);
+
+    if (size % 3 == 0)
+        testCompiler(input, true);
+    else if (size % 3 == 1)
+	testConstantOptimizer(input);
+    else
+	testCompiler(input, false);
     return 0;
 }
