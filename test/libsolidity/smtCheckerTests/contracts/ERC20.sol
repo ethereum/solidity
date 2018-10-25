399c41427dba45d0ab19b63dcefe370aeb1adb02
Leonardo Alt <leo@ethereum.org>
2018-10-25 17:21:22 +0200

    Zeppelin ERC20 contract

---
+++ test/libsolidity/smtCheckerTests/contracts/ERC20.sol
@@ -0,0 +1,56 @@
+pragma solidity >0.4.25;
+pragma experimental SMTChecker;
+
+import "./IERC20.sol";
+import "./SafeMath.sol";
+
+contract ERC20 is IERC20 {
+  using SafeMath for uint256;
+
+  mapping (address => uint256) private _balances;
+
+  uint256 private _totalSupply;
+
+  function totalSupply() public view returns (uint256) {
+    return _totalSupply;
+  }
+
+  function balanceOf(address owner) public view returns (uint256) {
+    return _balances[owner];
+  }
+
+  function transfer(address to, uint256 value) public returns (bool) {
+    _transfer(msg.sender, to, value);
+    return true;
+  }
+
+  function _transfer(address from, address to, uint256 value) internal {
+    require(value <= _balances[from]);
+    require(to != address(0));
+
+    _balances[from] = _balances[from].sub(value);
+    _balances[to] = _balances[to].add(value);
+    emit Transfer(from, to, value);
+  }
+
+  function _mint(address account, uint256 value) internal {
+    require(account != address(0));
+    _totalSupply = _totalSupply.add(value);
+    _balances[account] = _balances[account].add(value);
+    emit Transfer(address(0), account, value);
+  }
+
+  function _burn(address account, uint256 value) internal {
+    require(account != address(0));
+    require(value <= _balances[account]);
+
+    _totalSupply = _totalSupply.sub(value);
+    _balances[account] = _balances[account].sub(value);
+    emit Transfer(account, address(0), value);
+  }
+
+  function _burnFrom(address account, uint256 value) internal {
+    _burn(account, value);
+  }
+}
+
---
+++ test/libsolidity/smtCheckerTests/contracts/IERC20.sol
@@ -0,0 +1,22 @@
+pragma solidity >0.4.25;
+
+interface IERC20 {
+  function totalSupply() external view returns (uint256);
+
+  function balanceOf(address who) external view returns (uint256);
+
+  function transfer(address to, uint256 value) external returns (bool);
+
+  event Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 value
+  );
+
+  event Approval(
+    address indexed owner,
+    address indexed spender,
+    uint256 value
+  );
+}
+
