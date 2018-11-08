2b053ceeffdfeb5006d9c4de334b66a66f2c74d7
Dominik Harz <dominik.harz@gmail.com>
2018-11-08 11:12:47 +0000

    Fix cmake Z3 import issue for Fedora

--- cmake/FindZ3.cmake
+++ cmake/FindZ3.cmake
@@ -1,5 +1,5 @@
 if (USE_Z3)
-    find_path(Z3_INCLUDE_DIR z3++.h)
+    find_path(Z3_INCLUDE_DIR NAMES z3++.h PATH_SUFFIXES z3)
     find_library(Z3_LIBRARY NAMES z3 )
     include(FindPackageHandleStandardArgs)
     find_package_handle_standard_args(Z3 DEFAULT_MSG Z3_LIBRARY Z3_INCLUDE_DIR)
