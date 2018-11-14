4a71080ae575390a72421572396c37fd977b0115
Leonardo Alt <leo@ethereum.org>
2018-11-14 17:08:53 +0100

    Remove pragma ABIEncoderV2 from smt test

--- test/libsolidity/smtCheckerTests/warn_on_struct.sol
+++ test/libsolidity/smtCheckerTests/warn_on_struct.sol
@@ -1,12 +1,14 @@
 pragma experimental SMTChecker;
-pragma experimental ABIEncoderV2;
+
 contract C {
     struct A { uint a; uint b; }
-    function f() public pure returns (A memory) {
-        return A({ a: 1, b: 2 });
+    function f() public pure returns (uint) {
+        A memory a = A({ a: 1, b: 2 });
     }
 }
 // ----
-// Warning: (32-65): Experimental features are turned on. Do not use experimental features on live deployments.
-// Warning: (150-158): Assertion checker does not yet support the type of this variable.
-// Warning: (177-194): Assertion checker does not yet implement this expression.
+// Warning: (133-143): Unused local variable.
+// Warning: (133-143): Assertion checker does not yet support the type of this variable.
+// Warning: (146-163): Assertion checker does not yet implement this expression.
+// Warning: (146-163): Internal error: Expression undefined for SMT solver.
+// Warning: (146-163): Assertion checker does not yet implement this type.
