aa9c24e09bc5ed60a33fe1d53fcb7715aa308241
Chris Ward <chris.ward@ethereum.org>
2018-12-19 14:01:36 +0100

    Add introduction section to ignore

--- docs/conf.py
+++ docs/conf.py
@@ -81,7 +81,7 @@ else:
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build','assembly/*','contracts/*','types/*']
+exclude_patterns = ['_build','assembly/*','contracts/*','types/*','introduction/*']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
