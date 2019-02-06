ee28cb65a6dd9b83fae8f5f71f6179e8d1d10f49
Mathias Baumann <marenz@supradigital.org>
2019-02-06 20:43:53 +0100

    Add tests to increase coverage of TypeChecker

--- test/libsolidity/syntaxTests/nameAndTypeResolution/396_invalid_mobile_type.sol
+++ test/libsolidity/syntaxTests/nameAndTypeResolution/396_invalid_mobile_type.sol
@@ -1,8 +1,10 @@
     contract C {
         function f() public {
             // Invalid number
+            78901234567890123456789012345678901234567890123456789345678901234567890012345678012345678901234567;
             [1, 78901234567890123456789012345678901234567890123456789345678901234567890012345678012345678901234567];
         }
     }
 // ----
-// TypeError: (93-191): Invalid rational number.
+// TypeError: (89-187): Invalid rational number.
+// TypeError: (205-303): Invalid rational number.
