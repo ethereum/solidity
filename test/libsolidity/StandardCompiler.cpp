fcbb8ec51c5e4b6c101f613755413b6e2fa9fb4d
Alex Beregszaszi <alex@rtfs.hu>
2019-02-28 13:08:21 +0000

    Allow returning metadata in StandardCompiler without compilation

--- test/libsolidity/StandardCompiler.cpp
+++ test/libsolidity/StandardCompiler.cpp
@@ -1015,6 +1015,68 @@ BOOST_AUTO_TEST_CASE(optimizer_settings_details_different)
 	BOOST_CHECK(optimizer["runs"].asUInt() == 600);
 }
 
+BOOST_AUTO_TEST_CASE(metadata_without_compilation)
+{
+	// NOTE: the contract code here should fail to compile due to "out of stack"
+	// If the metadata is successfully returned, that means no compilation was attempted.
+	char const* input = R"(
+	{
+		"language": "Solidity",
+		"settings": {
+			"outputSelection": {
+				"fileA": { "A": [ "metadata" ] }
+			}
+		},
+		"sources": {
+			"fileA": {
+				"content": "contract A {
+  function x(uint a, uint b, uint c, uint d, uint e, uint f, uint g, uint h, uint i, uint j, uint k, uint l, uint m, uint n, uint o, uint p) pure public {}
+  function y() pure public {
+    uint a; uint b; uint c; uint d; uint e; uint f; uint g; uint h; uint i; uint j; uint k; uint l; uint m; uint n; uint o; uint p;
+    x(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p);
+  }
+}"
+			}
+		}
+	}
+	)";
+	Json::Value result = compile(input);
+	BOOST_CHECK(containsAtMostWarnings(result));
+	Json::Value contract = getContractResult(result, "fileA", "A");
+	BOOST_CHECK(contract.isObject());
+	BOOST_CHECK(contract["metadata"].isString());
+	BOOST_CHECK(dev::test::isValidMetadata(contract["metadata"].asString()));
+}
+
+BOOST_AUTO_TEST_CASE(common_pattern)
+{
+	char const* input = R"(
+	{
+		"language": "Solidity",
+		"settings": {
+			"outputSelection": {
+				"*": {
+					"*": [ "evm.bytecode.object", "metadata" ]
+				}
+			}
+		},
+		"sources": {
+			"fileA": {
+				"content": "contract A { function f() pure public {} }"
+			}
+		}
+	}
+	)";
+	Json::Value result = compile(input);
+	BOOST_CHECK(containsAtMostWarnings(result));
+	Json::Value contract = getContractResult(result, "fileA", "A");
+	BOOST_CHECK(contract.isObject());
+	BOOST_CHECK(contract["metadata"].isString());
+	BOOST_CHECK(dev::test::isValidMetadata(contract["metadata"].asString()));
+	BOOST_CHECK(contract["evm"]["bytecode"].isObject());
+	BOOST_CHECK(contract["evm"]["bytecode"]["object"].isString());
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 }
