2de5554e4a59f81a056b398f0448287793d6683f
chriseth <chris@ethereum.org>
2019-03-21 17:17:42 +0100

    Fix MacOS commandline tests.

--- test/cmdlineTests.sh
+++ test/cmdlineTests.sh
@@ -127,6 +127,8 @@ function test_solc_behaviour()
     fi
     # Remove path to cpp file
     sed -i -e 's/^\(Exception while assembling:\).*/\1/' "$stderr_path"
+    # Remove exception class name.
+    sed -i -e 's/^\(Dynamic exception type:\).*/\1/' "$stderr_path"
 
     if [[ $exitCode -ne "$exit_code_expected" ]]
     then
