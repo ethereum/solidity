9633b285ab8c3ff5f02ebb33cae2b3924b7a4d12
Mathias Baumann <marenz@supradigital.org>
2019-04-02 11:07:00 +0200

    Log aleth output upon failure

---
+++ scripts/aleth_with_log.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+ALETH_PATH=$1
+ALETH_TMP_OUT=$2
+shift
+shift
+
+
+$ALETH_PATH $@ &> >(tail -n 10000 &> "$ALETH_TMP_OUT") &
+
+PID=$!
+
+function cleanup()
+{
+	kill $PID
+}
+
+trap cleanup INT TERM
+
+wait $PID
+
