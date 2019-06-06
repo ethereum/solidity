2ec45797b24906317b88ed05c68171e7d5dcddaf
Daniel Kirchner <daniel@ekpyron.org>
2019-06-06 17:19:18 +0200

    Update boost to 1.70 for emscripten and cache a proper installation.

--- scripts/travis-emscripten/build_emscripten.sh
+++ scripts/travis-emscripten/build_emscripten.sh
@@ -64,15 +64,14 @@ fi
 
 # Boost
 echo -en 'travis_fold:start:compiling_boost\\r'
-cd "$WORKSPACE"/boost_1_68_0
-# if b2 exists, it is a fresh checkout, otherwise it comes from the cache
-# and is already compiled
-test -e b2 && (
+test -e "$WORKSPACE"/boost_1_70_0_install || (
+cd "$WORKSPACE"/boost_1_70_0
 ./b2 toolset=emscripten link=static variant=release threading=single runtime-link=static \
-       system regex filesystem unit_test_framework program_options cxxflags="-Wno-unused-local-typedef -Wno-variadic-macros -Wno-c99-extensions -Wno-all"
-find . -name 'libboost*.a' -exec cp {} . \;
-rm -rf b2 libs doc tools more bin.v2 status
+       --with-system --with-regex --with-filesystem --with-test --with-program_options cxxflags="-Wno-unused-local-typedef -Wno-variadic-macros -Wno-c99-extensions -Wno-all" \
+       --prefix="$WORKSPACE"/boost_1_70_0_install install
 )
+ln -sf "$WORKSPACE"/boost_1_70_0_install/lib/* /emsdk_portable/sdk/system/lib
+ln -sf "$WORKSPACE"/boost_1_70_0_install/include/* /emsdk_portable/sdk/system/include
 echo -en 'travis_fold:end:compiling_boost\\r'
 
 echo -en 'travis_fold:start:install_cmake.sh\\r'
@@ -87,15 +86,8 @@ cd $BUILD_DIR
 cmake \
   -DCMAKE_TOOLCHAIN_FILE=../cmake/toolchains/emscripten.cmake \
   -DCMAKE_BUILD_TYPE=Release \
-  -DBoost_FOUND=1 \
   -DBoost_USE_STATIC_LIBS=1 \
   -DBoost_USE_STATIC_RUNTIME=1 \
-  -DBoost_INCLUDE_DIR="$WORKSPACE"/boost_1_68_0/ \
-  -DBoost_FILESYSTEM_LIBRARY_RELEASE="$WORKSPACE"/boost_1_68_0/libboost_filesystem.a \
-  -DBoost_PROGRAM_OPTIONS_LIBRARY_RELEASE="$WORKSPACE"/boost_1_68_0/libboost_program_options.a \
-  -DBoost_REGEX_LIBRARY_RELEASE="$WORKSPACE"/boost_1_68_0/libboost_regex.a \
-  -DBoost_SYSTEM_LIBRARY_RELEASE="$WORKSPACE"/boost_1_68_0/libboost_system.a \
-  -DBoost_UNIT_TEST_FRAMEWORK_LIBRARY_RELEASE="$WORKSPACE"/boost_1_68_0/libboost_unit_test_framework.a \
   -DTESTS=0 \
   ..
 make -j 4
