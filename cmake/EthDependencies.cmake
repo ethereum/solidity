226f8bb64d6ade2615dba6416e6b61ede42c4c30
Daniel Kirchner <daniel@ekpyron.org>
2019-06-06 17:18:48 +0200

    Use imported cmake targets for boost.

--- cmake/EthDependencies.cmake
+++ cmake/EthDependencies.cmake
@@ -1,21 +1,3 @@
-# all dependencies that are not directly included in the cpp-ethereum distribution are defined here
-# for this to work, download the dependency via the cmake script in extdep or install them manually!
-
-function(eth_show_dependency DEP NAME)
-	get_property(DISPLAYED GLOBAL PROPERTY ETH_${DEP}_DISPLAYED)
-	if (NOT DISPLAYED)
-		set_property(GLOBAL PROPERTY ETH_${DEP}_DISPLAYED TRUE)
-		if (NOT("${${DEP}_VERSION}" STREQUAL ""))
-			message(STATUS "${NAME} version: ${${DEP}_VERSION}")
-		endif()
-		message(STATUS "${NAME} headers: ${${DEP}_INCLUDE_DIRS}")
-		message(STATUS "${NAME} lib   : ${${DEP}_LIBRARIES}")
-		if (NOT("${${DEP}_DLLS}" STREQUAL ""))
-			message(STATUS "${NAME} dll   : ${${DEP}_DLLS}")
-		endif()
-	endif()
-endfunction()
-
 if (DEFINED MSVC)
 	# by defining CMAKE_PREFIX_PATH variable, cmake will look for dependencies first in our own repository before looking in system paths like /usr/local/ ...
 	# this must be set to point to the same directory as $ETH_DEPENDENCY_INSTALL_DIR in /extdep directory
@@ -41,6 +23,14 @@ set(ETH_SCRIPTS_DIR ${ETH_CMAKE_DIR}/scripts)
 set(Boost_USE_MULTITHREADED ON)
 option(Boost_USE_STATIC_LIBS "Link Boost statically" ON)
 
-find_package(Boost 1.65.0 QUIET REQUIRED COMPONENTS regex filesystem unit_test_framework program_options system)
+set(BOOST_COMPONENTS "regex;filesystem;unit_test_framework;program_options;system")
 
-eth_show_dependency(Boost boost)
+find_package(Boost 1.65.0 QUIET REQUIRED COMPONENTS ${BOOST_COMPONENTS})
+
+# make sure we actually get all required imported targets for boost
+list(APPEND BOOST_COMPONENTS "boost") # header only target
+foreach (BOOST_COMPONENT IN LISTS BOOST_COMPONENTS)
+	if (NOT TARGET Boost::${BOOST_COMPONENT})
+		message(FATAL_ERROR "Boost target Boost::${BOOST_COMPONENT} was not defined by cmake.")
+    endif()
+endforeach()
