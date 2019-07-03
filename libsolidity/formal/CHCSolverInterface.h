71144d0d39dc545d9f9cf577be5969371a4d7aa5
Leonardo Alt <leo@ethereum.org>
2019-07-03 16:33:20 +0200

    [CHCChecker] Add CHCSolverInterface and Z3CHCSolverInterface

---
+++ libsolidity/formal/CHCSolverInterface.h
@@ -0,0 +1,56 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * Interface for constrained Horn solvers.
+ */
+
+#pragma once
+
+#include <libsolidity/formal/SolverInterface.h>
+
+namespace dev
+{
+namespace solidity
+{
+namespace smt
+{
+
+class CHCSolverInterface
+{
+public:
+	virtual ~CHCSolverInterface() = default;
+
+	virtual void declareVariable(std::string const& _name, Sort const& _sort) = 0;
+
+	/// Takes a function declaration as a relation.
+	virtual void registerRelation(Expression const& _expr) = 0;
+
+	/// Takes an implication and adds as rule.
+	/// Needs to bound all vars as universally quantified.
+	virtual void addRule(Expression const& _expr, std::string const& _name) = 0;
+
+	/// Takes a function application and checks
+	/// for reachability.
+	virtual std::pair<CheckResult, std::vector<std::string>> query(
+		Expression const& _expr
+	) = 0;
+};
+
+}
+}
+}
--- libsolidity/formal/Z3Interface.h
+++ libsolidity/formal/Z3Interface.h
@@ -43,17 +43,24 @@ public:
 	void addAssertion(Expression const& _expr) override;
 	std::pair<CheckResult, std::vector<std::string>> check(std::vector<Expression> const& _expressionsToEvaluate) override;
 
+	z3::expr toZ3Expr(Expression const& _expr);
+
+	std::map<std::string, z3::expr> constants() const { return m_constants; }
+	std::map<std::string, z3::func_decl> functions() const { return m_functions; }
+
+	z3::context* context() { return &m_context; }
+
 private:
 	void declareFunction(std::string const& _name, Sort const& _sort);
 
-	z3::expr toZ3Expr(Expression const& _expr);
 	z3::sort z3Sort(smt::Sort const& _sort);
 	z3::sort_vector z3Sort(std::vector<smt::SortPointer> const& _sorts);
 
-	z3::context m_context;
-	z3::solver m_solver;
 	std::map<std::string, z3::expr> m_constants;
 	std::map<std::string, z3::func_decl> m_functions;
+
+	z3::context m_context;
+	z3::solver m_solver;
 };
 
 }
