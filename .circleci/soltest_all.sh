b94bdfcec040f8c526ba2630b621e3783d3a1ca7
Christian Parpart <christian@ethereum.org>
2019-08-14 15:43:24 +0200

    Merges soltest tests per EVM/flags into a single one.
    Because of EVMONE, speeds up test runs we can do that now.

---
+++ .circleci/soltest_all.sh
@@ -0,0 +1,37 @@
+#! /bin/bash
+#------------------------------------------------------------------------------
+# Bash script to execute the Solidity tests by CircleCI.
+#
+# The documentation for solidity is hosted at:
+#
+#     https://solidity.readthedocs.org
+#
+# ------------------------------------------------------------------------------
+# This file is part of solidity.
+#
+# solidity is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# solidity is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with solidity.  If not, see <http://www.gnu.org/licenses/>
+#
+# (c) 2016-2019 solidity contributors.
+# ------------------------------------------------------------------------------
+set -e
+
+REPODIR="$(realpath $(dirname $0)/..)"
+
+for OPTIMIZE in 0 1; do
+    for EVM in homestead byzantium constantinople petersburg; do
+        EVM=$EVM OPTIMIZE=$OPTIMIZE ${REPODIR}/.circleci/soltest.sh
+    done
+done
+
+EVM=constantinople OPTIMIZE=1 ABI_ENCODER_V2=1 ${REPODIR}/.circleci/soltest.sh
