0b35cbc22c1f3905eb9d2e5fcdc3d074f5dffddc
Bhargava Shastry <bhargava.shastry@ethereum.org>
2019-08-14 12:42:35 +0200

    Inherit defaults cmake options in toolchain file used by ossfuzz builder.

--- cmake/toolchains/libfuzzer.cmake
+++ cmake/toolchains/libfuzzer.cmake
@@ -1,2 +1,9 @@
-# Require libfuzzer specific flags
-set(CMAKE_CXX_FLAGS "-O1 -gline-tables-only -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link -stdlib=libstdc++ -fopenmp=libgomp")
+# Inherit default options
+include("${CMAKE_CURRENT_LIST_DIR}/default.cmake")
+# Disable Z3 and CVC4 since none of the existing fuzzers need them
+set(USE_Z3 OFF CACHE BOOL "" FORCE)
+set(USE_CVC4 OFF CACHE BOOL "" FORCE)
+# Build fuzzing binaries
+set(OSSFUZZ 1)
+# clang/libfuzzer specific flags for ASan instrumentation
+set(CMAKE_CXX_FLAGS "-O1 -gline-tables-only -fsanitize=address -fsanitize-address-use-after-scope -fsanitize=fuzzer-no-link -stdlib=libstdc++")
