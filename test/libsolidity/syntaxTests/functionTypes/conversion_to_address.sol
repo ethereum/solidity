4354ef2ef48227c322d0dd9ad57ab7195d33c722
Daniel Kirchner <daniel@ekpyron.org>
2019-08-15 18:46:44 +0200

    Add update suggestion.

--- test/libsolidity/syntaxTests/functionTypes/conversion_to_address.sol
+++ test/libsolidity/syntaxTests/functionTypes/conversion_to_address.sol
@@ -4,4 +4,4 @@ contract C {
     }
 }
 // ----
-// TypeError: (77-92): Explicit type conversion not allowed from "function () view external returns (address)" to "address".
+// TypeError: (77-92): Explicit type conversion not allowed from "function () view external returns (address)" to "address". To obtain the address of the contract of the function, you can use the .address member of the function.
