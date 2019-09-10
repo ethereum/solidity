dc2adb3d9d5aa973dca300afce85dae1b965becc
Christian Parpart <christian@ethereum.org>
2019-09-10 11:45:56 +0200

    Assembly: Removing EVMDialect::looseAssemblyForEVM().

--- libsolidity/analysis/ReferencesResolver.cpp
+++ libsolidity/analysis/ReferencesResolver.cpp
@@ -328,7 +328,7 @@ bool ReferencesResolver::visit(InlineAssembly const& _inlineAssembly)
 		analysisInfo,
 		errorsIgnored,
 		errorTypeForLoose,
-		yul::EVMDialect::looseAssemblyForEVM(EVMVersion{}),
+		yul::EVMDialect::strictAssemblyForEVM(EVMVersion{}),
 		resolver
 	).analyze(_inlineAssembly.operations());
 	return false;
