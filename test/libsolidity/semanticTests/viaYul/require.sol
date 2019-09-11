c74b1daef25f3ec54ab3f635d3d417dd51fdad1f
Mathias Baumann <git@supradigital.org>
2019-09-11 12:26:31 +0200

    Test `require` with string literals
    fixes #6651

--- test/libsolidity/semanticTests/viaYul/require.sol
+++ test/libsolidity/semanticTests/viaYul/require.sol
@@ -12,17 +12,33 @@ contract C {
 		x = true;
 		require(true);
 	}
-	/* Not properly supported by test system yet
 	function f2(bool a) public pure returns (bool x) {
 		x = a;
 		string memory message;
+		message = "fancy message!";
 		require(a, message);
-	}*/
+	}
+	function f3(bool a) public pure returns (bool x) {
+		x = a;
+		require(a, "msg");
+	}
+	function f4(bool a) public pure returns (bool x) {
+		x = a;
+		string memory message;
+		require(a, message);
+	}
 }
 // ====
-// compileViaYul: true
+// compileViaYul: also
+// EVMVersion: >=byzantium
 // ----
 // f(bool): true -> true
 // f(bool): false -> FAILURE
 // fail() -> FAILURE
 // succeed() -> true
+// f2(bool): true -> true
+// f2(bool): false -> FAILURE, hex"08c379a0", 0x20, 14, "fancy message!"
+// f3(bool): true -> true
+// f3(bool): false -> FAILURE, hex"08c379a0", 0x20, 3, "msg"
+// f4(bool): true -> true
+// f4(bool): false -> FAILURE, hex"08c379a0", 0x20, 0
