1a4ee4ea34e84c0c02eb52229ea49d65785818b9
Alex Beregszaszi <alex@rtfs.hu>
2019-11-26 19:11:11 +0100

    Revert "Run solc-js tests against callback-context [REMOVE]"
    This reverts commit 31335443130545fc921cf0439ad1cc2c21214732.

--- test/externalTests/solc-js/solc-js.sh
+++ test/externalTests/solc-js/solc-js.sh
@@ -35,7 +35,7 @@ function solcjs_test
     SOLCJS_INPUT_DIR="$TEST_DIR"/test/externalTests/solc-js
 
     # set up solc-js on the branch specified
-    setup callback-context
+    setup master_060
 
     printLog "Updating index.js file..."
     echo "require('./determinism.js');" >> test/index.js
