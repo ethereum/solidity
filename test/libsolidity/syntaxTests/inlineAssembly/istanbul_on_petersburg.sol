cf12e20e436ecc0c7d9910a3d967f8a82c1199b4
chriseth <chris@ethereum.org>
2019-12-05 09:12:05 +0100

    Fix test.

--- test/libsolidity/syntaxTests/inlineAssembly/istanbul_on_petersburg.sol
+++ test/libsolidity/syntaxTests/inlineAssembly/istanbul_on_petersburg.sol
@@ -13,7 +13,7 @@ contract C {
 // ====
 // EVMVersion: =petersburg
 // ----
-// DeclarationError: (101-108): Function not found.
+// TypeError: (101-108): The "chainid" instruction is only available for Istanbul-compatible VMs  (you are currently compiling for "petersburg").
 // DeclarationError: (95-110): Variable count does not match number of values (1 vs. 0)
-// DeclarationError: (215-226): Function not found.
+// TypeError: (215-226): The "selfbalance" instruction is only available for Istanbul-compatible VMs  (you are currently compiling for "petersburg").
 // DeclarationError: (209-228): Variable count does not match number of values (1 vs. 0)
