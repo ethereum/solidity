7d3ccee921e9dcf7e140e88dda406040f9ac77cb
Alex Beregszaszi <alex@rtfs.hu>
2019-12-18 14:56:29 +0000

    Fix implementation of getCodeSize in Ewasm

--- libyul/backends/wasm/EVMToEWasmTranslator.cpp
+++ libyul/backends/wasm/EVMToEWasmTranslator.cpp
@@ -401,8 +401,7 @@ function calldatacopy(x1, x2, x3, x4, y1, y2, y3, y4, z1, z2, z3, z4) {
 
 // Needed?
 function codesize() -> z1, z2, z3, z4 {
-	eth.getCodeSize(0)
-	z1, z2, z3, z4 := mload_internal(0)
+	z4 := eth.getCodeSize()
 }
 function codecopy(x1, x2, x3, x4, y1, y2, y3, y4, z1, z2, z3, z4) {
 	eth.codeCopy(
