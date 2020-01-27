4a87f6e4035e9feece7cd228fd82a389965af764
Erik Kundt <erik@ethereum.org>
2020-01-27 11:39:54 +0100

    Removes the binary option from JSON AST extraction script.

--- scripts/splitSources.py
+++ scripts/splitSources.py
@@ -48,7 +48,7 @@ def writeSourceToFile(lines):
 if __name__ == '__main__':
     filePath = sys.argv[1]
     # decide if file has multiple sources
-    lines = open(filePath, mode='rb', encoding='utf8').read().splitlines()
+    lines = open(filePath, mode='r', encoding='utf8').read().splitlines()
     if lines[0][:12] == "==== Source:":
         hasMultipleSources = True
         writeSourceToFile(lines)
