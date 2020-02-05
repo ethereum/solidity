cecf912d6ff2773a17d151e3db9836592d6a03e4
cameel <cameel2@gmail.com>
2020-02-05 14:42:38 +0100

    [yul-phaser] Add geneRandomization(), geneDeletion(), geneAddition and alternativeMutations()

---
+++ tools/yulPhaser/Mutations.h
@@ -0,0 +1,38 @@
+/*
+	This file is part of solidity.
+
+	solidity is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+#include <tools/yulPhaser/Chromosome.h>
+
+#include <functional>
+
+namespace solidity::phaser
+{
+
+using Mutation = Chromosome(Chromosome const&);
+
+std::function<Mutation> geneRandomization(double _chance);
+std::function<Mutation> geneDeletion(double _chance);
+std::function<Mutation> geneAddition(double _chance);
+std::function<Mutation> alternativeMutations(
+	double _firstMutationChance,
+	std::function<Mutation> _mutation1,
+	std::function<Mutation> _mutation2
+);
+
+}
