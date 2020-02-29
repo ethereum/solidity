1cd6f2b37bdd6ee03d854671b60717e0bfe4d880
Alexander Arlt <alexander.arlt@arlt-labs.com>
2020-02-29 12:41:28 -0500

    Fix formatting for some contracts.

--- test/libsolidity/semanticTests/scientific_notation.sol
+++ test/libsolidity/semanticTests/scientific_notation.sol
@@ -1,25 +1,29 @@
+contract C {
+    function f() public returns(uint) {
+        return 2e10 wei;
+    }
 
-		contract C {
-			function f() public returns (uint) {
-				return 2e10 wei;
-			}
-			function g() public returns (uint) {
-				return 200e-2 wei;
-			}
-			function h() public returns (uint) {
-				return 2.5e1;
-			}
-			function i() public returns (int) {
-				return -2e10;
-			}
-			function j() public returns (int) {
-				return -200e-2;
-			}
-			function k() public returns (int) {
-				return -2.5e1;
-			}
-		}
-	
+    function g() public returns(uint) {
+        return 200e-2 wei;
+    }
+
+    function h() public returns(uint) {
+        return 2.5e1;
+    }
+
+    function i() public returns(int) {
+        return -2e10;
+    }
+
+    function j() public returns(int) {
+        return -200e-2;
+    }
+
+    function k() public returns(int) {
+        return -2.5e1;
+    }
+}
+
 // ====
 // compileViaYul: also
 // ----
