78bb5aa0b05d9620edba9cdc41cdee58d5d6b023
Alexander Arlt <alexander.arlt@arlt-labs.com>
2020-03-02 16:30:48 -0500

    Format new test-cases.

--- test/libsolidity/semanticTests/abi_encode_decode_simple.sol
+++ test/libsolidity/semanticTests/abi_encode_decode_simple.sol
@@ -1,11 +1,9 @@
+contract C {
+    function f() public pure returns (uint256, bytes memory) {
+        bytes memory arg = "abcdefg";
+        return abi.decode(abi.encode(uint256(33), arg), (uint256, bytes));
+    }
+}
 
-		contract C {
-			function f() public pure returns (uint, bytes memory) {
-				bytes memory arg = "abcdefg";
-				return abi.decode(abi.encode(uint(33), arg), (uint, bytes));
-			}
-		}
-	
 // ----
 // f() -> 0x21, 0x40, 0x7, "abcdefg"
-
