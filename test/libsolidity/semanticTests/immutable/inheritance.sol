e255c15227a295dba2657b3c4c2a8f51c48a66ab
Daniel Kirchner <daniel@ekpyron.org>
2020-03-12 17:32:10 +0100

    Tests.

---
+++ test/libsolidity/semanticTests/immutable/inheritance.sol
@@ -0,0 +1,30 @@
+contract A {
+	uint8 immutable a;
+	constructor() public {
+		a = 4;
+	}
+}
+contract B is A {
+	uint8 immutable b;
+	constructor() public {
+		b = 3;
+	}
+}
+contract C is A {
+	uint8 immutable c;
+	constructor() public {
+		c = 2;
+	}
+}
+contract D is B, C {
+	uint8 immutable d;
+
+	constructor() public {
+		d = 1;
+	}
+	function f() public view returns (uint256, uint256, uint, uint) {
+		return (a, b, c, d);
+	}
+}
+// ----
+// f() -> 4, 3, 2, 1
---
+++ test/libsolidity/semanticTests/immutable/multi_creation.sol
@@ -0,0 +1,31 @@
+contract A {
+	uint immutable a;
+	constructor() public {
+		a = 7;
+	}
+	function f() public view returns (uint) { return a; }
+}
+contract B {
+	uint immutable a;
+	constructor() public {
+		a = 5;
+	}
+	function f() public view returns (uint) { return a; }
+}
+contract C {
+	uint immutable a;
+	uint public x;
+	uint public y;
+	constructor() public {
+		a = 3;
+		x = (new A()).f();
+		y = (new B()).f();
+	}
+	function f() public returns (uint256, uint, uint) {
+		return (a, (new A()).f(), (new B()).f());
+	}
+}
+// ----
+// f() -> 3, 7, 5
+// x() -> 7
+// y() -> 5
