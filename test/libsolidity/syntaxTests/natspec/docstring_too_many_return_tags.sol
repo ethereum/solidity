42b143098e4fdea625da062575f400261b806558
Leonardo Alt <leo@ethereum.org>
2020-03-24 13:04:07 +0100

    Add natspec test with too many return tags

---
+++ test/libsolidity/syntaxTests/natspec/docstring_too_many_return_tags.sol
@@ -0,0 +1,8 @@
+abstract contract C {
+    /// @param id Some identifier
+    /// @return value Some value
+	/// @return value2 Some value 2
+    function vote(uint id) public virtual returns (uint value);
+}
+// ----
+// DocstringParsingError: (26-121): Documentation tag "@return value2 Some value 2" exceeds the number of return parameters.
