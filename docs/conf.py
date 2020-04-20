c59854c4bb32ecf34e8196a1a3de528ccf01c4a2
iamdefinitelyahuman <ben.hauser@hyperlink.technology>
2020-04-20 20:23:45 +0400

    docs: add dark mode to documentation

--- docs/conf.py
+++ docs/conf.py
@@ -146,10 +146,14 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = ["css/toggle.css"]
+
+html_js_files = ["js/toggle.js"]
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-#html_extra_path = []
+html_extra_path = ["_static/css"]
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
