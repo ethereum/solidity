7908e6c3738cea10ab22f9231f957d935c0b24db
chriseth <chris@ethereum.org>
2020-06-10 18:23:19 +0200

    Fix compiler error.

--- libsolidity/analysis/TypeChecker.cpp
+++ libsolidity/analysis/TypeChecker.cpp
@@ -334,7 +334,7 @@ bool TypeChecker::visit(FunctionDefinition const& _function)
 			m_errorReporter.warning(5815_error, _function.location(), "Interface functions are implicitly \"virtual\"");
 		if (_function.visibility() == Visibility::Private)
 			m_errorReporter.typeError(3942_error, _function.location(), "\"virtual\" and \"private\" cannot be used together.");
-		if (isLibraryFunction)
+		if (_function.libraryFunction())
 			m_errorReporter.typeError(1878_error, _function.location(), "Library functions cannot be \"virtual\".");
 	}
 
