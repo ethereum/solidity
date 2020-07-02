8eb459c825f544c32747bb69be1e4aabd77aa23a
chriseth <chris@ethereum.org>
2020-07-02 14:38:36 +0200

    Disallow dot in inline assembly identifiers.

--- libsolidity/analysis/ReferencesResolver.h
+++ libsolidity/analysis/ReferencesResolver.h
@@ -88,6 +88,9 @@ private:
 	void operator()(yul::Identifier const& _identifier) override;
 	void operator()(yul::VariableDeclaration const& _varDecl) override;
 
+	/// Checks if the name contains a '.'.
+	void validateYulIdentifierName(yul::YulString _name, langutil::SourceLocation const& _location);
+
 	langutil::ErrorReporter& m_errorReporter;
 	NameAndTypeResolver& m_resolver;
 	langutil::EVMVersion m_evmVersion;
