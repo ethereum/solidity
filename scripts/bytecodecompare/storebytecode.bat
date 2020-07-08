fe5a2f7c278fa7419a7a48d52441fc48addf8fc3
Sachin Grover <sacgrover@gmail.com>
2020-07-08 17:01:46 +0530

    Add SPDX license identifier in source files of compiler. Fixes: #9220

--- scripts/bytecodecompare/storebytecode.bat
+++ scripts/bytecodecompare/storebytecode.bat
@@ -1,22 +1,7 @@
 @ECHO OFF
 
 REM ---------------------------------------------------------------------------
-REM This file is part of solidity.
-REM
-REM solidity is free software: you can redistribute it and/or modify
-REM it under the terms of the GNU General Public License as published by
-REM the Free Software Foundation, either version 3 of the License, or
-REM (at your option) any later version.
-REM
-REM solidity is distributed in the hope that it will be useful,
-REM but WITHOUT ANY WARRANTY; without even the implied warranty of
-REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-REM GNU General Public License for more details.
-REM
-REM You should have received a copy of the GNU General Public License
-REM along with solidity.  If not, see <http://www.gnu.org/licenses/>
-REM
-REM Copyright (c) 2017 solidity contributors.
+REM SPDX-License-Identifier: GPL-3.0
 REM ---------------------------------------------------------------------------
 
 set CONFIGURATION=%1
--- scripts/install_deps.bat
+++ scripts/install_deps.bat
@@ -40,22 +40,7 @@ REM
 REM http://solidity.readthedocs.org
 REM
 REM ---------------------------------------------------------------------------
-REM This file is part of solidity.
-REM
-REM solidity is free software: you can redistribute it and/or modify
-REM it under the terms of the GNU General Public License as published by
-REM the Free Software Foundation, either version 3 of the License, or
-REM (at your option) any later version.
-REM
-REM solidity is distributed in the hope that it will be useful,
-REM but WITHOUT ANY WARRANTY; without even the implied warranty of
-REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-REM GNU General Public License for more details.
-REM
-REM You should have received a copy of the GNU General Public License
-REM along with solidity.  If not, see <http://www.gnu.org/licenses/>
-REM
-REM Copyright (c) 2016 solidity contributors.
+REM SPDX-License-Identifier: GPL-3.0
 REM ---------------------------------------------------------------------------
 
 cmake -P scripts\install_deps.cmake
