23ee011c56130ed15139d261d69223cc08d2300f
Leonardo Alt <leo@ethereum.org>
2020-09-02 10:45:47 +0200

    [SMTChecker] Fix imports

---
+++ test/libsolidity/smtCheckerTests/imports/imported_fail_2.sol
@@ -0,0 +1,27 @@
+==== Source:  ====
+import "B.sol";
+pragma experimental SMTChecker;
+contract C is B {
+	function h(uint _x) public view {
+		assert(_x < x);
+	}
+}
+==== Source: A.sol ====
+contract A {
+	uint x;
+	function f(uint _x) public {
+		x = _x;
+	}
+}
+==== Source: B.sol ====
+import "A.sol";
+pragma experimental SMTChecker;
+contract B is A {
+	function g(uint _x) public view {
+		assert(_x > x);
+	}
+}
+// ----
+// Warning 6328: (B.sol:103-117): Assertion violation happens here.
+// Warning 6328: (103-117): Assertion violation happens here.
+// Warning 6328: (B.sol:103-117): Assertion violation happens here.
---
+++ test/libsolidity/smtCheckerTests/imports/simple_imported_fail_two_pragmas.sol
@@ -0,0 +1,14 @@
+==== Source:  ====
+import "A.sol";
+pragma experimental SMTChecker;
+contract C is A {}
+==== Source: A.sol ====
+pragma experimental SMTChecker;
+contract A {
+	function f(uint x) public pure {
+		assert(x > 0);
+	}
+}
+// ----
+// Warning 6328: (A.sol:81-94): Assertion violation happens here.
+// Warning 6328: (A.sol:81-94): Assertion violation happens here.
