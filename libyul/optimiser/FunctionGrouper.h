d166c2540a0d530582760e145579709e196f2976
Harikrishnan Mulackal <webmail.hari@gmail.com>
2020-09-07 17:24:03 +0200

    Fix typo block

--- libyul/optimiser/FunctionGrouper.h
+++ libyul/optimiser/FunctionGrouper.h
@@ -16,7 +16,7 @@
 */
 // SPDX-License-Identifier: GPL-3.0
 /**
- * Optimiser component that changes the code of a black so that all non-function definition
+ * Optimiser component that changes the code of a block so that all non-function definition
  * instructions are moved to a block of their own followed by all function definitions.
  */
 
