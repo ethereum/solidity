ec6d21cde2b9e265c25ce2a149e85c49e7843057
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-09-08 15:37:41 +0200

    [Sol->Yul] Fixing storage array index access

---
+++ test/libsolidity/semanticTests/array/copying/array_storage_multi_items_per_slot.sol
@@ -0,0 +1,16 @@
+contract C {
+    uint8[33] a;
+    uint32[9] b;
+    uint120[3] c;
+
+    function f() public returns (uint8, uint32, uint120) {
+        a[32] = 1; a[31] = 2; a[30] = 3;
+        b[0] = 1; b[1] = 2; b[2] = 3;
+        c[2] = 3; c[1] = 1;
+        return (a[32], b[1], c[2]);
+    }
+}
+// ====
+// compileViaYul: also
+// ----
+// f() -> 1, 2, 3
