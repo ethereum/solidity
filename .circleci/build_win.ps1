d66b6f1c467469ea6c1e346f09f60384868837c7
Daniel Kirchner <daniel@ekpyron.org>
2020-09-15 14:47:30 +0200

    CircleCI Windows config and install_deps.ps1 script.

---
+++ .circleci/build_win.ps1
@@ -0,0 +1,7 @@
+cd "$PSScriptRoot\.."
+mkdir build
+cd build
+$boost_dir=(Resolve-Path $PSScriptRoot\..\deps\boost\lib\cmake\Boost-*)
+..\deps\cmake\bin\cmake -G "Visual Studio 16 2019" -DBoost_DIR="$boost_dir\" -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_INSTALL_PREFIX="$PSScriptRoot\..\upload" ..
+msbuild solidity.sln /p:Configuration=Release /m:5 /v:minimal
+..\deps\cmake\bin\cmake --build . -j 5 --target install --config Release
