b2f1d558da69dd41d83fd437928f8f3a39c4befd
chriseth <chris@ethereum.org>
2020-09-29 16:43:04 +0200

    Update existing tests.

--- test/libsolidity/syntaxTests/freeFunctions/free_receive.sol
+++ test/libsolidity/syntaxTests/freeFunctions/free_receive.sol
@@ -1,3 +1,3 @@
 receive() {}
 // ----
-// ParserError 7858: (0-7): Expected pragma, import directive or contract/interface/library/struct/enum/function definition.
+// ParserError 7858: (0-7): Expected pragma, import directive or contract/interface/library/struct/enum/constant/function definition.
--- test/libsolidity/syntaxTests/unexpected.sol
+++ test/libsolidity/syntaxTests/unexpected.sol
@@ -1,3 +1,3 @@
 unexpected
 // ----
-// ParserError 7858: (0-10): Expected pragma, import directive or contract/interface/library/struct/enum/function definition.
+// ParserError 7858: (0-10): Expected pragma, import directive or contract/interface/library/struct/enum/constant/function definition.
