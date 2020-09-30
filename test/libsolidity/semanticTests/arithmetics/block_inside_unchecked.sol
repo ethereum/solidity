c9ef727136a5aed0b2e1fec787abdbdd6bd57b0a
chriseth <chris@ethereum.org>
2020-10-01 00:13:03 +0200

    New tests.

---
+++ test/libsolidity/semanticTests/arithmetics/block_inside_unchecked.sol
@@ -0,0 +1,13 @@
+contract C {
+  function f() public returns (uint y) {
+    unchecked{{
+        uint max = type(uint).max;
+        uint x = max + 1;
+        y = x;
+    }}
+  }
+}
+// ====
+// compileViaYul: also
+// ----
+// f() -> 0x00
