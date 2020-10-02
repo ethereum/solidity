32cb5207b36ba02c4217fd8035d8dc42bbedf41f
Alex Beregszaszi <alex@rtfs.hu>
2020-10-02 11:37:13 +0100

    Add more tests to ViewPureChecker

---
+++ test/libsolidity/syntaxTests/viewPureChecker/address.sol
@@ -0,0 +1,8 @@
+contract C {
+    function f() public view returns (uint) {
+        return address(this).balance;
+    }
+    function g() public view returns (uint) {
+        return address(0).balance;
+    }
+}
