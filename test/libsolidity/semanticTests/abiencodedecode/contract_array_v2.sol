79d9a1ca81d53c92f47649eca6bd782825dbf142
Daniel Kirchner <daniel@ekpyron.org>
2020-10-16 20:24:43 +0200

    Tests for abi.encode.

--- test/libsolidity/semanticTests/abiencodedecode/contract_array_v2.sol
+++ test/libsolidity/semanticTests/abiencodedecode/contract_array_v2.sol
@@ -3,6 +3,13 @@ contract C {
   function f(bytes calldata x) public returns (C[] memory) {
     return abi.decode(x, (C[]));
   }
+  function g() public returns (bytes memory) {
+    C[] memory c = new C[](3);
+    c[0] = C(address(0x42));
+    c[1] = C(address(0x21));
+    c[2] = C(address(0x23));
+    return abi.encode(c);
+  }
 }
 // ====
 // compileViaYul: also
@@ -10,3 +17,4 @@ contract C {
 // f(bytes): 0x20, 0xA0, 0x20, 3, 0x01, 0x02, 0x03 -> 0x20, 3, 0x01, 0x02, 0x03
 // f(bytes): 0x20, 0x60, 0x20, 1, 0x0102030405060708090a0b0c0d0e0f1011121314 -> 0x20, 1, 0x0102030405060708090a0b0c0d0e0f1011121314
 // f(bytes): 0x20, 0x60, 0x20, 1, 0x0102030405060708090a0b0c0d0e0f101112131415 -> FAILURE
+// g() -> 0x20, 0xa0, 0x20, 3, 0x42, 0x21, 0x23
