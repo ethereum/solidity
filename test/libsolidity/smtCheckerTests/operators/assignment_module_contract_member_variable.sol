4755cfe157d0b077a895e292a370b434c4bb43ca
Leonardo Alt <leo@ethereum.org>
2020-10-23 11:44:04 +0100

    Fix assignment to contract member access

---
+++ test/libsolidity/smtCheckerTests/operators/assignment_module_contract_member_variable.sol
@@ -0,0 +1,31 @@
+==== Source: AASource ====
+pragma experimental SMTChecker;
+import "AASource" as AA;
+contract A {
+	int x;
+	int y;
+	function a() public {
+		require(A.x < 100);
+		AA.A.y = A.x++;
+		assert(A.y == AA.A.x - 1);
+		// Fails
+		assert(AA.A.y == 0);
+		A.y = ++AA.A.x;
+		assert(A.y == A.x);
+		delete AA.A.x;
+		assert(A.x == 0);
+		A.y = A.x--;
+		assert(AA.A.y == AA.A.x + 1);
+		A.y = --A.x;
+		assert(A.y == A.x);
+		AA.A.x += 10;
+		// Fails
+		assert(A.y == 0);
+		assert(A.y + 10 == A.x);
+		A.x -= 10;
+		assert(AA.A.y == A.x);
+	}
+}
+// ----
+// Warning 6328: (AASource:191-210): CHC: Assertion violation happens here.
+// Warning 6328: (AASource:402-418): CHC: Assertion violation happens here.
