18a464f4f42a9e6da1ec1c8f61c7805ba12eb28c
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-11-02 13:11:18 +0100

    Moving some tests to semantic tests.

---
+++ test/libsolidity/semanticTests/empty_for_loop.sol
@@ -0,0 +1,13 @@
+contract test {
+    function f() public returns(uint ret) {
+        ret = 1;
+        for (;;) {
+            ret += 1;
+            if (ret >= 10) break;
+        }
+    }
+}
+// ====
+// compileViaYul: also
+// ----
+// f() -> 10
