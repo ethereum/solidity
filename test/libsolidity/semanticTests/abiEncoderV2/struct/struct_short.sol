6915d9d2a87b4ae2dcca0a97f138f6c11d80bf7b
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-11-03 16:52:04 +0100

    Moving some struct tests to semanticTests

---
+++ test/libsolidity/semanticTests/abiEncoderV2/struct/struct_short.sol
@@ -0,0 +1,13 @@
+pragma experimental ABIEncoderV2;
+
+contract C {
+    struct S { int a; uint b; bytes16 c; }
+    function f(S memory s) public pure returns (S memory q) {
+        q = s;
+    }
+}
+// ====
+// compileViaYul: also
+// ----
+// f((int256,uint256,bytes16)): 0xff010, 0xff0002, "abcd" -> 0xff010, 0xff0002, "abcd"
+// f((int256,uint256,bytes16)): 0xff010, 0xff0002, 0x1111222233334444555566667777888800000000000000000000000000000000 -> 0xff010, 0xff0002, left(0x11112222333344445555666677778888)
---
+++ test/libsolidity/semanticTests/abiEncoderV2/struct/struct_validation.sol
@@ -0,0 +1,19 @@
+pragma experimental ABIEncoderV2;
+
+contract C {
+	struct S { int16 a; uint8 b; bytes2 c; }
+	function f(S memory s) public pure returns (uint a, uint b, uint c) {
+		assembly {
+			a := mload(s)
+			b := mload(add(s, 0x20))
+			c := mload(add(s, 0x40))
+		}
+	}
+}
+// ====
+// compileViaYul: also
+// ----
+// f((int16,uint8,bytes2)): 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff01, 0xff, "ab" -> 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff01, 0xff, "ab"
+// f((int16,uint8,bytes2)): 0xff010, 0xff, "ab" -> FAILURE
+// f((int16,uint8,bytes2)): 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff01, 0xff0002, "ab" -> FAILURE
+// f((int16,uint8,bytes2)): 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff01, 0xff, "abcd" -> FAILURE
