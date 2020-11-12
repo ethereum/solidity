11033c95361216c0e8ce4bb153465e18c3c55ecc
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-11-12 15:29:08 +0100

    Moving some bytes and array tests to semanticTests

---
+++ test/libsolidity/semanticTests/array/dynamic_array_cleanup.sol
@@ -0,0 +1,23 @@
+contract c {
+    uint[20] spacer;
+    uint[] dynamic;
+    function fill() public {
+        for (uint i = 0; i < 21; ++i)
+            dynamic.push(i + 1);
+    }
+    function halfClear() public {
+        while (dynamic.length > 5)
+            dynamic.pop();
+    }
+    function fullClear() public { delete dynamic; }
+}
+// ====
+// compileViaYul: also
+// ----
+// storage: empty
+// fill() ->
+// storage: nonempty
+// halfClear() ->
+// storage: nonempty
+// fullClear() ->
+// storage: empty
