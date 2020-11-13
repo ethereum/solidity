1428a939a667b7dd4cc1adc11f40939ee6ceb0ef
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-11-13 12:14:30 +0100

    Moving some more array tests to semanticTests.

---
+++ test/libsolidity/semanticTests/constructor/bytes_in_constructors_packer.sol
@@ -0,0 +1,28 @@
+contract Base {
+    uint public m_x;
+    bytes m_s;
+    constructor(uint x, bytes memory s) {
+        m_x = x;
+        m_s = s;
+    }
+    function part(uint i) public returns (byte) {
+        return m_s[i];
+    }
+}
+contract Main is Base {
+    constructor(bytes memory s, uint x) Base(x, f(s)) {}
+    function f(bytes memory s) public returns (bytes memory) {
+        return s;
+    }
+}
+contract Creator {
+    function f(uint x, bytes memory s) public returns (uint r, byte ch) {
+        Main c = new Main(s, x);
+        r = c.m_x();
+        ch = c.part(x);
+    }
+}
+// ====
+// compileViaYul: also
+// ----
+// f(uint256,bytes): 7, 0x40, 78, "abcdefghijklmnopqrstuvwxyzabcdef", "ghijklmnopqrstuvwxyzabcdefghijkl", "mnopqrstuvwxyz" -> 7, "h"
