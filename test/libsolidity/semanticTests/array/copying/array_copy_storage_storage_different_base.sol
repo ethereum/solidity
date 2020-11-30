bd86588459498105cbdca205557e328f04e8c936
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-11-30 17:55:20 +0100

    Adding more tests for array copying from storage to storage.

---
+++ test/libsolidity/semanticTests/array/copying/array_copy_storage_storage_different_base.sol
@@ -0,0 +1,19 @@
+contract c {
+    uint64[] data1;
+    uint256[] data2;
+
+    function test() public returns (uint256 x, uint256 y) {
+        data2.push(11);
+        data1.push(0);
+        data1.push(1);
+        data1.push(2);
+        data1.push(3);
+        data1.push(4);
+        data2 = data1;
+        assert(data1[0] == data2[0]);
+        x = data2.length;
+        y = data2[4];
+    }
+}
+// ----
+// test() -> 5, 4
