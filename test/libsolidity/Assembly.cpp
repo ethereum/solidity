dff7dcdc272fdd00a6ee8e749e2b321ebf230103
chriseth <chris@ethereum.org>
2020-12-02 15:49:41 +0100

    Update source location tests.

--- test/libsolidity/Assembly.cpp
+++ test/libsolidity/Assembly.cpp
@@ -173,18 +173,18 @@ BOOST_AUTO_TEST_CASE(location_test)
 	vector<SourceLocation> locations;
 	if (solidity::test::CommonOptions::get().optimize)
 		locations =
-			vector<SourceLocation>(31, SourceLocation{2, 82, sourceCode}) +
-			vector<SourceLocation>(21, SourceLocation{20, 79, sourceCode}) +
-			vector<SourceLocation>(1, SourceLocation{72, 74, sourceCode}) +
-			vector<SourceLocation>(2, SourceLocation{20, 79, sourceCode});
+			vector<SourceLocation>(31, SourceLocation{23, 103, sourceCode}) +
+			vector<SourceLocation>(21, SourceLocation{41, 100, sourceCode}) +
+			vector<SourceLocation>(1, SourceLocation{93, 95, sourceCode}) +
+			vector<SourceLocation>(2, SourceLocation{41, 100, sourceCode});
 	else
 		locations =
-			vector<SourceLocation>(hasShifts ? 31 : 32, SourceLocation{2, 82, sourceCode}) +
-			vector<SourceLocation>(24, SourceLocation{20, 79, sourceCode}) +
-			vector<SourceLocation>(1, SourceLocation{49, 58, sourceCode}) +
-			vector<SourceLocation>(1, SourceLocation{72, 74, sourceCode}) +
-			vector<SourceLocation>(2, SourceLocation{65, 74, sourceCode}) +
-			vector<SourceLocation>(2, SourceLocation{20, 79, sourceCode});
+			vector<SourceLocation>(hasShifts ? 31 : 32, SourceLocation{23, 103, sourceCode}) +
+			vector<SourceLocation>(24, SourceLocation{41, 100, sourceCode}) +
+			vector<SourceLocation>(1, SourceLocation{70, 79, sourceCode}) +
+			vector<SourceLocation>(1, SourceLocation{93, 95, sourceCode}) +
+			vector<SourceLocation>(2, SourceLocation{86, 95, sourceCode}) +
+			vector<SourceLocation>(2, SourceLocation{41, 100, sourceCode});
 	checkAssemblyLocations(items, locations);
 }
 
