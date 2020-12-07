8edd5d4fd960c5963d9e66b8366d279af7944f82
Kamil Śliwak <kamil.sliwak@codepoets.it>
2020-12-07 20:00:07 +0100

    Make printError() print to stderr

--- scripts/common.sh
+++ scripts/common.sh
@@ -23,11 +23,11 @@ if [ "$CIRCLECI" ]
 then
     export TERM="${TERM:-xterm}"
     function printTask() { echo "$(tput bold)$(tput setaf 2)$1$(tput setaf 7)"; }
-    function printError() { echo "$(tput setaf 1)$1$(tput setaf 7)"; }
+    function printError() { >&2 echo "$(tput setaf 1)$1$(tput setaf 7)"; }
     function printLog() { echo "$(tput setaf 3)$1$(tput setaf 7)"; }
 else
     function printTask() { echo "$(tput bold)$(tput setaf 2)$1$(tput sgr0)"; }
-    function printError() { echo "$(tput setaf 1)$1$(tput sgr0)"; }
+    function printError() { >&2 echo "$(tput setaf 1)$1$(tput sgr0)"; }
     function printLog() { echo "$(tput setaf 3)$1$(tput sgr0)"; }
 fi
 
