2d5a2c65a844f42906d9ea1d723f269e8cc106a9
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-12-09 18:02:25 +0100

    [Sol->Yul] Implementing getters for bytes and structs containing bytes member.
    Co-authored-by: chriseth <chris@ethereum.org>

---
+++ test/libsolidity/semanticTests/getters/bytes.sol
@@ -0,0 +1,10 @@
+contract C {
+    bytes public b;
+    constructor() {
+        b = "abc";
+    }
+}
+// ====
+// compileViaYul: also
+// ----
+// b() -> 0x20, 0x03, 0x6162630000000000000000000000000000000000000000000000000000000000
