ab74194ca3b6cacfee200f3fd3238a89b07d7236
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-12-09 19:29:11 +0100

    [Sol->Yul] Implementing empty byte array push.

--- test/libsolidity/semanticTests/array/copying/empty_bytes_copy.sol
+++ test/libsolidity/semanticTests/array/copying/empty_bytes_copy.sol
@@ -22,6 +22,8 @@ contract C {
         return data[0];
     }
 }
+// ====
+// compileViaYul: also
 // ----
 // fromMemory() -> 0x00
 // fromCalldata(bytes): 0x40, 0x60, 0x00, 0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff -> 0x00
