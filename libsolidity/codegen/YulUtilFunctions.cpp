8aa4568b10e6bde427fd5e84dfa21c01e74c78d9
Djordje Mijovic <djordje.mijovic@gmail.com>
2020-12-11 11:07:22 +0100

    [Sol->Yul] Implementing conversion from calldata slices to memory arrays.
    Co-authored-by: Kamil Śliwak <kamil.sliwak@codepoets.it>

--- libsolidity/codegen/YulUtilFunctions.cpp
+++ libsolidity/codegen/YulUtilFunctions.cpp
@@ -2852,14 +2852,17 @@ string YulUtilFunctions::conversionFunction(Type const& _from, Type const& _to)
 		solAssert(_from.dataStoredIn(DataLocation::CallData), "");
 		solAssert(_to.category() == Type::Category::Array, "");
 
-		ArraySliceType const& fromType = dynamic_cast<ArraySliceType const&>(_from);
-		ArrayType const& targetType = dynamic_cast<ArrayType const&>(_to);
+		auto const& fromType = dynamic_cast<ArraySliceType const&>(_from);
+		auto const& targetType = dynamic_cast<ArrayType const&>(_to);
 
 		solAssert(!fromType.arrayType().baseType()->isDynamicallyEncoded(), "");
 		solAssert(
 			*fromType.arrayType().baseType() == *targetType.baseType(),
 			"Converting arrays of different type is not possible"
 		);
+
+		if (!targetType.dataStoredIn(DataLocation::CallData))
+			return arrayConversionFunction(fromType.arrayType(), targetType);
 
 		string const functionName =
 			"convert_" +
