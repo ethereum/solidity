dbfd3db03840fbdd843a87ca0e086bb6dc8e7480
Kamil Śliwak <kamil.sliwak@codepoets.it>
2020-12-12 03:05:06 +0100

    Use mapfile instead of () for reading multiple lines as separate array items

--- scripts/docs_version_pragma_check.sh
+++ scripts/docs_version_pragma_check.sh
@@ -66,11 +66,12 @@ function versionEqual()
 function getAllAvailableVersions()
 {
     allVersions=()
-    local allListedVersions=( $(
+    local allListedVersions
+    mapfile -t allListedVersions <<< "$(
         wget -q -O- https://binaries.soliditylang.org/bin/list.txt |
         grep -Po '(?<=soljson-v)\d+.\d+.\d+(?=\+commit)' |
         sort -V
-    ) )
+    )"
     for listed in "${allListedVersions[@]}"
     do
         if versionGreater "$listed" "0.4.10"
