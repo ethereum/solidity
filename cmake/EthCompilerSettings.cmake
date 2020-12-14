843e37b6e641aa08fa83db78629e8b7d198707d7
chriseth <chris@ethereum.org>
2020-12-14 13:55:05 +0100

    Do not warn about unreachable break.

--- cmake/EthCompilerSettings.cmake
+++ cmake/EthCompilerSettings.cmake
@@ -60,7 +60,6 @@ if (("${CMAKE_CXX_COMPILER_ID}" MATCHES "GNU") OR ("${CMAKE_CXX_COMPILER_ID}" MA
 	eth_add_cxx_compiler_flag_if_supported(-Wfinal-dtor-non-final-class)
 	eth_add_cxx_compiler_flag_if_supported(-Wnewline-eof)
 	eth_add_cxx_compiler_flag_if_supported(-Wsuggest-destructor-override)
-	eth_add_cxx_compiler_flag_if_supported(-Wunreachable-code-break)
 	eth_add_cxx_compiler_flag_if_supported(-Wduplicated-cond)
 	eth_add_cxx_compiler_flag_if_supported(-Wduplicate-enum)
 	eth_add_cxx_compiler_flag_if_supported(-Wlogical-op)
