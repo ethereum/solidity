fdaede9b3e0d37985bba6894c582ffe1935860df
hrkrshnn <webmail.hari@gmail.com>
2020-12-15 12:45:32 +0100

    Tests after disallowing some explicit payable conversions

--- test/libsolidity/syntaxTests/types/address/address_uint_bytes20_this.sol
+++ test/libsolidity/syntaxTests/types/address/address_uint_bytes20_this.sol
@@ -4,16 +4,12 @@ contract C {
         address a2 = address(bytes20(0));
         address a3 = address(this);
 
-        address payable a4 = payable(uint160(0));
-        address payable a5 = payable(bytes20(0));
-        address payable a6 = payable(this);
-
         // Trivial conversions
-        address payable a7 = payable(address(uint160(0)));
-        address payable a8 = payable(address(bytes20(0)));
-        address payable a9 = payable(address(this));
+        address payable a4 = payable(address(uint160(0)));
+        address payable a5 = payable(address(bytes20(0)));
+        address payable a6 = payable(address(this));
 
-        a1; a2; a3; a4; a5; a6; a7; a8; a9;
+        a1; a2; a3; a4; a5; a6;
     }
 
     // to make payable(this) work
