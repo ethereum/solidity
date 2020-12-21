82b9344da8ec93d9da2d05db88a468ea142c1d4d
Kamil Śliwak <kamil.sliwak@codepoets.it>
2020-12-21 20:47:00 +0100

    prepare_report.js: Replace var with const/let + minor cleanup

--- scripts/bytecodecompare/prepare_report.js
+++ scripts/bytecodecompare/prepare_report.js
@@ -1,49 +1,46 @@
 #!/usr/bin/env node
-var process = require('process')
-var fs = require('fs')
+const process = require('process')
+const fs = require('fs')
 
-var compiler = require('./solc-js/wrapper.js')(require('./solc-js/soljson.js'))
+const compiler = require('./solc-js/wrapper.js')(require('./solc-js/soljson.js'))
 
-for (var optimize of [false, true])
+for (const optimize of [false, true])
 {
-    for (var filename of process.argv.slice(2))
+    for (const filename of process.argv.slice(2))
     {
         if (filename !== undefined)
         {
-            var inputs = {}
-            inputs[filename] = { content: fs.readFileSync(filename).toString() }
-            var input = {
+            const input = {
                 language: 'Solidity',
-                sources: inputs,
+                sources: {
+                    [filename]: {content: fs.readFileSync(filename).toString()}
+                },
                 settings: {
-                    optimizer: { enabled: optimize },
-                    outputSelection: { '*': { '*': ['evm.bytecode.object', 'metadata'] } },
-                    "modelChecker": { "engine": "none" }
+                    optimizer: {enabled: optimize},
+                    outputSelection: {'*': {'*': ['evm.bytecode.object', 'metadata']}},
+                    "modelChecker": {"engine": "none"}
                 }
             }
-            var result = JSON.parse(compiler.compile(JSON.stringify(input)))
+
+            const result = JSON.parse(compiler.compile(JSON.stringify(input)))
             if (
                 !('contracts' in result) ||
                 Object.keys(result['contracts']).length === 0 ||
                 !result['contracts'][filename] ||
                 Object.keys(result['contracts'][filename]).length === 0
             )
-            {
                 // NOTE: do not exit here because this may be run on source which cannot be compiled
                 console.log(filename + ': ERROR')
-            }
             else
-            {
-                for (var contractName in result['contracts'][filename])
+                for (const contractName in result['contracts'][filename])
                 {
-                    var contractData = result['contracts'][filename][contractName];
+                    const contractData = result['contracts'][filename][contractName];
                     if (contractData.evm !== undefined && contractData.evm.bytecode !== undefined)
                         console.log(filename + ':' + contractName + ' ' + contractData.evm.bytecode.object)
                     else
                         console.log(filename + ':' + contractName + ' NO BYTECODE')
                     console.log(filename + ':' + contractName + ' ' + contractData.metadata)
                 }
-            }
         }
     }
 }
