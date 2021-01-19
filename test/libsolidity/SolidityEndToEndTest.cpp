52511a21633d5595cb94e020b3185af1b589e3a5
chriseth <chris@ethereum.org>
2021-01-19 14:55:18 +0100

    Split shift bytes test.

--- test/libsolidity/SolidityEndToEndTest.cpp
+++ test/libsolidity/SolidityEndToEndTest.cpp
@@ -4427,27 +4427,6 @@ BOOST_AUTO_TEST_CASE(shift_bytes)
 	ABI_CHECK(callContractFunction("right(bytes20,uint8)", "12345678901234567890", 8 * 8), encodeArgs(string(8, 0) + "123456789012"));
 }
 
-BOOST_AUTO_TEST_CASE(shift_bytes_cleanup)
-{
-	char const* sourceCode = R"(
-		contract C {
-			function left(uint8 y) public returns (bytes20) {
-				bytes20 x;
-				assembly { x := "12345678901234567890abcde" }
-				return x << y;
-			}
-			function right(uint8 y) public returns (bytes20) {
-				bytes20 x;
-				assembly { x := "12345678901234567890abcde" }
-				return x >> y;
-			}
-		}
-	)";
-	compileAndRun(sourceCode, 0, "C");
-	ABI_CHECK(callContractFunction("left(uint8)", 8 * 8), encodeArgs("901234567890" + string(8, 0)));
-	ABI_CHECK(callContractFunction("right(uint8)", 8 * 8), encodeArgs(string(8, 0) + "123456789012"));
-}
-
 BOOST_AUTO_TEST_CASE(contracts_separated_with_comment)
 {
 	char const* sourceCode = R"(
