b6183edd1890b8416992b37e576848e47387616f
chriseth <chris@ethereum.org>
2021-02-02 16:31:36 +0100

    fixup! Syntax for custom errors.

--- test/libsolidity/syntaxTests/errors/error_reserved_name.sol
+++ test/libsolidity/syntaxTests/errors/error_reserved_name.sol
@@ -1,3 +1,4 @@
 // TODO do we want to disallow this at all?
 error Error(uint);
 // ----
+// SyntaxError 1855: (44-62): The built-in errors "Error" and "Panic" cannot be re-defined.
--- test/libsolidity/syntaxTests/errors/panic_reserved_name.sol
+++ test/libsolidity/syntaxTests/errors/panic_reserved_name.sol
@@ -2,3 +2,4 @@
 // TODO I Think the best way would be to have Error in the global scope.
 error Panic(bytes2);
 // ----
+// SyntaxError 1855: (131-151): The built-in errors "Error" and "Panic" cannot be re-defined.
