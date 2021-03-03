0340510c538b9b2171ad382d88082c5738793a60
Martin Blicha <martin.blicha@usi.ch>
2021-03-03 16:48:32 +0100

    [SMTChecker] correct handling of FixedBytes constants initialized with string literal

---
+++ test/libsolidity/smtCheckerTests/typecast/string_literal_to_fixed_bytes_constant_initialization_1.sol
@@ -0,0 +1,10 @@
+pragma experimental SMTChecker;
+
+contract MockContract {
+	bytes4 public constant SENTINEL_ANY_MOCKS = hex"01";
+	mapping(bytes4 => bytes4) methodIdMocks;
+
+	constructor() {
+		methodIdMocks[SENTINEL_ANY_MOCKS] = 0;
+	}
+}
