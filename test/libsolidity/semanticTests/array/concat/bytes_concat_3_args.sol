84c05d35f3188ef72d86a342c554936148e35e8f
Alex Beregszaszi <alex@rtfs.hu>
2021-03-12 23:02:36 +0000

    Trivial isoltest updates: normalized whitespace

--- test/libsolidity/semanticTests/array/concat/bytes_concat_3_args.sol
+++ test/libsolidity/semanticTests/array/concat/bytes_concat_3_args.sol
@@ -6,5 +6,5 @@ contract C {
 // ====
 // compileViaYul: also
 // ----
-// f(bytes, bytes, bytes): 0x60, 0xa0, 0xe0, 32, "abcdabcdabcdabcdabcdabcdabcdabcd", 5, "bcdef", 3, "abc" -> 0x20, 40, "abcdabcdabcdabcdabcdabcdabcdabcd", "bcdefabc"
-// f(bytes, bytes, bytes): 0x60, 0xa0, 0xe0, 3, "abc", 2, "de", 3, "fgh" -> 0x20, 8, "abcdefgh"
+// f(bytes,bytes,bytes): 0x60, 0xa0, 0xe0, 32, "abcdabcdabcdabcdabcdabcdabcdabcd", 5, "bcdef", 3, "abc" -> 0x20, 40, "abcdabcdabcdabcdabcdabcdabcdabcd", "bcdefabc"
+// f(bytes,bytes,bytes): 0x60, 0xa0, 0xe0, 3, "abc", 2, "de", 3, "fgh" -> 0x20, 8, "abcdefgh"
