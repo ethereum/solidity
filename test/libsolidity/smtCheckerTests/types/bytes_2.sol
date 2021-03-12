6aa6c5f5f946729eea5839dbf62551af04bd57a3
Martin Blicha <martin.blicha@usi.ch>
2021-03-12 19:33:45 +0100

    [SMTChecker] Reset reference variables on assignment to a variable of reference type

--- test/libsolidity/smtCheckerTests/types/bytes_2.sol
+++ test/libsolidity/smtCheckerTests/types/bytes_2.sol
@@ -7,3 +7,5 @@ contract C
 		assert(b1[1] == b2[1]);
 	}
 }
+// ----
+// Warning 6328: (119-141): CHC: Assertion violation happens here.\nCounterexample:\n\n\nTransaction trace:\nC.constructor()\nC.f(b1, b2)
