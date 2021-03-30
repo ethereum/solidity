48ce2637436ec1c03b2a17d2c0a7a594d430a3e2
anurag4u80 <anurag4u80@gmail.com>
2021-03-30 17:23:04 +0530

    Replaced adaptors::reverse with views::reverse

--- libsolidity/formal/CHC.cpp
+++ libsolidity/formal/CHC.cpp
@@ -189,7 +189,7 @@ void CHC::endVisit(ContractDefinition const& _contract)
 		}
 	m_errorDest = nullptr;
 	// Then call initializer_Base from base -> derived
-	for (auto base: ranges::views::reverse(_contract.annotation().linearizedBaseContracts))
+	for (auto base: _contract.annotation().linearizedBaseContracts | ranges::views::reverse)
 	{
 		errorFlag().increaseIndex();
 		m_context.addAssertion(smt::constructorCall(*m_contractInitializers.at(&_contract).at(base), m_context));
