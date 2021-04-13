1df304b92d73fc2978eedca7e7a90ecd14dd7c6f
Bhargava Shastry <bhargava.shastry@ethereum.org>
2021-04-13 15:23:41 +0200

    Remove large literals

--- test/tools/ossfuzz/protoToYul.h
+++ test/tools/ossfuzz/protoToYul.h
@@ -138,7 +138,7 @@ private:
 	/// Adds @a _vars to current scope
 	void addVarsToScope(std::vector<std::string> const& _vars);
 
-	std::string createHex(std::string const& _hexBytes);
+//	std::string createHex(std::string const& _hexBytes);
 
 	/// Returns a new variable name.
 	std::string newVarName()
@@ -148,7 +148,7 @@ private:
 
 	/// Accepts an arbitrary string, removes all characters that are neither
 	/// alphabets nor digits from it and returns the said string.
-	static std::string createAlphaNum(std::string const& _strBytes);
+//	static std::string createAlphaNum(std::string const& _strBytes);
 
 	enum class NumFunctionReturns: unsigned
 	{
@@ -271,7 +271,7 @@ private:
 	///		index = (m_inputSize * m_inputSize + counter) % dictionarySize
 	/// where m_inputSize is the size of the protobuf input and
 	/// dictionarySize is the total number of entries in the dictionary.
-	std::string dictionaryToken(util::HexPrefix _p = util::HexPrefix::Add);
+	std::string dictionaryToken();
 
 	/// Returns an EVMVersion object corresponding to the protobuf
 	/// enum of type Program_Version
