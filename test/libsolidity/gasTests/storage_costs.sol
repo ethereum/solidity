d80a92752a6d31da151bccc5e196a587fdc7650b
hrkrshnn <webmail.hari@gmail.com>
2021-04-13 10:25:01 +0200

    Added a gas test for storage costs

---
+++ test/libsolidity/gasTests/storage_costs.sol
@@ -0,0 +1,24 @@
+contract C {
+    uint x;
+    function setX(uint y) public {
+        x = y;
+    }
+    function resetX() public {
+        x = 0;
+    }
+    function readX() public view returns(uint) {
+        return x;
+    }
+}
+// ====
+// optimize: true
+// optimize-yul: true
+// ----
+// creation:
+//   codeDepositCost: 27000
+//   executionCost: 81
+//   totalCost: 27081
+// external:
+//   readX(): 990
+//   resetX(): 5116
+//   setX(uint256): 20212
