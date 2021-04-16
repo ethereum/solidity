07b927018523a2b9423c82b02ec62587754a0853
Christian Parpart <christian@parpart.family>
2021-04-16 13:23:18 +0200

    liblangutil/LineColumn: adds missing convinience ctor.

--- liblangutil/SourceReferenceExtractor.h
+++ liblangutil/SourceReferenceExtractor.h
@@ -37,6 +37,7 @@ struct LineColumn
 
 	LineColumn() = default;
 	LineColumn(std::tuple<int, int> const& _t): line{std::get<0>(_t)}, column{std::get<1>(_t)} {}
+	LineColumn(int _line, int _column): line{_line}, column{_column} {}
 };
 
 struct SourceReference
