cf5510ca7f14582ba05d34a1e38ec2a8204874e4
Alex Beregszaszi <alex@rtfs.hu>
2021-05-14 21:53:08 +0100

    Update tests

--- test/libsolidity/syntaxTests/license/license_unicode.sol
+++ test/libsolidity/syntaxTests/license/license_unicode.sol
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: ⡉⡊⡋⡌⡍⡎⡏⡐⡑⡒
 contract C {}
 // ----
+// ParserError 5406: SPDX license identifier is ill-formatted. Please see https://spdx.org for more information.
 // Warning 1878: SPDX license identifier not provided in source file. Before publishing, consider adding a comment containing "SPDX-License-Identifier: <SPDX-License>" to each source file. Use "SPDX-License-Identifier: UNLICENSED" for non-open-source code. Please see https://spdx.org for more information.
