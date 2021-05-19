85e3fcb1bf7893aceb78c104e89dcc24138bc445
Alexander Arlt <alexander.arlt@arlt-labs.com>
2021-05-18 23:19:16 -0500

    [isoltest] Extract event specific end-to-end tests.

--- test/libsolidity/semanticTests/events/event_anonymous_with_signature_collision2.sol
+++ test/libsolidity/semanticTests/events/event_anonymous_with_signature_collision2.sol
@@ -1,11 +1,12 @@
 contract ClientReceipt {
+    event Withdraw(uint _value, string owner);
     event Deposit(uint256 indexed _from, bytes32 indexed _id, uint _value) anonymous;
     function deposit(bytes32 _id) public payable {
-        emit Deposit(0x2012159ca6b6372f102c535a4814d13a00bfc5568ddfd72151364061b00355d1, _id, msg.value); // 0x2012159c -> 'Deposit(uint256,bytes32,uint256)'
+        emit Deposit(0x5ddaa77ac5bda319ba947e31bee594711f39ed1b20d079d438dbad5ed729fb30, _id, msg.value); // 0x5ddaa77a -> 'Withdraw(uint256,string)'
     }
 }
 // ====
 // compileViaYul: also
 // ----
 // deposit(bytes32), 18 wei: 0x1234 ->
-// ~ emit <anonymous>: #0x2012159ca6b6372f102c535a4814d13a00bfc5568ddfd72151364061b00355d1, #0x1234, 0x12
+// ~ emit Withdraw(uint256,string): #0x1234, 0x12
