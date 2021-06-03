cc915f023e560a5a89053f191ada5d2f02fd6947
hrkrshnn <webmail.hari@gmail.com>
2021-06-03 10:36:17 +0200

    Errors about nested mapping in event parameter are fatal.
    (Also applies for error parameters.)

--- test/libsolidity/syntaxTests/errors/no_mappings.sol
+++ test/libsolidity/syntaxTests/errors/no_mappings.sol
@@ -4,6 +4,3 @@ contract C {
 }
 // ----
 // TypeError 3448: (14-35): Type containing a (nested) mapping is not allowed as error parameter type.
-// TypeError 3417: (14-35): Internal or recursive type is not allowed as error parameter type.
-// TypeError 3448: (70-91): Type containing a (nested) mapping is not allowed as error parameter type.
-// TypeError 3417: (70-91): Internal or recursive type is not allowed as error parameter type.
