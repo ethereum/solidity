fa3696878bdabbced2ffe383b81c360e850784f1
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-06-25 20:48:34 +0200

    Fix crash when passing empty strings to bytes.concat()

--- libsolidity/codegen/ExpressionCompiler.cpp
+++ libsolidity/codegen/ExpressionCompiler.cpp
@@ -1083,7 +1083,7 @@ bool ExpressionCompiler::visit(FunctionCall const& _functionCall)
 					targetTypes.emplace_back(argument->annotation().type);
 				else if (
 					auto const* literalType = dynamic_cast<StringLiteralType const*>(argument->annotation().type);
-					literalType && literalType->value().size() <= 32
+					literalType && !literalType->value().empty() && literalType->value().size() <= 32
 				)
 					targetTypes.emplace_back(TypeProvider::fixedBytes(static_cast<unsigned>(literalType->value().size())));
 				else if (auto const* literalType = dynamic_cast<RationalNumberType const*>(argument->annotation().type))
