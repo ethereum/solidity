543ccf52871dfafd2d7880354c14968ad755d45f
hrkrshnn <webmail.hari@gmail.com>
2021-07-12 16:49:53 +0200

    Added the basefee instruction in YulInterpreter

--- test/tools/yulInterpreter/EVMInstructionInterpreter.cpp
+++ test/tools/yulInterpreter/EVMInstructionInterpreter.cpp
@@ -220,6 +220,8 @@ u256 EVMInstructionInterpreter::eval(
 		return m_state.gasprice;
 	case Instruction::CHAINID:
 		return m_state.chainid;
+	case Instruction::BASEFEE:
+		return m_state.basefee;
 	case Instruction::EXTCODESIZE:
 		return u256(keccak256(h256(arg[0]))) & 0xffffff;
 	case Instruction::EXTCODEHASH:
