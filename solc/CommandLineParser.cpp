b7c124911a953b611fde05927361024ea43290b5
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-08-03 15:50:02 +0200

    CommandLineParser: Process linker mode before assembly mode

--- solc/CommandLineParser.cpp
+++ solc/CommandLineParser.cpp
@@ -948,6 +948,9 @@ General Information)").c_str(),
 			if (!parseLibraryOption(library))
 				return false;
 
+	if (m_options.input.mode == InputMode::Linker)
+		return true;
+
 	if (m_args.count(g_strEVMVersion))
 	{
 		string versionOptionStr = m_args[g_strEVMVersion].as<string>();
@@ -1077,9 +1080,6 @@ General Information)").c_str(),
 		serr() << "are only valid in assembly mode." << endl;
 		return false;
 	}
-
-	if (m_options.input.mode == InputMode::Linker)
-		return true;
 
 	if (m_args.count(g_strMetadataHash))
 	{
