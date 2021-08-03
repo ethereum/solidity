ebd8dd2a99cc6a15d5c39d1c2a3ff4b1057103b6
chriseth <chris@ethereum.org>
2021-08-03 14:26:35 +0200

    Update tests.

--- test/libsolidity/syntaxTests/nameAndTypeResolution/317_fixed_type_valid_explicit_conversions.sol
+++ test/libsolidity/syntaxTests/nameAndTypeResolution/317_fixed_type_valid_explicit_conversions.sol
@@ -1,10 +1,8 @@
 contract test {
-    function f() public {
+    function f() public pure {
         ufixed256x18 a = ufixed256x18(1/3); a;
         ufixed248x18 b = ufixed248x18(1/3); b;
         ufixed8x1 c = ufixed8x1(1/3); c;
     }
 }
 // ----
-// UnimplementedFeatureError: Not yet implemented - FixedPointType.
-// Warning 2018: (20-182): Function state mutability can be restricted to pure
