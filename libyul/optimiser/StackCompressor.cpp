6205b3a9d2b243cb67c803c42861d008005fe62f
Daniel Kirchner <daniel@ekpyron.org>
2021-08-12 17:17:21 +0200

    Compatibility with StackCompressor and StackLimitEvader.

--- libyul/optimiser/StackCompressor.cpp
+++ libyul/optimiser/StackCompressor.cpp
@@ -27,6 +27,13 @@
 #include <libyul/optimiser/Metrics.h>
 #include <libyul/optimiser/Semantics.h>
 
+#include <libyul/backends/evm/ControlFlowGraphBuilder.h>
+#include <libyul/backends/evm/StackHelpers.h>
+#include <libyul/backends/evm/StackLayoutGenerator.h>
+
+#include <libyul/AsmAnalysis.h>
+#include <libyul/AsmAnalysisInfo.h>
+
 #include <libyul/CompilabilityChecker.h>
 
 #include <libyul/AST.h>
@@ -162,6 +169,50 @@ void eliminateVariables(
 	UnusedPruner::runUntilStabilised(_dialect, _node, _allowMSizeOptimization);
 }
 
+void eliminateVariables(
+	Dialect const& _dialect,
+	Block& _block,
+	vector<StackLayoutGenerator::StackTooDeep> const& _unreachables,
+	bool _allowMSizeOptimization
+)
+{
+	RematCandidateSelector selector{_dialect};
+	selector(_block);
+	std::map<YulString, size_t> candidates;
+	for (auto [cost, candidatesWithCost]: selector.candidates())
+		for (auto candidate: candidatesWithCost)
+			candidates[get<0>(candidate)] = cost;
+
+	set<YulString> varsToEliminate;
+
+	// TODO: this currently ignores the fact that variables may reference other variables we want to eliminate.
+	for (auto const& unreachable: _unreachables)
+	{
+		map<size_t, vector<YulString>> suitableCandidates;
+		size_t neededSlots = unreachable.deficit;
+		for (auto varName: unreachable.variableChoices)
+		{
+			if (varsToEliminate.count(varName))
+				--neededSlots;
+			else if (size_t* cost = util::valueOrNullptr(candidates, varName))
+				if (!util::contains(suitableCandidates[*cost], varName))
+					suitableCandidates[*cost].emplace_back(varName);
+		}
+		for (auto candidatesByCost: suitableCandidates)
+		{
+			for (auto candidate: candidatesByCost.second)
+				if (neededSlots--)
+					varsToEliminate.emplace(candidate);
+				else
+					break;
+			if (!neededSlots)
+				break;
+		}
+	}
+	Rematerialiser::run(_dialect, _block, std::move(varsToEliminate), true);
+	UnusedPruner::runUntilStabilised(_dialect, _block, _allowMSizeOptimization);
+}
+
 }
 
 bool StackCompressor::run(
@@ -176,39 +227,66 @@ bool StackCompressor::run(
 		_object.code->statements.size() > 0 && holds_alternative<Block>(_object.code->statements.at(0)),
 		"Need to run the function grouper before the stack compressor."
 	);
+	bool usesOptimizedCodeGenerator = false;
+	if (auto evmDialect = dynamic_cast<EVMDialect const*>(&_dialect))
+		usesOptimizedCodeGenerator =
+			_optimizeStackAllocation &&
+			evmDialect->evmVersion().canOverchargeGasForCall() &&
+			evmDialect->providesObjectAccess();
 	bool allowMSizeOptimzation = !MSizeFinder::containsMSize(_dialect, *_object.code);
-	for (size_t iterations = 0; iterations < _maxIterations; iterations++)
+	if (usesOptimizedCodeGenerator)
 	{
-		map<YulString, int> stackSurplus = CompilabilityChecker(_dialect, _object, _optimizeStackAllocation).stackDeficit;
-		if (stackSurplus.empty())
-			return true;
-
-		if (stackSurplus.count(YulString{}))
-		{
-			yulAssert(stackSurplus.at({}) > 0, "Invalid surplus value.");
-			eliminateVariables(
-				_dialect,
-				std::get<Block>(_object.code->statements.at(0)),
-				static_cast<size_t>(stackSurplus.at({})),
-				allowMSizeOptimzation
-			);
-		}
-
+		yul::AsmAnalysisInfo analysisInfo = yul::AsmAnalyzer::analyzeStrictAssertCorrect(_dialect, _object);
+		unique_ptr<CFG> cfg = ControlFlowGraphBuilder::build(analysisInfo, _dialect, *_object.code);
+		Block& mainBlock = std::get<Block>(_object.code->statements.at(0));
+		if (
+			auto stackTooDeepErrors = StackLayoutGenerator::reportStackTooDeep(*cfg, YulString{});
+			!stackTooDeepErrors.empty()
+		)
+			eliminateVariables(_dialect, mainBlock, stackTooDeepErrors, allowMSizeOptimzation);
 		for (size_t i = 1; i < _object.code->statements.size(); ++i)
 		{
 			auto& fun = std::get<FunctionDefinition>(_object.code->statements[i]);
-			if (!stackSurplus.count(fun.name))
-				continue;
-
-			yulAssert(stackSurplus.at(fun.name) > 0, "Invalid surplus value.");
-			eliminateVariables(
-				_dialect,
-				fun,
-				static_cast<size_t>(stackSurplus.at(fun.name)),
-				allowMSizeOptimzation
-			);
+			if (
+				auto stackTooDeepErrors = StackLayoutGenerator::reportStackTooDeep(*cfg, fun.name);
+				!stackTooDeepErrors.empty()
+			)
+				eliminateVariables(_dialect, fun.body, stackTooDeepErrors, allowMSizeOptimzation);
 		}
 	}
+	else
+		for (size_t iterations = 0; iterations < _maxIterations; iterations++)
+		{
+			map<YulString, int> stackSurplus = CompilabilityChecker(_dialect, _object, _optimizeStackAllocation).stackDeficit;
+			if (stackSurplus.empty())
+				return true;
+
+			if (stackSurplus.count(YulString{}))
+			{
+				yulAssert(stackSurplus.at({}) > 0, "Invalid surplus value.");
+				eliminateVariables(
+					_dialect,
+					std::get<Block>(_object.code->statements.at(0)),
+					static_cast<size_t>(stackSurplus.at({})),
+					allowMSizeOptimzation
+				);
+			}
+
+			for (size_t i = 1; i < _object.code->statements.size(); ++i)
+			{
+				auto& fun = std::get<FunctionDefinition>(_object.code->statements[i]);
+				if (!stackSurplus.count(fun.name))
+					continue;
+
+				yulAssert(stackSurplus.at(fun.name) > 0, "Invalid surplus value.");
+				eliminateVariables(
+					_dialect,
+					fun,
+					static_cast<size_t>(stackSurplus.at(fun.name)),
+					allowMSizeOptimzation
+				);
+			}
+		}
 	return false;
 }
 
