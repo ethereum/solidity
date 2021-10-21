26abea876b67ff0d2b450404fe660704aa7544af
David Dzhalaev <72649244+DavidRomanovizc@users.noreply.github.com>
2021-10-22 00:33:22 +0300

    Update
    Since the definition of a nested namespace is an extension of C++17, we need to define each namespace separately

--- libsolidity/interface/DebugSettings.h
+++ libsolidity/interface/DebugSettings.h
@@ -22,18 +22,17 @@
 #pragma once
 
 #include <cstddef>
-#include <string>
 #include <optional>
+#include <string>
 
-namespace solidity::frontend
+namespace solidity { namespace frontend
 {
-
 enum class RevertStrings
 {
 
-	Default, // no compiler-generated strings, keep user-supplied strings
-	Strip, // no compiler-generated strings, remove user-supplied strings (if possible)
-	Debug, // add strings for internal reverts, keep user-supplied strings
+	Default,	 // no compiler-generated strings, keep user-supplied strings
+	Strip,		 // no compiler-generated strings, remove user-supplied strings (if possible)
+	Debug,		 // add strings for internal reverts, keep user-supplied strings
 	VerboseDebug // add strings for internal reverts, add user-supplied strings if not provided
 };
 
@@ -41,8 +40,29 @@ inline std::string revertStringsToString(RevertStrings _str)
 {
 	switch (_str)
 	{
-	case RevertStrings::Default: return "default";
-	case RevertStrings::Strip: return "strip";
+	case RevertStrings::Default:
+		return "default";
+	case RevertStrings::Strip:
+		return "strip";
+	case RevertStrings::Debug:
+		return "debug";
+	case RevertStrings::VerboseDebug:
+		return "verboseDebug";
+	}
+	// Cannot reach this.
+	return "INVALID";
+}
+
+inline std::optional<RevertStrings> revertStringsFromString(std::string const& _str)
+{
+	for (auto i: {RevertStrings::Default, RevertStrings::Strip, RevertStrings::Debug, RevertStrings::VerboseDebug})
+		if (revertStringsToString(i) == _str)
+			return i;
+	return std::nullopt;
+}
+
+} }
+
 	case RevertStrings::Debug: return "debug";
 	case RevertStrings::VerboseDebug: return "verboseDebug";
 	}
