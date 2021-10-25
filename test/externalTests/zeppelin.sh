44bc1fa1737d7275cae19d96a3623d1e5887a115
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-10-25 12:59:55 +0200

    Run external tests on latest upstream code where possible

--- test/externalTests/zeppelin.sh
+++ test/externalTests/zeppelin.sh
@@ -33,7 +33,7 @@ function zeppelin_test
     OPTIMIZER_LEVEL=1
     CONFIG="truffle-config.js"
 
-    truffle_setup "$SOLJSON" https://github.com/solidity-external-tests/openzeppelin-contracts.git master_080
+    truffle_setup "$SOLJSON" https://github.com/OpenZeppelin/openzeppelin-contracts.git master
     run_install "$SOLJSON" install_fn
 
     truffle_run_test "$SOLJSON" compile_fn test_fn
