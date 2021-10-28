67827188a97e272c52f964706ad229c05bf9f855
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-10-28 13:28:07 +0200

    [DEBUG] Test CI run with all jobs using 4 cores (medium resource class)

--- .circleci/build_win.ps1
+++ .circleci/build_win.ps1
@@ -20,7 +20,7 @@ cd build
 $boost_dir=(Resolve-Path $PSScriptRoot\..\deps\boost\lib\cmake\Boost-*)
 ..\deps\cmake\bin\cmake -G "Visual Studio 16 2019" -DBoost_DIR="$boost_dir\" -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded -DCMAKE_INSTALL_PREFIX="$PSScriptRoot\..\upload" -DUSE_Z3=OFF ..
 if ( -not $? ) { throw "CMake configure failed." }
-msbuild solidity.sln /p:Configuration=Release /m:5 /v:minimal
+msbuild solidity.sln /p:Configuration=Release /m:10 /v:minimal
 if ( -not $? ) { throw "Build failed." }
-..\deps\cmake\bin\cmake --build . -j 5 --target install --config Release
+..\deps\cmake\bin\cmake --build . -j 10 --target install --config Release
 if ( -not $? ) { throw "Install target failed." }
