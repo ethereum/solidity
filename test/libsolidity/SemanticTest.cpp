077b3a003d65e498e2bc7f5d0f421aba75d451f3
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-11-15 14:36:13 +0100

    Fix `--enforce-gas-cost-min-value` being applied only when the cost expectations are not provided in the test file

--- test/libsolidity/SemanticTest.cpp
+++ test/libsolidity/SemanticTest.cpp
@@ -565,14 +565,14 @@ bool SemanticTest::checkGasCostExpectation(TestFunctionCall& io_test, bool _comp
 	// We don't check gas if enforce gas cost is not active
 	// or test is run with abi encoder v1 only
 	// or gas used less than threshold for enforcing feature
+	// or the test has used up all available gas (test will fail anyway)
 	// or setting is "ir" and it's not included in expectations
 	// or if the called function is an isoltest builtin e.g. `smokeTest` or `storageEmpty`
 	if (
 		!m_enforceGasCost ||
-		(
-			(setting == "ir" || m_gasUsed < m_enforceGasCostMinValue || m_gasUsed >= m_gas) &&
-			io_test.call().expectations.gasUsed.count(setting) == 0
-		) ||
+		m_gasUsed < m_enforceGasCostMinValue ||
+		m_gasUsed >= m_gas ||
+		(setting == "ir" && io_test.call().expectations.gasUsed.count(setting) == 0) ||
 		io_test.call().kind == FunctionCall::Kind::Builtin
 	)
 		return true;
