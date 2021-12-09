6afb63200c704ec019ec0f7c3e8d5b1db30cbba2
Kamil Śliwak <kamil.sliwak@codepoets.it>
2021-12-09 16:36:09 +0100

    externalTests: Use `yarn install` instead of `yarn` in colony ext test to avoid compiling contracts twice

--- test/externalTests/colony.sh
+++ test/externalTests/colony.sh
@@ -49,7 +49,7 @@ function colony_test
 
     neutralize_package_json_hooks
     force_truffle_compiler_settings "$config_file" "$BINARY_TYPE" "${DIR}/solc" "$min_optimizer_level"
-    yarn
+    yarn install
     git submodule update --init
 
     cd lib
