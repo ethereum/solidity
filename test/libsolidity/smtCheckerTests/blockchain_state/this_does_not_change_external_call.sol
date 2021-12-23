fb8c138b8bb85d603f0ce4aef3170b63129d8887
Leo Alt <leo@ethereum.org>
2021-12-24 00:44:15 +0100

    Do not analyze unecessary contracts

--- test/libsolidity/smtCheckerTests/blockchain_state/this_does_not_change_external_call.sol
+++ test/libsolidity/smtCheckerTests/blockchain_state/this_does_not_change_external_call.sol
@@ -17,5 +17,4 @@ contract C {
 // ====
 // SMTEngine: all
 // SMTIgnoreOS: macos
-// ----
-// Info 1180: Contract invariant(s) for :C:\n(((address(this) + ((- 1) * t)) <= 0) && ((address(this) + ((- 1) * t)) >= 0))\nReentrancy property(ies) for :C:\n((!((t + ((- 1) * address(this))) = 0) || (<errorCode> <= 0)) && (!((t + ((- 1) * address(this))) <= 0) || ((t' + ((- 1) * address(this))) <= 0)) && (!((t + ((- 1) * address(this))) >= 0) || ((address(this) + ((- 1) * t')) <= 0)))\n((!(<errorCode> >= 2) || !((t + ((- 1) * address(this))) = 0)) && (!((t + ((- 1) * address(this))) <= 0) || ((t' + ((- 1) * address(this))) <= 0)) && (!((t + ((- 1) * address(this))) >= 0) || ((address(this) + ((- 1) * t')) <= 0)))\n<errorCode> = 0 -> no errors\n<errorCode> = 1 -> Assertion failed at assert(address(this) == t)\n<errorCode> = 2 -> Assertion failed at assert(a == t)\n
+// SMTIgnoreInv: yes
