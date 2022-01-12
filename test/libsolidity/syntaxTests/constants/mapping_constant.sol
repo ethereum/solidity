c0313fce94cb40f85cf7602c981476c0e9884d32
Marenz <github@supradigital.org>
2022-01-12 16:30:04 +0100

    Fix genetic algorithms CI failure

--- test/libsolidity/syntaxTests/constants/mapping_constant.sol
+++ test/libsolidity/syntaxTests/constants/mapping_constant.sol
@@ -1,3 +1,3 @@
 mapping(uint => uint) constant b = b;
 // ----
-// TypeError 9259: (0-36): Only constants of value type and byte arrays are implemented.
+// TypeError 9259: (0-36): Only constants of value type and byte array type are implemented.
--- test/libsolidity/syntaxTests/nameAndTypeResolution/constant_mapping.sol
+++ test/libsolidity/syntaxTests/nameAndTypeResolution/constant_mapping.sol
@@ -2,4 +2,4 @@ contract C {
     mapping(uint => uint) constant x;
 }
 // ----
-// TypeError 9259: (17-49): Only constants of value type and byte arrays are implemented.
+// TypeError 9259: (17-49): Only constants of value type and byte array type are implemented.
