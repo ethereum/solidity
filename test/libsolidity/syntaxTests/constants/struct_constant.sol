57d84c8bfb6cad75f13ddfe4a5aee4ed97300f51
Marenz <github@supradigital.org>
2022-01-12 16:30:04 +0100

    Fix genetic algorithms CI failure

--- test/libsolidity/syntaxTests/constants/struct_constant.sol
+++ test/libsolidity/syntaxTests/constants/struct_constant.sol
@@ -1,4 +1,4 @@
 struct S { uint x; }
 S constant s;
 // ----
-// TypeError 9259: (21-33): Only constants of value type and byte arrays are implemented.
+// TypeError 9259: (21-33): Only constants of value type and byte array type are implemented.
