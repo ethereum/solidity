b710bfafa35859d5396534f6b0d6d9810e89372d
Daniel Kirchner <daniel@ekpyron.org>
2022-01-17 20:12:11 +0100

    Update test/libsolidity/syntaxTests/inlineAssembly/assignment_to_function_pointer.sol
    Co-authored-by: Kamil Śliwak <kamil.sliwak@codepoets.it>

--- test/libsolidity/syntaxTests/inlineAssembly/assignment_to_function_pointer.sol
+++ test/libsolidity/syntaxTests/inlineAssembly/assignment_to_function_pointer.sol
@@ -1,9 +1,9 @@
 contract C {
-  function f() public pure {
-    function() external g;
-    assembly {
-      g.address := 0x42
-      g.selector := 0x23
+    function f() public pure {
+        function() external g;
+        assembly {
+            g.address := 0x42
+            g.selector := 0x23
+        }
     }
-  }
 }
