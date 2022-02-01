79f77215d989982cd862a8eec1369e95d197bf1c
Kamil Śliwak <kamil.sliwak@codepoets.it>
2022-02-01 15:25:13 +0100

    Add missing SELECTED_PRESETS argument to some of the recently added external tests

--- test/externalTests/elementfi.sh
+++ test/externalTests/elementfi.sh
@@ -27,6 +27,7 @@ source test/externalTests/common.sh
 verify_input "$@"
 BINARY_TYPE="$1"
 BINARY_PATH="$2"
+SELECTED_PRESETS="$3"
 
 function compile_fn { npm run build; }
 function test_fn { npm run test; }
