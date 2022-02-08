d51d0d93d304c3a1342efc4b67730af5ff063375
Daniel Kirchner <daniel@ekpyron.org>
2022-02-08 13:16:20 +0100

    Adjust mini-lz4.js and base64DecToArr.js.

--- scripts/ci/base64DecToArr.js
+++ scripts/ci/base64DecToArr.js
@@ -1,3 +1,4 @@
+function base64DecToArr (sBase64) {
 /*\
 |*|
 |*|  Base64 / binary data / UTF-8 strings utilities
@@ -25,15 +26,13 @@ function b64ToUint6 (nChr) {
 
 }
 
-function base64DecToArr (sBase64, nBlocksSize) {
-
   var
-    sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, ""), nInLen = sB64Enc.length,
-    nOutLen = nBlocksSize ? Math.ceil((nInLen * 3 + 1 >> 2) / nBlocksSize) * nBlocksSize : nInLen * 3 + 1 >> 2, taBytes = new Uint8Array(nOutLen);
+    nInLen = sBase64.length,
+    nOutLen = nInLen * 3 + 1 >> 2, taBytes = new Uint8Array(nOutLen);
 
   for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
     nMod4 = nInIdx & 3;
-    nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 6 * (3 - nMod4);
+    nUint24 |= b64ToUint6(sBase64.charCodeAt(nInIdx)) << 6 * (3 - nMod4);
     if (nMod4 === 3 || nInLen - nInIdx === 1) {
       for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
         taBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
