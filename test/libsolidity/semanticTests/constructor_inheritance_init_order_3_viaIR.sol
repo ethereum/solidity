290b9326e387a231932dc93af1273bd8a5b9dbc1
Daniel Kirchner <daniel@ekpyron.org>
2022-02-10 13:38:17 +0100

    Move stray semantics test to proper directory.

---
+++ test/libsolidity/semanticTests/constructor_inheritance_init_order_3_viaIR.sol
@@ -0,0 +1,12 @@
+contract A {
+    uint public x = 2;
+    constructor(uint) {}
+    function f() public returns(uint) { x = 4; }
+}
+contract B is A {
+    constructor() A(f()) {}
+}
+// ====
+// compileViaYul: true
+// ----
+// x() -> 2
