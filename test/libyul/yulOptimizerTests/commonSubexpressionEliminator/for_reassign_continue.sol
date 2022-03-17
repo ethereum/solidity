b98c663f95e748a2e6da3c43a7ea9c9763cda680
chriseth <chris@ethereum.org>
2022-03-17 17:27:20 +0100

    our tests

--- test/libyul/yulOptimizerTests/commonSubexpressionEliminator/for_reassign_continue.sol
+++ test/libyul/yulOptimizerTests/commonSubexpressionEliminator/for_reassign_continue.sol
@@ -1,9 +1,14 @@
 {
     let x := 0
-    for {} sload(0) { let t := 0 } {
+    for {} sload(0) { let t := 0 /* cannot replace this because of continue */ } {
         x := 0
         if calldataload(0) { x := 2 continue }
+        let r := 0
+        if calldataload(1) { x := 2 revert(0, 0) }
+        let s := 0
     }
+    // cannot replace this
+    let u := 0
 }
 // ----
 // step: commonSubexpressionEliminator
@@ -18,5 +23,13 @@
 //             x := 2
 //             continue
 //         }
+//         let r := x
+//         if calldataload(1)
+//         {
+//             x := 2
+//             revert(0, 0)
+//         }
+//         let s := x
 //     }
+//     let u := 0
 // }
