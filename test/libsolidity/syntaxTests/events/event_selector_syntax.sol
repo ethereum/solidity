5375dfff9dd7e97df34f1ae38341481a1eee5295
Kamil Śliwak <kamil.sliwak@codepoets.it>
2022-06-14 12:00:16 +0200

    Refactor error/event selector tests
    - Remove redundant semantic tests for error selector
    - Consolidate semanticTests/error/ and semanticTests/errors/ dirs
    - Make "selector syntax test" for errors and event an actual test for syntax rather than a copy of the same semantic test

--- test/libsolidity/syntaxTests/events/event_selector_syntax.sol
+++ test/libsolidity/syntaxTests/events/event_selector_syntax.sol
@@ -1,37 +1,20 @@
 library L {
-    event E();
-}
-library S {
-    event E(uint);
-}
-library T {
-    event E();
+    event E(bytes32, bool, bytes indexed);
 }
 
-contract D {
-    event F();
+contract B {
+    event E(bytes32, bool, bytes indexed);
 }
 
-contract C is D {
-    function test1() external pure returns (bytes32, bytes32) {
-        assert(L.E.selector == T.E.selector);
+contract C is B {
+    bytes32 public librarySelector = L.E.selector;
+    bytes32 inheritedSelector = E.selector;
 
-        assert(L.E.selector != S.E.selector);
-        assert(T.E.selector != S.E.selector);
+    function f() public {
+        assert(librarySelector == L.E.selector);
+        assert(E.selector == B.E.selector);
 
-        return (L.E.selector, S.E.selector);
-    }
-
-    bytes32 s1 = L.E.selector;
-    bytes32 s2 = S.E.selector;
-    bytes32 s3 = T.E.selector;
-
-    function test2() view external returns (bytes32, bytes32, bytes32) {
-        return (s1, s2, s3);
-    }
-
-    function test3() pure external returns (bytes32) {
-        return (F.selector);
+        emit E(E.selector, true, "123");
+        emit L.E((B.E.selector), true, "123");
     }
 }
-// ----
