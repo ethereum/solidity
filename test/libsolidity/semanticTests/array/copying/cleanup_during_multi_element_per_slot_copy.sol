9c3a0f11f9fcf06a6396f4714402bb329987c1ef
Daniel Kirchner <daniel@ekpyron.org>
2022-06-15 12:15:54 +0200

    Review suggested test adjustments.

---
+++ test/libsolidity/semanticTests/array/copying/cleanup_during_multi_element_per_slot_copy.sol
@@ -0,0 +1,22 @@
+contract C {
+    uint32[] s;
+    constructor()
+    {
+        s.push();
+        s.push();
+    }
+    function f() external returns (uint)
+    {
+        (s[1], s) = (4, [0]);
+        s = [0];
+        s.push();
+        return s[1];
+        // used to return 4 via IR.
+    }
+}
+// ----
+// constructor()
+// gas irOptimized: 237351
+// gas legacy: 221315
+// gas legacyOptimized: 185247
+// f() -> 0
