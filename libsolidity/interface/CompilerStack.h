23c2bc2b9bf1e89755becf1213ed1362c3e1b160
Alex Beregszaszi <alex@rtfs.hu>
2022-06-27 01:05:53 +0200

    Always enable stdlib

--- libsolidity/interface/CompilerStack.h
+++ libsolidity/interface/CompilerStack.h
@@ -491,7 +491,7 @@ private:
 	RevertStrings m_revertStrings = RevertStrings::Default;
 	State m_stopAfter = State::CompilationSuccessful;
 	bool m_viaIR = false;
-	bool m_stdlib = false;
+	bool m_stdlib = true;
 	langutil::EVMVersion m_evmVersion;
 	ModelCheckerSettings m_modelCheckerSettings;
 	std::map<std::string, std::set<std::string>> m_requestedContractNames;
