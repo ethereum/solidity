2215b6d44b90f1d398212ac548b0271174913fff
Duc Thanh Nguyen <ng.duc.tahn@gmail.com>
2022-07-05 15:30:02 -0400

    Update rangev3 to 0.12.0

--- scripts/release_ppa.sh
+++ scripts/release_ppa.sh
@@ -108,7 +108,7 @@ mv solidity solc
 # Fetch dependencies
 mkdir -p ./solc/deps/downloads/ 2>/dev/null || true
 wget -O ./solc/deps/downloads/jsoncpp-1.9.3.tar.gz https://github.com/open-source-parsers/jsoncpp/archive/1.9.3.tar.gz
-wget -O ./solc/deps/downloads/range-v3-0.11.0.tar.gz https://github.com/ericniebler/range-v3/archive/0.11.0.tar.gz
+wget -O ./solc/deps/downloads/range-v3-0.12.0.tar.gz https://github.com/ericniebler/range-v3/archive/0.12.0.tar.gz
 wget -O ./solc/deps/downloads/fmt-8.0.1.tar.gz https://github.com/fmtlib/fmt/archive/8.0.1.tar.gz
 
 # Determine version
