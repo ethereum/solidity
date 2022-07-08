dd251379e7915910d908cadd9d0c338018782eac
wechman <damian.wechman.it@gmail.com>
2022-07-08 09:31:53 +0200

    Update multiple_custom_operators_same_contract

--- test/libsolidity/semanticTests/operators/custom/multiple_custom_operators_same_contract.sol
+++ test/libsolidity/semanticTests/operators/custom/multiple_custom_operators_same_contract.sol
@@ -1,16 +1,18 @@
+// TODO: Isn't it suppose to be the exact same as all_operators.sol ?
+
 type MyInt is int;
 using {add as +} for MyInt;
 
-function add(MyInt, MyInt) pure returns (bool) {
-    return true;
+function add(MyInt, MyInt) pure returns (MyInt) {
+    return MyInt.wrap(0);
 }
 
 contract C {
-    function f() public pure returns (bool t) {
+    function f() public pure returns (MyInt t) {
         t = MyInt.wrap(2) + MyInt.wrap(7);
     }
 }
 // ====
 // compileViaYul: also
 // ----
-// f() -> true
+// f() -> 0
