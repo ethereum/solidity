fac5666dc926cb693973f5b3076505686563df0a
chriseth <chris@ethereum.org>
2022-11-23 15:28:11 +0100

    Re-implement unused assign / unused store eliminator.

--- libyul/optimiser/UnusedAssignEliminator.h
+++ libyul/optimiser/UnusedAssignEliminator.h
@@ -26,6 +26,7 @@
 #include <libyul/optimiser/ASTWalker.h>
 #include <libyul/optimiser/OptimiserStep.h>
 #include <libyul/optimiser/UnusedStoreBase.h>
+#include <libyul/optimiser/Semantics.h>
 
 #include <map>
 #include <vector>
@@ -62,28 +63,34 @@ struct Dialect;
  * Detailed rules:
  *
  * The AST is traversed twice: in an information gathering step and in the
- * actual removal step. During information gathering, we maintain a
- * mapping from assignment statements to the three states
- * "unused", "undecided" and "used".
- * When an assignment is visited, it is added to the mapping in the "undecided" state
- * (see remark about for loops below) and every other assignment to the same variable
- * that is still in the "undecided" state is changed to "unused".
- * When a variable is referenced, the state of any assignment to that variable still
- * in the "undecided" state is changed to "used".
- * At points where control flow splits, a copy
- * of the mapping is handed over to each branch. At points where control flow
- * joins, the two mappings coming from the two branches are combined in the following way:
- * Statements that are only in one mapping or have the same state are used unchanged.
- * Conflicting values are resolved in the following way:
- * "unused", "undecided" -> "undecided"
- * "unused", "used" -> "used"
- * "undecided, "used" -> "used".
+ * actual removal step. During information gathering, assignment statements
+ * can be marked as "potentially unused" or as "used".
+ *
+ * When an assignment is visited, it is stored in the "set of all stores" and
+ * added to the branch-dependent "active" sets for the assigned variables. This active
+ * set for a variable contains all statements where that variable was last assigned to, i.e.
+ * where a read from that variable could read from.
+ * Furthermore, all other active sets for the assigned variables are cleared.
+ *
+ * When a reference to a variable is visited, the active assignments to that variable
+ * in the current branch are marked as "used". This mark is permanent.
+ * Also, the active set for this variable in the current branch is cleared.
+ *
+ * At points where control-flow splits, we maintain a copy of the active set
+ * (all other data structures are shared across branches).
+ *
+ * At control-flow joins, we combine the sets of active stores for each variable.
+ *
+ * In the example above, the active set right after the assignment "b := mload(a)" (but before
+ * the control-flow join) is "b := mload(a)"; the assignment "b := 2" was removed.
+ * After the control-flow join it will contain both "b := mload(a)" and "b := 2", coming from
+ * the two branches.
  *
  * For for-loops, the condition, body and post-part are visited twice, taking
  * the joining control-flow at the condition into account.
  * In other words, we create three control flow paths: Zero runs of the loop,
  * one run and two runs and then combine them at the end.
- * Running at most twice is enough because there are only three different states.
+ * Running at most twice is enough because this takes into account all possible control-flow connections.
  *
  * Since this algorithm has exponential runtime in the nesting depth of for loops,
  * a shortcut is taken at a certain nesting level: We only use the zero- and
@@ -93,14 +100,13 @@ struct Dialect;
  * For switch statements that have a "default"-case, there is no control-flow
  * part that skips the switch.
  *
- * At ``leave`` statements, all return variables are set to "used".
+ * At ``leave`` statements, all return variables are set to "used" and the set of active statements
+ * is cleared.
  *
- * When a variable goes out of scope, all statements still in the "undecided"
- * state are changed to "unused", unless the variable is the return
- * parameter of a function - there, the state changes to "used".
+ * If a function or builtin is called that does not continue, the set of active statements is
+ * cleared for all variables.
  *
- * In the second traversal, all assignments that are in the "unused" state are removed.
- *
+ * In the second traversal, all assignments that are not marked as "used" are removed.
  *
  * This step is usually run right after the SSA transform to complete
  * the generation of the pseudo-SSA.
@@ -113,12 +119,18 @@ public:
 	static constexpr char const* name{"UnusedAssignEliminator"};
 	static void run(OptimiserStepContext&, Block& _ast);
 
-	explicit UnusedAssignEliminator(Dialect const& _dialect): UnusedStoreBase(_dialect) {}
+	explicit UnusedAssignEliminator(
+		Dialect const& _dialect,
+		std::map<YulString, ControlFlowSideEffects> _controlFlowSideEffects
+	):
+		UnusedStoreBase(_dialect),
+		m_controlFlowSideEffects(_controlFlowSideEffects)
+	{}
 
 	void operator()(Identifier const& _identifier) override;
-	void operator()(VariableDeclaration const& _variableDeclaration) override;
 	void operator()(Assignment const& _assignment) override;
 	void operator()(FunctionDefinition const&) override;
+	void operator()(FunctionCall const& _functionCall) override;
 	void operator()(Leave const&) override;
 	void operator()(Block const& _block) override;
 
@@ -126,18 +138,13 @@ public:
 	void visit(Statement const& _statement) override;
 
 private:
-	void shortcutNestedLoop(TrackedStores const& _beforeLoop) override;
+	void shortcutNestedLoop(ActiveStores const& _beforeLoop) override;
 	void finalizeFunctionDefinition(FunctionDefinition const& _functionDefinition) override;
 
-	void changeUndecidedTo(YulString _variable, State _newState);
-	/// Called when a variable goes out of scope. Sets the state of all still undecided
-	/// assignments to the final state. In this case, this also applies to pending
-	/// break and continue TrackedStores.
-	void finalize(YulString _variable, State _finalState);
+	void markUsed(YulString _variable);
 
-
-	std::set<YulString> m_declaredVariables;
 	std::set<YulString> m_returnVariables;
+	std::map<YulString, ControlFlowSideEffects> m_controlFlowSideEffects;
 };
 
 }
