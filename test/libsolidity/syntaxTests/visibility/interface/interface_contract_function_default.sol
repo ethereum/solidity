9ac638ccbd1fbebd08b6e08d17d92050bbf584b1
Hadrien Croubois <hadrien.croubois@gmail.com>
2023-01-26 20:43:14 +0100

    update tests

--- test/libsolidity/syntaxTests/visibility/interface/interface_contract_function_default.sol
+++ test/libsolidity/syntaxTests/visibility/interface/interface_contract_function_default.sol
@@ -9,4 +9,3 @@ abstract contract C {
 // ----
 // SyntaxError 4937: (158-171): No visibility specified. Did you intend to add "external"?
 // SyntaxError 4937: (200-215): No visibility specified. Did you intend to add "public"?
-// TypeError 1560: (158-171): Functions in interfaces must be declared external or public.
