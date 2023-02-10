e8987727b2113857a1649eb2f12e5c8175dfc370
Tushar Gupta <kshitizgupta108@gmail.com>
2023-02-10 22:08:51 +0530

    added test for prototypeLocation()

--- test/cmdlineTests/ast_function_location/input.sol
+++ test/cmdlineTests/ast_function_location/input.sol
@@ -1,45 +1,6 @@
-// // SPDX-License-Identifier: GPL-3.0
-// pragma solidity >=0.0;
-
-// // vector<Declaration const*> TypeChecker::cleanOverloadedDeclarations(
-// // 	Identifier const& _identifier,
-// // 	vector<Declaration const*> const& _candidates
-// // )
-// // function visit(Identifier _identifier ){
-// //     IdentifierAnnotation& annotation = _identifier.annotation();
-// //     // pp.prototypeLocation();
-// // SecondarySourceLocation ssl;
-// // for (Declaration* declaration : annotation.overloadedDeclarations){
-// //     if (!declaration->location().isValid())
-// //     {
-// //         // Try to re-construct function definition
-// //         string description;
-// //         for (auto const& param: declaration->functionType(true)->parameterTypes())
-// //             description += (description.empty() ? "" : ", ") + param->humanReadableName();
-// //         description = "function " + _identifier.name() + "(" + description + ")";
-
-// //         ssl.append("Candidate: " + description, declaration->location());
-// //     }
-// //     else
-// //         ssl.append("Candidate:", declaration->location());
-// // }
-
-// // }
-// function f(uint x) pure returns (uint) { return x ** 2; }
-// function f() pure returns (uint) { return 42; }
-// contract C {
-//      f(1);
-//     // f.prototypeLocation();
-//         // function (uint) pure returns (uint) immutable i = f;
-// }
-
+// SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.17;
 
-// contract HelloWorld {
-//     string public greet = "Hello World!";
-// }
-function f(uint x) pure returns (uint) { return x ** 2; }
-function f() pure returns (uint) { return 42; }
 function checkPattern(int _x, int _y,int _xStep, int _yStep)  returns(int startX, int startY, int endX, int endY)
 { 
    
@@ -61,10 +22,9 @@ function checkStealing(uint _x, uint _y)
 }
 
 contract C {
-        // function (uint) pure returns (uint) immutable i = f;
+
         uint a = 1;
         uint b = 1;
-        // f(1);
         function callFuncWithKeyValue() public {
           checkStealing(a,b);
         }
