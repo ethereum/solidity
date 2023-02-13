8afdc26cabd24d6afb172761a492ab982b4405a4
chriseth <chris@ethereum.org>
2023-02-13 13:04:37 +0100

    More aggressive runtime code exclusion.

---
+++ test/libsolidity/semanticTests/deployedCodeExclusion/bound_function.sol
@@ -0,0 +1,40 @@
+function longdata(S memory) pure returns (bytes memory) {
+    return
+        "xasopca.pngaibngidak.jbtnudak.cAP.BRRSMCPJAGPD KIAJDOMHUKR,SCPID"
+        "xasopca.pngaibngidak.jbtnudak.cAP.BRRSMCPJAGPD KIAJDOMHUKR,SCPID"
+        "M,SEYBDXCNTKIMNJGO;DUIAQBQUEHAKMPGIDSAJCOUKANJBCUEBKNA.GIAKMV.TI"
+        "AJMO<KXBANJCPGUD ABKCJIDHA NKIMAJU,EKAMHSO;PYCAKUM,L.UCA MR;KITA"
+        "M,SEYBDXCNTKIMNJGO;DUIAQBQUEHAKMPGIDSAJCOUKANJBCUEBKNA.GIAKMV.TI"
+        "AJMO<KXBANJCPGUD ABKCJIDHA NKIMAJU,EKAMHSO;PYCAKUM,L.UCA MR;KITA"
+        " .RPOKIDAS,.CKUMT.,ORKAD ,NOKIDHA .CGKIAD OVHAMS CUAOGT DAKN OIT"
+        "xasopca.pngaibngidak.jbtnudak.cAP.BRRSMCPJAGPD KIAJDOMHUKR,SCPID"
+        "M,SEYBDXCNTKIMNJGO;DUIAQBQUEHAKMPGIDSAJCOUKANJBCUEBKNA.GIAKMV.TI"
+        "AJMO<KXBANJCPGUD ABKCJIDHA NKIMAJU,EKAMHSO;PYCAKUM,L.UCA MR;KITA"
+        "apibakrpidbacnidkacjadtnpdkylca.,jda,r.kuadc,jdlkjd',c'dj, ncg d"
+        "anosumantkudkc,djntudkantuadnc,ui,c.ud,.nujdncud,j.rsch'pkl.'pih";
+}
+
+struct S {
+    uint x;
+}
+
+using { longdata } for S;
+
+contract C {
+    bytes data;
+    constructor() {
+        S memory x;
+        data = x.longdata();
+    }
+    function test() public view returns (bool) {
+        // Tests that the function longdata is not
+        // included in the runtime code.
+        uint x;
+        assembly {
+            x := codesize()
+        }
+        return x < data.length;
+    }
+}
+// ----
+// test() -> true
---
+++ test/libsolidity/semanticTests/deployedCodeExclusion/module_function_deployed.sol
@@ -0,0 +1,32 @@
+==== Source: mod.sol ====
+function longdata() pure returns (bytes memory) {
+    return
+        "xasopca.pngaibngidak.jbtnudak.cAP.BRRSMCPJAGPD KIAJDOMHUKR,SCPID"
+        "xasopca.pngaibngidak.jbtnudak.cAP.BRRSMCPJAGPD KIAJDOMHUKR,SCPID"
+        "M,SEYBDXCNTKIMNJGO;DUIAQBQUEHAKMPGIDSAJCOUKANJBCUEBKNA.GIAKMV.TI"
+        "AJMO<KXBANJCPGUD ABKCJIDHA NKIMAJU,EKAMHSO;PYCAKUM,L.UCA MR;KITA"
+        "M,SEYBDXCNTKIMNJGO;DUIAQBQUEHAKMPGIDSAJCOUKANJBCUEBKNA.GIAKMV.TI"
+        "AJMO<KXBANJCPGUD ABKCJIDHA NKIMAJU,EKAMHSO;PYCAKUM,L.UCA MR;KITA"
+        " .RPOKIDAS,.CKUMT.,ORKAD ,NOKIDHA .CGKIAD OVHAMS CUAOGT DAKN OIT"
+        "xasopca.pngaibngidak.jbtnudak.cAP.BRRSMCPJAGPD KIAJDOMHUKR,SCPID"
+        "M,SEYBDXCNTKIMNJGO;DUIAQBQUEHAKMPGIDSAJCOUKANJBCUEBKNA.GIAKMV.TI"
+        "AJMO<KXBANJCPGUD ABKCJIDHA NKIMAJU,EKAMHSO;PYCAKUM,L.UCA MR;KITA"
+        "apibakrpidbacnidkacjadtnpdkylca.,jda,r.kuadc,jdlkjd',c'dj, ncg d"
+        "anosumantkudkc,djntudkantuadnc,ui,c.ud,.nujdncud,j.rsch'pkl.'pih";
+}
+==== Source: main.sol ====
+import "mod.sol" as M;
+
+contract C {
+    bytes data;
+    function () pure returns (bytes memory) f;
+    constructor() {
+        data = M.longdata();
+        f = M.longdata;
+    }
+    function test() public view returns (bool) {
+        return keccak256(data) == keccak256(f());
+    }
+}
+// ----
+// test() -> true
