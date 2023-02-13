593572b18a8888c28a16c674d8b673f530c6059b
chriseth <chris@ethereum.org>
2023-02-13 13:27:02 +0100

    Gas updates.

--- test/libsolidity/semanticTests/userDefinedValueType/erc20.sol
+++ test/libsolidity/semanticTests/userDefinedValueType/erc20.sol
@@ -114,8 +114,8 @@ contract ERC20 {
 // constructor()
 // ~ emit Transfer(address,address,uint256): #0x00, #0x1212121212121212121212121212120000000012, 0x14
 // gas irOptimized: 362054
-// gas legacy: 852973
-// gas legacyOptimized: 419237
+// gas legacy: 852825
+// gas legacyOptimized: 418837
 // totalSupply() -> 20
 // gas irOptimized: 23415
 // gas legacy: 23653
