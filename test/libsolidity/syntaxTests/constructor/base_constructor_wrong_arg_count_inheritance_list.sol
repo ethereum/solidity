1a981af5481ae8c012fc2cd2c5671afc913b0e64
Kamil Śliwak <kamil.sliwak@codepoets.it>
2023-02-13 20:25:19 +0100

    Only suggest removing parentheses from a construction call if they're empty

--- test/libsolidity/syntaxTests/constructor/base_constructor_wrong_arg_count_inheritance_list.sol
+++ test/libsolidity/syntaxTests/constructor/base_constructor_wrong_arg_count_inheritance_list.sol
@@ -5,5 +5,5 @@ contract C {
 contract D is C(1, true, "a") {}
 contract E is C(1) {}
 // ----
-// TypeError 7927: (61-76): Wrong argument count for constructor call: 3 arguments given but expected 2. Remove parentheses if you do not want to provide arguments here.
-// TypeError 7927: (94-98): Wrong argument count for constructor call: 1 arguments given but expected 2. Remove parentheses if you do not want to provide arguments here.
+// TypeError 7927: (61-76): Wrong argument count for constructor call: 3 arguments given but expected 2
+// TypeError 7927: (94-98): Wrong argument count for constructor call: 1 arguments given but expected 2
