9b000cc5d280d47b7b1682c7c9f9194fa02a7691
Kamil Śliwak <kamil.sliwak@codepoets.it>
2023-02-24 19:49:42 +0100

    Parser: Extract literal parsing into a separate function

--- libsolidity/parsing/Parser.h
+++ libsolidity/parsing/Parser.h
@@ -159,6 +159,7 @@ private:
 	ASTPointer<Expression> parseLeftHandSideExpression(
 		ASTPointer<Expression> const& _partiallyParsedExpression = ASTPointer<Expression>()
 	);
+	ASTPointer<Expression> parseLiteral();
 	ASTPointer<Expression> parsePrimaryExpression();
 	std::vector<ASTPointer<Expression>> parseFunctionCallListArguments();
 
