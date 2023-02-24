0d1b6393d52ef0fec3d08e1880b504522eefd507
Kamil Śliwak <kamil.sliwak@codepoets.it>
2023-02-24 19:35:45 +0100

    Allow marking free functions as suffixes

--- Changelog.md
+++ Changelog.md
@@ -15,6 +15,10 @@ Bugfixes:
  * Antlr Grammar: Stricter rules for function definitions. The grammar will no longer accept as valid free functions having specifiers which are exclusive to contract functions.
 
 
+AST Changes:
+ * AST: Add ``suffix`` field to ``FunctionDefinition`` AST node.
+
+
 ### 0.8.19 (2023-02-22)
 
 Language Features:
--- libsolidity/parsing/Parser.cpp
+++ libsolidity/parsing/Parser.cpp
@@ -526,7 +526,7 @@ StateMutability Parser::parseStateMutability()
 	return stateMutability;
 }
 
-Parser::FunctionHeaderParserResult Parser::parseFunctionHeader(bool _isStateVariable)
+Parser::FunctionHeaderParserResult Parser::parseFunctionHeader(bool _isStateVariable, bool _freeFunction)
 {
 	RecursionGuard recursionGuard(*this);
 	FunctionHeaderParserResult result;
@@ -538,7 +538,18 @@ Parser::FunctionHeaderParserResult Parser::parseFunctionHeader(bool _isStateVari
 	{
 		Token token = m_scanner->currentToken();
 		if (!_isStateVariable && token == Token::Identifier)
-			result.modifiers.push_back(parseModifierInvocation());
+		{
+			if (_freeFunction && currentLiteral() == "suffix")
+			{
+				if (result.usableAsSuffix)
+					parserError(2878_error, "Suffix already specified.");
+				else
+					result.usableAsSuffix = true;
+				advance();
+			}
+			else
+				result.modifiers.push_back(parseModifierInvocation());
+		}
 		else if (TokenTraits::isVisibilitySpecifier(token))
 		{
 			if (result.visibility != Visibility::Default)
@@ -648,7 +659,7 @@ ASTPointer<ASTNode> Parser::parseFunctionDefinition(bool _freeFunction)
 		name = make_shared<ASTString>();
 	}
 
-	FunctionHeaderParserResult header = parseFunctionHeader(false);
+	FunctionHeaderParserResult header = parseFunctionHeader(false /* _isStateVariable */, _freeFunction);
 
 	ASTPointer<Block> block;
 	nodeFactory.markEndPosition();
@@ -667,6 +678,7 @@ ASTPointer<ASTNode> Parser::parseFunctionDefinition(bool _freeFunction)
 		_freeFunction,
 		kind,
 		header.isVirtual,
+		header.usableAsSuffix,
 		header.overrides,
 		documentation,
 		header.parameters,
@@ -1185,7 +1197,7 @@ ASTPointer<FunctionTypeName> Parser::parseFunctionType()
 	RecursionGuard recursionGuard(*this);
 	ASTNodeFactory nodeFactory(*this);
 	expectToken(Token::Function);
-	FunctionHeaderParserResult header = parseFunctionHeader(true);
+	FunctionHeaderParserResult header = parseFunctionHeader(true /* _isStateVariable */, false /* _freeFunction */);
 	return nodeFactory.createNode<FunctionTypeName>(
 		header.parameters,
 		header.returnParameters,
