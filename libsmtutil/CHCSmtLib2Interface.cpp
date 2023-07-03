1d605595519c8b5a237f4ca9d4fef329f30b1399
Martin Blicha <martin.blicha@gmail.com>
2023-07-03 16:09:25 +0200

    Use same spacer parameters as before

--- libsmtutil/CHCSmtLib2Interface.cpp
+++ libsmtutil/CHCSmtLib2Interface.cpp
@@ -189,7 +189,7 @@ std::string CHCSmtLib2Interface::querySolver(std::string const& _input)
 		if (m_enabledSolvers.eld)
 			return "eld";
 		if (m_enabledSolvers.z3)
-			return "z3 rlimit=1000000";
+			return "z3 rlimit=1000000 fp.spacer.q3.use_qgen=true fp.spacer.mbqi=false fp.spacer.ground_pobs=false";
 		return "";
 	}();
 	auto result = m_smtCallback(ReadCallback::kindString(ReadCallback::Kind::SMTQuery) + " " + solverBinary, _input);
