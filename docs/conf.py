9b48434b6a4cb260686507f36623547916d7a2b6
Paul Wackerow <54227730+wackerow@users.noreply.github.com>
2023-07-19 15:57:13 -0400

    fix docs layout and link bugs [Fixes #14436]
    using cdnfonts import instead
    
    fix mobile menu layout
    
    fix mobile content positioning/sizing
    
    fix desktop content widths
    
    prevent header links from wrapping
    
    update desktop positioning
    
    fix backdrop-filter
    
    move to .unified-header::before
    
    feat: independent scrolling for content and menu

--- docs/conf.py
+++ docs/conf.py
@@ -169,7 +169,7 @@ html_js_files = ["js/constants.js", "js/initialize.js", "js/toggle.js"]
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-html_extra_path = ["_static/css", "_static/fonts"]
+html_extra_path = ["_static/css"]
 
 # List of templates of static files to be included in the HTML output.
 # Keys represent paths to input files and values are dicts containing:
