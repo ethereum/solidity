6574c10f2589d868ab7e2846d990807ebd2fd0e7
Pawel Gebal <pawel.gebal@imapp.pl>
2023-07-27 18:16:35 +0200

    SMTChecker: Visit the condition in for and while loops after loop is unrolled

--- test/libsolidity/smtCheckerTests/loops/while_bmc_iterations_semantics_1.sol
+++ test/libsolidity/smtCheckerTests/loops/while_bmc_iterations_semantics_1.sol
@@ -7,14 +7,14 @@ contract C {
 				y = 1;
 			++x;
 		}
-        // BMC loop iteration setting is not enough to leave the loop
-		assert(x == 3);
-		assert(y == 1);
+		// BMC loop iteration setting is not enough to leave the loop
+		assert(x == 0); // should hold - no assumptions on value if didn't complete the loop
+		assert(y == 0); // should hold - no assumptions on value if didn't complete the loop
 	}
 }
 // ====
 // SMTEngine: bmc
 // SMTSolvers: z3
-// BMCLoopIterations: 3
+// BMCLoopIterations: 2
 // ----
 // Info 6002: BMC: 3 verification condition(s) proved safe! Enable the model checker option "show proved safe" to see all of them.
