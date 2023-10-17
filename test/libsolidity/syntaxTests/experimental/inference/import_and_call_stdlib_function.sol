2aa3ed8c43e2d69991ad3916144052edf22ef05e
Kamil Śliwak <kamil.sliwak@codepoets.it>
2023-10-17 13:16:37 +0200

    fixup! Move around experimental tests

--- test/libsolidity/syntaxTests/experimental/inference/import_and_call_stdlib_function.sol
+++ test/libsolidity/syntaxTests/experimental/inference/import_and_call_stdlib_function.sol
@@ -12,6 +12,7 @@ contract C
 
 // ====
 // EVMVersion: >=constantinople
+// compileViaYul: true
 // ----
 // Warning 2264: (std.stub:63-92): Experimental features are turned on. Do not use experimental features on live deployments.
 // Warning 2264: (0-29): Experimental features are turned on. Do not use experimental features on live deployments.
