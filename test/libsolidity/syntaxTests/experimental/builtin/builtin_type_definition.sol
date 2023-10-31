59f25f5e2f56b7c1dcf80cf5415324b704f8b9c0
Kamil Śliwak <kamil.sliwak@codepoets.it>
2023-10-31 13:00:53 +0100

    Mark calls via function pointers as unimplemented in the codegen

--- test/libsolidity/syntaxTests/experimental/builtin/builtin_type_definition.sol
+++ test/libsolidity/syntaxTests/experimental/builtin/builtin_type_definition.sol
@@ -33,6 +33,7 @@ contract C {
 // EVMVersion: >=constantinople
 // compileViaYul: true
 // ----
+// UnimplementedFeatureError: No support for calling functions pointers yet.
 // Warning 2264: (0-29): Experimental features are turned on. Do not use experimental features on live deployments.
 // Info 4164: (31-61): Inferred type: void
 // Info 4164: (63-93): Inferred type: bool
