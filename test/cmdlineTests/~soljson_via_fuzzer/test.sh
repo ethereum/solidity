dade6e774349c5279d8df93b09b56228c4b74374
Nikola Matic <nikola.matic@ethereum.org>
2023-12-11 09:58:30 +0100

    Install GNU grep for OSX, and use if exists
    Tighten up the grep

--- test/cmdlineTests/~soljson_via_fuzzer/test.sh
+++ test/cmdlineTests/~soljson_via_fuzzer/test.sh
@@ -10,7 +10,7 @@ cd "$SOLTMPDIR"
 "$REPO_ROOT"/scripts/isolate_tests.py "$REPO_ROOT"/test/
 "$REPO_ROOT"/scripts/isolate_tests.py "$REPO_ROOT"/docs/
 
-grep -L --include="*.sol" "pragma experimental solidity;" * | xargs -P 4 -n 50 "${SOLIDITY_BUILD_DIR}/test/tools/solfuzzer" --quiet --input-files
-grep -L --include="*.sol" "pragma experimental solidity;" * | xargs -P 4 -n 50 "${SOLIDITY_BUILD_DIR}/test/tools/solfuzzer" --without-optimizer --quiet --input-files
+gnu_grep -L --include="*.sol" "^pragma experimental solidity;$" ./* | xargs -P 4 -n 50 "${SOLIDITY_BUILD_DIR}/test/tools/solfuzzer" --quiet --input-files
+gnu_grep -L --include="*.sol" "^pragma experimental solidity;$" ./* | xargs -P 4 -n 50 "${SOLIDITY_BUILD_DIR}/test/tools/solfuzzer" --without-optimizer --quiet --input-files
 
 rm -r "$SOLTMPDIR"
