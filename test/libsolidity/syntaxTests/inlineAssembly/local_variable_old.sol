5e000eab41b4b8740564046c15cabaff13c5fd32
00guap <andrekorol@protonmail.com>
2024-01-27 03:19:16 -0300

    test(libsolidity): simplify local_variable_old.sol test case

--- test/libsolidity/syntaxTests/inlineAssembly/local_variable_old.sol
+++ test/libsolidity/syntaxTests/inlineAssembly/local_variable_old.sol
@@ -1,25 +1,12 @@
 contract C {
-    address someAddress = 0xCAfEcAfeCAfECaFeCaFecaFecaFECafECafeCaFe;
-    bytes4 private constant _SOME_SELECTOR = bytes4(keccak256("hey()"));
-
     fallback() external {
-        (bool success, bytes memory result) = someAddress.call(
-            abi.encodePacked(_SOME_SELECTOR, msg.sender, uint8(0xff))
-        );
-
         assembly {
-            // `mload(result)` -> offset in memory where `result.length` is located
-            // `add(result, 32)` -> offset in memory where `result` data starts
-            let resultdata_size := mload(result)
-            let resultdata_offset := add(result, 32)
-
-            // if call failed, revert
-            if eq(success, 0) {
-                revert(resultdata_offset, resultdata_size)
+            let x_offset := 0
+            let x_slot := 0
+            {
+                x_offset := 1
+                x_slot := 1
             }
-
-            // otherwise return the data returned by the external call
-            return(resultdata_offset, resultdata_size)
         }
     }
 }
