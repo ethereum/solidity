b0ffcccf5c7b1b86b924aa27f7157cbc49f0d9da
r0qs <deepmarolaest@gmail.com>
2024-03-14 07:40:36 +0100

    Use specific hardhat version on openzeppelin and gp2 external tests (#14938)

--- test/externalTests/gp2.sh
+++ test/externalTests/gp2.sh
@@ -66,6 +66,15 @@ function gp2_test
     force_hardhat_compiler_settings "$config_file" "$(first_word "$SELECTED_PRESETS")" "$config_var"
     force_hardhat_unlimited_contract_size "$config_file" "$config_var"
     yarn
+    # We require to install hardhat 2.20.0 due to support for evm version cancun, otherwise we get the following error:
+    # Invalid value {"blockGasLimit":12500000,"hardfork":"cancun","allowUnlimitedContractSize":true} for HardhatConfig.networks.hardhat - Expected a value of type HardhatNetworkConfig.
+    # See: https://github.com/NomicFoundation/hardhat/issues/4176
+    yarn add hardhat@2.20.0
+
+    # Ignore bench directory which fails to compile with current hardhat and ethers versions.
+    # bench/trace/gas.ts:123:19 - error TS2339: Property 'equals' does not exist on type 'Uint8Array'.
+    jq '. + {"exclude": ["bench"]}' tsconfig.json > temp.json
+    mv temp.json tsconfig.json
 
     # Remove the config section that requires an Etherscan key. We don't need it just to run tests.
     sed -i '/^  etherscan: {$/,/^  },$/d' hardhat.config.ts
