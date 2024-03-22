bb868cf90d459f4263c8b51ee4b027c0d9660098
Drew Roberts <github@drewroberts.com>
2024-03-22 18:30:53 -0400

    Update copyright year

--- test/externalTests/prb-math.py
+++ test/externalTests/prb-math.py
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with solidity.  If not, see <http://www.gnu.org/licenses/>
 #
-# (c) 2023 solidity contributors.
+# (c) 2024 solidity contributors.
 # ------------------------------------------------------------------------------
 
 import sys
