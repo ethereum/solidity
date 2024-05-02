66f08332737ab5fabbd0b3a70b9ceb8d123b0217
Hadrien Croubois <hadrien.croubois@gmail.com>
2024-05-02 10:21:58 +0200

    fix tests after merge

--- test/libsolidity/syntaxTests/abiEncoder/abi_encodeError_tuple_from_error_event.sol
+++ test/libsolidity/syntaxTests/abiEncoder/abi_encodeError_tuple_from_error_event.sol
@@ -12,5 +12,5 @@ contract C {
 }
 // ----
 // TypeError 9063: (122-126): Expected an inline tuple, not an expression of a tuple type.
-// TypeError 9063: (157-161): Expected an inline tuple, not an expression of a tuple type.
+// TypeError 7516: (137-162): Expected a tuple with 0 components instead of a single non-tuple parameter.
 // TypeError 9063: (192-200): Expected an inline tuple, not an expression of a tuple type.
