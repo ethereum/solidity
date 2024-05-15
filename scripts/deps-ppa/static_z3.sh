70e1edcebe3c0e5621ca590afae2744e9a284e92
Daniel Kirchner <daniel@ekpyron.org>
2024-05-15 18:39:25 +0200

    Update PPA release script.

--- scripts/deps-ppa/static_z3.sh
+++ scripts/deps-ppa/static_z3.sh
@@ -41,7 +41,7 @@ sourcePPAConfig
 # Sanity check
 checkDputEntries "\[cpp-build-deps\]"
 
-DISTRIBUTIONS="focal jammy mantic"
+DISTRIBUTIONS="focal jammy mantic noble"
 
 for distribution in $DISTRIBUTIONS
 do
