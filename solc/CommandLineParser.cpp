88a87278d839c58553ad147b7c21403b94d8a83b
r0qs <rodrigo.saramago@ethereum.org>
2024-06-04 11:32:49 +0200

    Allow yul cfg export in assembler mode

--- solc/CommandLineParser.cpp
+++ solc/CommandLineParser.cpp
@@ -472,6 +472,7 @@ void CommandLineParser::parseOutputSelection()
 			CompilerOutputs::componentName(&CompilerOutputs::binary),
 			CompilerOutputs::componentName(&CompilerOutputs::irOptimized),
 			CompilerOutputs::componentName(&CompilerOutputs::astCompactJson),
+			CompilerOutputs::componentName(&CompilerOutputs::yulCFGJson),
 		};
 		static std::set<std::string> const evmAssemblyJsonImportModeOutputs = {
 			CompilerOutputs::componentName(&CompilerOutputs::asm_),
