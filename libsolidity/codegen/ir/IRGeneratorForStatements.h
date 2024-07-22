5cbfd0d1985aab589c8501998a4459c79e42171d
Nikola Matic <nikola.matic@ethereum.org>
2024-07-22 12:43:17 +0200

    Via-IR code-gen for transient state variables
    Alternative approach
    
    Set variable declaration location explicitly

--- libsolidity/codegen/ir/IRGeneratorForStatements.h
+++ libsolidity/codegen/ir/IRGeneratorForStatements.h
@@ -228,6 +228,7 @@ private:
 
 	/// Assigns the value of @a _value to the lvalue @a _lvalue.
 	void writeToLValue(IRLValue const& _lvalue, IRVariable const& _value);
+
 	/// @returns a fresh IR variable containing the value of the lvalue @a _lvalue.
 	IRVariable readFromLValue(IRLValue const& _lvalue);
 
