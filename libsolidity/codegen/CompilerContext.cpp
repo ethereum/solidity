remispbost/1212121

yul: AsmPrinter has mode enum to determine printing mode instead of dialect pointer

libsolidity/codegen/CompilerContext.cpp
@@ -491,8 +491,8 @@ void CompilerContext::appendInlineAssembly(
 		{
 			// Store as generated sources, but first re-parse to update the source references.
 			solAssert(m_generatedYulUtilityCode.empty(), "");
-			m_generatedYulUtilityCode = yul::AsmPrinter(dialect)(*obj.code);
-			std::string code = yul::AsmPrinter{dialect}(*obj.code);
+			m_generatedYulUtilityCode = yul::AsmPrinter(yul::AsmPrinter::Mode::OmitDefaultType, dialect)(*obj.code);
+			std::string code = yul::AsmPrinter{yul::AsmPrinter::Mode::OmitDefaultType, dialect}(*obj.code);
 			langutil::CharStream charStream(m_generatedYulUtilityCode, _sourceName);
 			obj.code = yul::Parser(errorReporter, dialect).parse(charStream);
 			*obj.analysisInfo = yul::AsmAnalyzer::analyzeStrictAssertCorrect(dialect, obj);

libsolidity/codegen/ir/IRGeneratorForStatements.cpp
@@ -2256,8 +2256,7 @@ bool IRGeneratorForStatements::visit(InlineAssembly const& _inlineAsm)
 
 	solAssert(std::holds_alternative<yul::Block>(modified));
 
-	// Do not provide dialect so that we get the full type information.
-	appendCode() << yul::AsmPrinter()(std::get<yul::Block>(modified)) << "\n";
+	appendCode() << yul::AsmPrinter(yul::AsmPrinter::Mode::FullTypeInfo, _inlineAsm.dialect())(std::get<yul::Block>(modified)) << "\n";
 	return false;
 }
 

libyul/Object.cpp
@@ -38,13 +38,14 @@ using namespace solidity::langutil;
 using namespace solidity::util;
 using namespace solidity::yul;
 
-std::string Data::toString(Dialect const*, DebugInfoSelection const&, CharStreamProvider const*) const
+std::string Data::toString(Dialect const&, AsmPrinter::Mode, DebugInfoSelection const&, CharStreamProvider const*) const
 {
 	return "data \"" + name.str() + "\" hex\"" + util::toHex(data) + "\"";
 }
 
 std::string Object::toString(
-	Dialect const* _dialect,
+	Dialect const& _dialect,
+	AsmPrinter::Mode const _printingMode,
 	DebugInfoSelection const& _debugInfoSelection,
 	CharStreamProvider const* _soliditySourceProvider
 ) const
@@ -63,6 +64,7 @@ std::string Object::toString(
 			"\n";
 
 	std::string inner = "code " + AsmPrinter(
+		_printingMode,
 		_dialect,
 		debugData->sourceNames,
 		_debugInfoSelection,
@@ -70,7 +72,7 @@ std::string Object::toString(
 	)(*code);
 
 	for (auto const& obj: subObjects)
-		inner += "\n" + obj->toString(_dialect, _debugInfoSelection, _soliditySourceProvider);
+		inner += "\n" + obj->toString(_dialect, _printingMode, _debugInfoSelection, _soliditySourceProvider);
 
 	return useSrcComment + "object \"" + name.str() + "\" {\n" + indent(inner) + "\n}";
 }

libyul/Object.h
@@ -21,6 +21,7 @@
 
 #pragma once
 
+#include <libyul/AsmPrinter.h>
 #include <libyul/ASTForward.h>
 #include <libyul/YulString.h>
 
@@ -54,7 +55,8 @@ struct ObjectNode
 	/// Can be empty since .yul files can also just contain code, without explicitly placing it in an object.
 	YulString name;
 	virtual std::string toString(
-		Dialect const* _dialect,
+		Dialect const& _dialect,
+		AsmPrinter::Mode printingMode,
 		langutil::DebugInfoSelection const& _debugInfoSelection,
 		langutil::CharStreamProvider const* _soliditySourceProvider
 	) const = 0;
@@ -71,7 +73,8 @@ struct Data: public ObjectNode
 	bytes data;
 
 	std::string toString(
-		Dialect const* _dialect,
+		Dialect const& _dialect,
+		AsmPrinter::Mode printingMode,
 		langutil::DebugInfoSelection const& _debugInfoSelection,
 		langutil::CharStreamProvider const* _soliditySourceProvider
 	) const override;
@@ -93,7 +96,8 @@ struct Object: public ObjectNode
 public:
 	/// @returns a (parseable) string representation.
 	std::string toString(
-		Dialect const* _dialect,
+		Dialect const& _dialect,
+		AsmPrinter::Mode printingMode = AsmPrinter::Mode::FullTypeInfo,
 		langutil::DebugInfoSelection const& _debugInfoSelection = langutil::DebugInfoSelection::Default(),
 		langutil::CharStreamProvider const* _soliditySourceProvider = nullptr
 	) const;

libyul/optimiser/Suite.cpp
@@ -510,7 +510,7 @@ void OptimiserSuite::runSequence(std::vector<std::string> const& _steps, Block& 
 			else
 			{
 				std::cout << "== Running " << step << " changed the AST." << std::endl;
-				std::cout << AsmPrinter{}(_ast) << std::endl;
+				std::cout << AsmPrinter{AsmPrinter::Mode::FullTypeInfo, m_context.dialect}(_ast) << std::endl;
 				copy = std::make_unique<Block>(std::get<Block>(ASTCopier{}(_ast)));
 			}
 		}

test/libyul/Common.cpp
@@ -96,7 +96,9 @@ yul::Block yul::test::disambiguate(std::string const& _source, bool _yul)
 
 std::string yul::test::format(std::string const& _source, bool _yul)
 {
-	return yul::AsmPrinter()(*parse(_source, _yul).first);
+	auto const version = solidity::test::CommonOptions::get().evmVersion();
+	Dialect const& dialect = _yul ? EVMDialectTyped::instance(version) : EVMDialect::strictAssemblyForEVMObjects(version);
+	return yul::AsmPrinter(yul::AsmPrinter::Mode::FullTypeInfo, dialect)(*parse(_source, _yul).first);
 }
 
 namespace

test/tools/yulopti.cpp
@@ -181,7 +181,7 @@ public:
 		parse(_source);
 		disambiguate();
 		OptimiserSuite{m_context}.runSequence(_steps, *m_ast);
-		std::cout << AsmPrinter{m_dialect}(*m_ast) << std::endl;
+		std::cout << AsmPrinter{AsmPrinter::Mode::OmitDefaultType, m_dialect}(*m_ast) << std::endl;
 	}
 
 	void runInteractive(std::string _source, bool _disambiguated = false)
@@ -229,7 +229,7 @@ public:
 							*m_ast
 						);
 				}
-				_source = AsmPrinter{m_dialect}(*m_ast);
+				_source = AsmPrinter{AsmPrinter::Mode::OmitDefaultType, m_dialect}(*m_ast);
 			}
 			catch (...)
 			{

ethereum/solidity

libsolidity/codegen/CompilerContext.cpp
@@ -491,8 +491,8 @@ void CompilerContext::appendInlineAssembly(
 		{
 			// Store as generated sources, but first re-parse to update the source references.
 			solAssert(m_generatedYulUtilityCode.empty(), "");
-			m_generatedYulUtilityCode = yul::AsmPrinter(dialect)(*obj.code);
-			std::string code = yul::AsmPrinter{dialect}(*obj.code);
+			m_generatedYulUtilityCode = yul::AsmPrinter(yul::AsmPrinter::TypePrinting::OmitDefault, dialect)(*obj.code);
+			std::string code = yul::AsmPrinter{yul::AsmPrinter::TypePrinting::OmitDefault, dialect}(*obj.code);
 			langutil::CharStream charStream(m_generatedYulUtilityCode, _sourceName);
 			obj.code = yul::Parser(errorReporter, dialect).parse(charStream);
 			*obj.analysisInfo = yul::AsmAnalyzer::analyzeStrictAssertCorrect(dialect, obj);

libsolidity/codegen/ir/IRGeneratorForStatements.cpp
@@ -2256,8 +2256,7 @@ bool IRGeneratorForStatements::visit(InlineAssembly const& _inlineAsm)
 
 	solAssert(std::holds_alternative<yul::Block>(modified));
 
-	// Do not provide dialect so that we get the full type information.
-	appendCode() << yul::AsmPrinter()(std::get<yul::Block>(modified)) << "\n";
+	appendCode() << yul::AsmPrinter(yul::AsmPrinter::TypePrinting::Full, _inlineAsm.dialect())(std::get<yul::Block>(modified)) << "\n";
 	return false;
 }
 

libyul/Object.cpp
@@ -38,13 +38,14 @@ using namespace solidity::langutil;
 using namespace solidity::util;
 using namespace solidity::yul;
 
-std::string Data::toString(Dialect const*, DebugInfoSelection const&, CharStreamProvider const*) const
+std::string Data::toString(Dialect const&, AsmPrinter::TypePrinting, DebugInfoSelection const&, CharStreamProvider const*) const
 {
 	return "data \"" + name + "\" hex\"" + util::toHex(data) + "\"";
 }
 
 std::string Object::toString(
-	Dialect const* _dialect,
+	Dialect const& _dialect,
+	AsmPrinter::TypePrinting const _printingMode,
 	DebugInfoSelection const& _debugInfoSelection,
 	CharStreamProvider const* _soliditySourceProvider
 ) const
@@ -63,6 +64,7 @@ std::string Object::toString(
 			"\n";
 
 	std::string inner = "code " + AsmPrinter(
+		_printingMode,
 		_dialect,
 		debugData->sourceNames,
 		_debugInfoSelection,
@@ -70,7 +72,7 @@ std::string Object::toString(
 	)(*code);
 
 	for (auto const& obj: subObjects)
-		inner += "\n" + obj->toString(_dialect, _debugInfoSelection, _soliditySourceProvider);
+		inner += "\n" + obj->toString(_dialect, _printingMode, _debugInfoSelection, _soliditySourceProvider);
 
 	return useSrcComment + "object \"" + name + "\" {\n" + indent(inner) + "\n}";
 }

libyul/Object.h
@@ -21,6 +21,7 @@
 
 #pragma once
 
+#include <libyul/AsmPrinter.h>
 #include <libyul/ASTForward.h>
 
 #include <liblangutil/CharStreamProvider.h>
@@ -53,7 +54,8 @@ struct ObjectNode
 	/// Can be empty since .yul files can also just contain code, without explicitly placing it in an object.
 	std::string name;
 	virtual std::string toString(
-		Dialect const* _dialect,
+		Dialect const& _dialect,
+		AsmPrinter::TypePrinting printingMode,
 		langutil::DebugInfoSelection const& _debugInfoSelection,
 		langutil::CharStreamProvider const* _soliditySourceProvider
 	) const = 0;
@@ -70,7 +72,8 @@ struct Data: public ObjectNode
 	bytes data;
 
 	std::string toString(
-		Dialect const* _dialect,
+		Dialect const& _dialect,
+		AsmPrinter::TypePrinting printingMode,
 		langutil::DebugInfoSelection const& _debugInfoSelection,
 		langutil::CharStreamProvider const* _soliditySourceProvider
 	) const override;
@@ -92,7 +95,8 @@ struct Object: public ObjectNode
 public:
 	/// @returns a (parseable) string representation.
 	std::string toString(
-		Dialect const* _dialect,
+		Dialect const& _dialect,
+		AsmPrinter::TypePrinting printingMode = AsmPrinter::TypePrinting::Full,
 		langutil::DebugInfoSelection const& _debugInfoSelection = langutil::DebugInfoSelection::Default(),
 		langutil::CharStreamProvider const* _soliditySourceProvider = nullptr
 	) const;

libyul/optimiser/Suite.cpp
@@ -510,7 +510,7 @@ void OptimiserSuite::runSequence(std::vector<std::string> const& _steps, Block& 
 			else
 			{
 				std::cout << "== Running " << step << " changed the AST." << std::endl;
-				std::cout << AsmPrinter{}(_ast) << std::endl;
+				std::cout << AsmPrinter{AsmPrinter::TypePrinting::Full, m_context.dialect}(_ast) << std::endl;
 				copy = std::make_unique<Block>(std::get<Block>(ASTCopier{}(_ast)));
 			}
 		}

test/libyul/Common.cpp
@@ -96,7 +96,9 @@ yul::Block yul::test::disambiguate(std::string const& _source, bool _yul)
 
 std::string yul::test::format(std::string const& _source, bool _yul)
 {
-	return yul::AsmPrinter()(*parse(_source, _yul).first);
+	auto const version = solidity::test::CommonOptions::get().evmVersion();
+	Dialect const& dialect = _yul ? EVMDialectTyped::instance(version) : EVMDialect::strictAssemblyForEVMObjects(version);
+	return yul::AsmPrinter(yul::AsmPrinter::TypePrinting::Full, dialect)(*parse(_source, _yul).first);
 }
 
 namespace

test/tools/yulopti.cpp
@@ -181,7 +181,7 @@ public:
 		parse(_source);
 		disambiguate();
 		OptimiserSuite{m_context}.runSequence(_steps, *m_ast);
-		std::cout << AsmPrinter{m_dialect}(*m_ast) << std::endl;
+		std::cout << AsmPrinter{AsmPrinter::TypePrinting::OmitDefault, m_dialect}(*m_ast) << std::endl;
 	}
 
 	void runInteractive(std::string _source, bool _disambiguated = false)
@@ -229,7 +229,7 @@ public:
 							*m_ast
 						);
 				}
-				_source = AsmPrinter{m_dialect}(*m_ast);
+				_source = AsmPrinter{AsmPrinter::TypePrinting::OmitDefault, m_dialect}(*m_ast);
 			}
 			catch (...)
 			{