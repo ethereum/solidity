a298438fbb296e4a65aa8e179ac9855ded915a0a
Kamil Śliwak <kamil.sliwak@codepoets.it>
2024-09-02 18:38:48 +0200

    We no longer use a `release` branch

--- scripts/docker_deploy_manual.sh
+++ scripts/docker_deploy_manual.sh
@@ -21,7 +21,7 @@ cd solidity
 commithash=$(git rev-parse --short=8 HEAD)
 echo -n "$commithash" > commit_hash.txt
 version=$("$(dirname "$0")/get_version.sh")
-if [ "$branch" = "release" ] || [ "$branch" = v"$version" ]
+if [ "$branch" = v"$version" ]
 then
     echo -n > prerelease.txt
 else
