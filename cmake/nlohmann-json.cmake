aa47181eef8fa63a6b4f52bff2c05517c66297a2
Rui Chen <rui@chenrui.dev>
2024-09-08 10:14:44 -0400

    chore(build): prefer system deps for fmt/nlohmann_json/range-v3
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- cmake/nlohmann-json.cmake
+++ cmake/nlohmann-json.cmake
@@ -1,9 +1,14 @@
-include(${CMAKE_SOURCE_DIR}/cmake/submodules.cmake)
-initialize_submodule(nlohmann-json)
+find_package(nlohmann_json QUIET)
 
 set(JSON_Install OFF CACHE INTERNAL "")
-add_subdirectory(
-	${CMAKE_SOURCE_DIR}/deps/nlohmann-json
-	EXCLUDE_FROM_ALL
-)
 
+if(nlohmann_json_FOUND)
+	message(STATUS "Using system-installed nlohmann-json library")
+else()
+	include(${CMAKE_SOURCE_DIR}/cmake/submodules.cmake)
+	initialize_submodule(nlohmann-json)
+	add_subdirectory(
+		${CMAKE_SOURCE_DIR}/deps/nlohmann-json
+		EXCLUDE_FROM_ALL
+	)
+endif()
