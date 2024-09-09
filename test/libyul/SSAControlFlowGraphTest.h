8162c84e962ad870c9d3ff59077429adac0a0713
clonker <1685266+clonker@users.noreply.github.com>
2024-09-09 12:34:41 +0200

    Add liveness information to dot output

--- test/libyul/SSAControlFlowGraphTest.h
+++ test/libyul/SSAControlFlowGraphTest.h
@@ -1,18 +1,18 @@
 /*
-This file is part of solidity.
+	This file is part of solidity.
 
-solidity is free software: you can redistribute it and/or modify
+	solidity is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
-		the Free Software Foundation, either version 3 of the License, or
-									 (at your option) any later version.
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
 
-									 solidity is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+	solidity is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with solidity.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with solidity.  If not, see <http://www.gnu.org/licenses/>.
 */
 // SPDX-License-Identifier: GPL-3.0
 
@@ -25,6 +25,7 @@ along with solidity.  If not, see <http://www.gnu.org/licenses/>.
 namespace solidity::yul
 {
 struct Dialect;
+class SSACFG;
 
 namespace test
 {
@@ -36,6 +37,7 @@ public:
 	explicit SSAControlFlowGraphTest(std::string const& _filename);
 	TestResult run(std::ostream& _stream, std::string const& _linePrefix = "", bool const _formatted = false) override;
 private:
+	void printLiveness(SSACFG const& _cfg) const;
 	Dialect const* m_dialect = nullptr;
 };
 }
