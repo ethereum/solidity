3651f231fd701340927c354c18a598122d7dbf1c
clonker <1685266+clonker@users.noreply.github.com>
2024-10-07 08:13:55 +0200

    Use gold linker for ossfuzz, codecov, and cxx20 builds

--- cmake/toolchains/libfuzzer.cmake
+++ cmake/toolchains/libfuzzer.cmake
@@ -7,7 +7,8 @@ set(OSSFUZZ ON CACHE BOOL "Enable fuzzer build" FORCE)
 # Use libfuzzer as the fuzzing back-end
 set(LIB_FUZZING_ENGINE "-fsanitize=fuzzer" CACHE STRING "Use libfuzzer back-end" FORCE)
 # clang/libfuzzer specific flags for UBSan instrumentation
-set(CMAKE_CXX_FLAGS "-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -I /usr/local/include/c++/v1 -fsanitize=undefined -fsanitize=fuzzer-no-link -stdlib=libc++" CACHE STRING "Custom compilation flags" FORCE)
+# uses the more memory-efficient gold, which allows us to stay with the "large" resource class without OOM errors
+set(CMAKE_CXX_FLAGS "-O1 -fno-omit-frame-pointer -gline-tables-only -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -I /usr/local/include/c++/v1 -fsanitize=undefined -fsanitize=fuzzer-no-link -fuse-ld=gold -stdlib=libc++" CACHE STRING "Custom compilation flags" FORCE)
 # Link statically against boost libraries
 set(BOOST_FOUND ON CACHE BOOL "" FORCE)
 set(Boost_USE_STATIC_LIBS ON CACHE BOOL "Link against static Boost libraries" FORCE)
