911b322d249c9a3fa135726e0ca2d9d8d10e2e8a
Kamil Śliwak <kamil.sliwak@codepoets.it>
2024-10-11 17:55:06 +0200

    FullInliner: Remove separate pass for inlining tiny functions

--- test/libsolidity/semanticTests/array/copying/array_of_function_external_storage_to_storage_dynamic.sol
+++ test/libsolidity/semanticTests/array/copying/array_of_function_external_storage_to_storage_dynamic.sol
@@ -45,7 +45,7 @@ contract C {
 }
 // ----
 // copyExternalStorageArrayOfFunctionType() -> true
-// gas irOptimized: 104566
+// gas irOptimized: 104629
 // gas legacy: 108554
 // gas legacyOptimized: 102405
 // copyInternalArrayOfFunctionType() -> true
