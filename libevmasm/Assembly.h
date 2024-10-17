6fc82fe9e2a71d91fb2abbeb04f9f034d1e1782e
rodiazet <radek.zagorowicz@gmail.com>
2024-10-17 21:57:12 +0200

    eof: `auxdatastore` in EOF

--- libevmasm/Assembly.h
+++ libevmasm/Assembly.h
@@ -80,6 +80,7 @@ public:
 	AssemblyItem newPushImmutable(std::string const& _identifier);
 	AssemblyItem newImmutableAssignment(std::string const& _identifier);
 	AssemblyItem newAuxDataLoadN(size_t offset);
+	AssemblyItem newAuxDataStore();
 
 	AssemblyItem const& append(AssemblyItem _i);
 	AssemblyItem const& append(bytes const& _data) { return append(newData(_data)); }
@@ -91,8 +92,10 @@ public:
 	void appendProgramSize() { append(AssemblyItem(PushProgramSize)); }
 	void appendLibraryAddress(std::string const& _identifier) { append(newPushLibraryAddress(_identifier)); }
 	void appendImmutable(std::string const& _identifier) { append(newPushImmutable(_identifier)); }
-	void appendImmutableAssignment(std::string const& _identifier) { append(newImmutableAssignment(_identifier)); }
+	void appendImmutableAssignment(std::string const& _identifier) { append(newImmutableAssignment(_identifier));}
+
 	void appendAuxDataLoadN(uint16_t _offset) { append(newAuxDataLoadN(_offset));}
+	void appendAuxDataStore() { append(newAuxDataStore());}
 
 	void appendVerbatim(bytes _data, size_t _arguments, size_t _returnVariables)
 	{
